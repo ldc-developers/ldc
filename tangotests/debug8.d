6565e9e09f068a06659bd29333497b9e54619eae
Tomas Lindquist Olsen <tomas.l.olsen@gmail.com>
2008-06-12 17:01:31 +0200

    [svn r270] Forgot the test for struct debug info.

---
+++ tangotests/debug8.d
@@ -0,0 +1,31 @@
+module tangotests.debug8;
+
+struct Str
+{
+    size_t len;
+    void* ptr;
+}
+
+struct Foo
+{
+    long l;
+    Bar bar;
+}
+
+struct Bar
+{
+    float x,y,z;
+    Foo* foo;
+}
+
+void main()
+{
+    Str str;
+    Foo foo;
+    foo.l = 42;
+    foo.bar.y = 3.1415;
+    foo.bar.foo = &foo;
+
+    int* fail;
+    *fail = 0;
+}
