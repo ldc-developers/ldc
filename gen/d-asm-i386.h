732cdc576603178e66149f2b33e1f525f9e5f11f
Christian Kamm <kamm@incasoftware.de>
2008-07-26 13:38:25 +0200

    Fix inline asm FS:4 by writing the displacement directly into the asm instead of using %fs:$0 with an input constraint.

--- gen/d-asm-i386.h
+++ gen/d-asm-i386.h
@@ -1929,7 +1929,8 @@ struct AsmProcessor
 		if (operand->constDisplacement) {
 		    if (operand->symbolDisplacement.dim)
 			insnTemplate->writebyte('+');
-		    addOperand(fmt, Arg_Integer, newIntExp(operand->constDisplacement), asmcode);
+		    //addOperand(fmt, Arg_Integer, newIntExp(operand->constDisplacement), asmcode);
+		    insnTemplate->printf("%d", operand->constDisplacement);
 		    if (opInfo->operands[i] & Opr_Dest)
 			asmcode->clobbersMemory = 1;
 		}
