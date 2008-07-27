befaf511e6cfcfd4ac604d367ae0242a8d0ff84b
Christian Kamm <kamm@incasoftware.de>
2008-07-27 13:29:31 +0200

    Fix compile-time comparison of template value parameters of real type.

--- dmd/expression.c
+++ dmd/expression.c
@@ -1498,7 +1498,7 @@ int RealEquals(real_t x1, real_t x2)
 	/* In some cases, the REALPAD bytes get garbage in them,
 	 * so be sure and ignore them.
 	 */
-	memcmp(&x1, &x2, REALSIZE - REALPAD) == 0;
+	memcmp(&x1, &x2, sizeof(real_t)) == 0;
 }
 
 int RealExp::equals(Object *o)
