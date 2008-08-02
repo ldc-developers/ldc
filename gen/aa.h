1ee9104354d17b30e44597cc64f986965d0d3ab6
Tomas Lindquist Olsen <tomas.l.olsen@gmail.com>
2008-08-02 22:35:24 +0200

    Fixed AA Rvalue-only access (like indexing an AA return value immediately).

--- gen/aa.h
+++ gen/aa.h
@@ -1,7 +1,7 @@
 #ifndef LLVMDC_GEN_AA_H
 #define LLVMDC_GEN_AA_H
 
-DValue* DtoAAIndex(Loc& loc, Type* type, DValue* aa, DValue* key);
+DValue* DtoAAIndex(Loc& loc, Type* type, DValue* aa, DValue* key, bool lvalue);
 DValue* DtoAAIn(Loc& loc, Type* type, DValue* aa, DValue* key);
 void DtoAARemove(Loc& loc, DValue* aa, DValue* key);
 
