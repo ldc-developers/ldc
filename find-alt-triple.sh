5275ac0db4922ca5f1d27469ba1185890cab687b
Tomas Lindquist Olsen <tomas.l.olsen@gmail.com>
2009-03-01 22:27:37 +0100

    Added script to find the alternative triple of the host.

---
+++ find-alt-triple.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+HOST_TARGET=$1
+
+[ "$HOST_TARGET" == "" ] && exit 1
+
+REST=`echo $HOST_TARGET   | sed -e 's/[a-zA-Z0-9_]*\(\-.*\)/\1/'`
+X86=`echo $HOST_TARGET    | sed -e 's/\(i[3-9]86\)\-.*/\1/'`
+X86_64=`echo $HOST_TARGET | sed -e 's/\(x86_64\)\-.*/\1/'`
+
+ALT=
+if [ "$X86_64" != "$HOST_TARGET" ]; then
+    ALT="i686$REST"
+
+elif [ "$X86" != "$HOST_TARGET" ]; then
+    ALT="x86_64$REST"
+fi
+
+echo $ALT
