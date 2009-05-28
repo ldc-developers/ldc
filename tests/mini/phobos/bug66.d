5d4fe64ac27811e5ded7aede20e8d8248f0319bd
Christian Kamm <kamm@incasoftware.de>
2009-05-28 22:00:08 +0200

    Get rid of old phobos-based tests.

--- tests/mini/phobos/bug66.d
+++
@@ -1,5 +0,0 @@
-module bug66;
-import std.stdio;
-class Scene { string name() { return "Scene"; } }
-class Group : Scene { this () { } }
-void main() { writefln((new Group).name); }
--- tests/mini/phobos/imports2.d
+++
@@ -1,6 +0,0 @@
-module imports2;
-import std.stdio;
-
-void main() {
-    writefln("Hello world!"[]);
-}
