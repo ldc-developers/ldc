cae60aa4063368bd4d092aafed3294946d1f772c
Leandro Lucarella <llucax@gmail.com>
2010-01-06 15:18:23 -0300

    Merge DMD r324: bugzilla 3663 and 3664 - fwd ref regressions ---  dmd/class.c  |    2 +-  dmd/enum.c   |    4 +++-  dmd/enum.h   |    2 ++  dmd/mars.c   |    2 +-  dmd/struct.c |    5 ++++-  5 files changed, 11 insertions(+), 4 deletions(-)

--- dmd/enum.c
+++ dmd/enum.c
@@ -61,7 +61,7 @@ void EnumDeclaration::semantic(Scope *sc)
 	memtype = Type::tint32;
 
     if (symtab)			// if already done
-    {	if (!scope)
+    {	if (isdone || !scope)
 	    return;		// semantic() already completed
     }
     else
@@ -95,6 +95,8 @@ void EnumDeclaration::semantic(Scope *sc)
     {	error("base type must be of integral type, not %s", memtype->toChars());
 	memtype = Type::tint32;
     }
+
+    isdone = 1;
 
     t = isAnonymous() ? memtype : type;
     symtab = new DsymbolTable();
--- dmd/struct.c
+++ dmd/struct.c
@@ -253,8 +253,11 @@ void StructDeclaration::semantic(Scope *sc)
 	return;
 
     if (symtab)
-    {   if (!scope)
+    {   if (sizeok == 1 || !scope)
+	{   //printf("already completed\n");
+	    scope = NULL;
             return;             // semantic() already completed
+	}
     }
     else
         symtab = new DsymbolTable();
