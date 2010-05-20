72f600e2ea9b9ef3610f5c20ea72d9a5571d001d
Moritz Warning <moritzwarning@web.de>
2010-05-20 20:01:40 +0200

    moved llvm-d bindings to dsource.org/projects/bindings/

--- tools/binding/prebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-g++ llvm-ext.cpp -c `llvm-config --cxxflags`
-g++ llvm-opt.cpp -c `llvm-config --cxxflags`
-g++ llvm-typemonitor.cpp -c `llvm-config --cxxflags`
-
-rm -f libllvm-c-ext.a
-ar rc libllvm-c-ext.a llvm-ext.o llvm-opt.o llvm-typemonitor.o
-ranlib libllvm-c-ext.a
