2b9dbffda42e59471c5a232a557954dceba812ca
alexrp <alex@lycus.org>
2012-07-08 03:08:42 +0200

    Add __jmp_buf for ARM and PPC32/64.

--- src/core/sys/posix/setjmp.d
+++ src/core/sys/posix/setjmp.d
@@ -61,6 +61,20 @@ version( linux )
     {
         alias int[3] __jmp_buf;
     }
+    else version (ARM)
+    {
+        alias int[64] __jmp_buf;
+    }
+    else version (PPC)
+    {
+        alias int[64 + 12 * 4] __jmp_buf;
+    }
+    else version (PPC64)
+    {
+        alias long[64] __jmp_buf;
+    }
+    else
+        static assert(false, "Unsupported architecture.");
 
     struct __jmp_buf_tag
     {
