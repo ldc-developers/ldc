ldc-developers/ldc

commit cae03a87ba0c067a791c9b3af80d049ba122bb93

Add version (Windows): to Windows headers.

src/core/sys/windows/windows.d
@@ -14,8 +14,9 @@
  */
 module core.sys.windows.windows;
 
-
-extern (Windows) nothrow:
+version (Windows):
+extern (Windows)
+nothrow:
 
     alias uint ULONG;
     alias ULONG *PULONG;
@@ -140,7 +141,7 @@ else // Win32
     alias DWORD   *LPCOLORREF;
     alias WORD    ATOM;
 
-version (0)
+version (none)
 {   // Properly prototyped versions
     alias BOOL function(HWND, UINT, WPARAM, LPARAM) DLGPROC;
     alias VOID function(HWND, UINT, UINT_PTR, DWORD) TIMERPROC;

src/core/sys/windows/dbghelp.d
@@ -14,9 +14,9 @@
  */
 module core.sys.windows.dbghelp;
 
+version (Windows):
 
 import core.sys.windows.windows;
-
 
 alias CHAR TCHAR;
 

commit 4f3bd5bd3eb34da6d585b47223d44a712ee6ac71

Fix a version statement broken in cae03a87ba0c067a791c9b3af80d049ba122bb93.

src/core/sys/windows/windows.d
@@ -141,7 +141,7 @@ else // Win32
     alias DWORD   *LPCOLORREF;
     alias WORD    ATOM;
 
-version (none)
+version (all)
 {   // Properly prototyped versions
     alias BOOL function(HWND, UINT, WPARAM, LPARAM) DLGPROC;
     alias VOID function(HWND, UINT, UINT_PTR, DWORD) TIMERPROC;