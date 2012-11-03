2f78dc686d1891aa737848bf3a7a5f95aecfad35
David Nadlinger <code@klickverbot.at>
2012-11-04 00:26:15 +0100

    Use llvm::PassManagerBuilder for optimization scheduling.
    This commit is based on Kai's pull request #157.

--- gen/optimizer.h
+++ gen/optimizer.h
@@ -12,12 +12,8 @@ namespace llvm { class Module; }
 
 bool ldc_optimize_module(llvm::Module* m);
 
-// Determines whether the inliner will run in the -O<N> list of passes
-bool doInline();
-// Determines whether the inliner will be run at all.
+// Returns whether the normal, full inlining pass will be run.
 bool willInline();
-
-int optLevel();
 
 bool optimize();
 
