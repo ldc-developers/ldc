8880645bf65e9c594bbcfd857a01782dc4b15a40
David Nadlinger <code@klickverbot.at>
2012-12-24 02:18:29 +0100

    Typo fix.
    Turns out I still had USE_METADATA=OFF in the CMake cache when
    testing the previous commit.

--- gen/passes/GarbageCollect2Stack.cpp
+++ gen/passes/GarbageCollect2Stack.cpp
@@ -46,7 +46,7 @@
 #include "llvm/ADT/Statistic.h"
 #include "llvm/Support/Compiler.h"
 #include "llvm/Support/Debug.h"
-#include "llvm/Support/MathExtra.h"
+#include "llvm/Support/MathExtras.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include <algorithm>
@@ -180,7 +180,7 @@ namespace {
             if (SizeLimit > 0)
             {
                 uint64_t ElemSize = A.TD.getTypeAllocSize(Ty);
-                unsigned BitsLimit = Log2_64(SizeLimit / ElemSize));
+                unsigned BitsLimit = Log2_64(SizeLimit / ElemSize);
 
                 // LLVM's alloca ueses an i32 for the number of elements.
                 BitsLimit = std::min(BitsLimit, 32U);
