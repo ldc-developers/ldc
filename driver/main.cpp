177b892bf0d2188266843d0808c3534b3934080e
David Nadlinger <code@klickverbot.at>
2013-10-05 22:41:58 +0200

    Cleanup: We don't support DMC.

--- driver/main.cpp
+++ driver/main.cpp
@@ -141,21 +141,10 @@ static void initFromString(char*& dest, const cl::opt<std::string>& src) {
     }
 }
 
-#if _WIN32 && __DMC__
-extern "C"
-{
-    extern int _xi_a;
-    extern int _end;
-}
-#endif
-
 int main(int argc, char** argv)
 {
     mem.init();                         // initialize storage allocator
     mem.setStackBottom(&argv);
-#if _WIN32 && __DMC__
-    mem.addroots((char *)&_xi_a, (char *)&_end);
-#endif
 
     // stack trace on signals
     llvm::sys::PrintStackTraceOnErrorSignal();
