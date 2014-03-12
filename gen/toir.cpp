7772b4ecbe8c746d7364573e08f631232d823c1b
kai <kai@redstar.de>
2014-03-12 17:10:49 +0100

    Add a failure ordering to cmpxchg in LLVM 3.5.
    For now, the D code is not changed. Failure ordering is the same as success ordering.

--- gen/toir.cpp
+++ gen/toir.cpp
@@ -1029,7 +1029,11 @@ DValue* CallExp::toElem(IRState* p)
             LLValue* ptr = exp1->toElem(p)->getRVal();
             LLValue* cmp = exp2->toElem(p)->getRVal();
             LLValue* val = exp3->toElem(p)->getRVal();
+#if LDC_LLVM_VER >= 305
+            LLValue* ret = gIR->ir->CreateAtomicCmpXchg(ptr, cmp, val, llvm::AtomicOrdering(atomicOrdering), llvm::AtomicOrdering(atomicOrdering));
+#else
             LLValue* ret = gIR->ir->CreateAtomicCmpXchg(ptr, cmp, val, llvm::AtomicOrdering(atomicOrdering));
+#endif
             return new DImValue(exp3->type, ret);
         // atomicrmw instruction
         } else if (fndecl->llvmInternal == LLVMatomic_rmw) {
