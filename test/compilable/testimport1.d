9rnsr/dmd

Fix import issues
- Add package table in each scope, due to check which package/module name is accessible via imports.
- Renamed/Selective imports no longer create aliases for the introduced names.
- Merge imports in base class members to derived class

test/compilable/testimport1.d
@@ -1,0 +1,41 @@
+// PERMUTE_ARGS:
+
+module testimport1;
+
+import imports.testimport1a;
+
+class C
+{
+    import imports.testimport1b;
+
+    void test()
+    {
+        imports.testimport1b.bar();
+        imports.testimport1a.foo();
+    }
+}
+
+int global;
+
+void main()
+{
+    // From here, 1a is visible but 1b isn't.
+    imports.testimport1a.foo();
+    static assert(!__traits(compiles, imports.testimport1b.bar()));
+
+    testimport1.C c;
+    auto y1 = testimport1.global;
+
+    // A declaration always hide same name root of package hierarchy.
+    {
+        int imports;
+        static assert(!__traits(compiles, imports.testimport1a.foo()));
+    }
+
+    // FQN access with Module Scope Operator works
+    .imports.testimport1a.foo();
+    auto y2 = .testimport1.global;
+
+    // FQN access through class is not allowed
+    static assert(!__traits(compiles, { C.imports.testimport1b.bar(); }));
+}

test/compilable/testimport2.d
@@ -1,0 +1,50 @@
+// REQUIRED_ARGS: -Icompilable/extra-files
+// PERMUTE_ARGS:
+// EXTRA_SOURCE: imports/testimport2a.d
+// EXTRA_SOURCE: imports/testimport2b.d
+// EXTRA_SOURCE: imports/testimport2c.d
+
+import imports.testimport2a;
+
+void main()
+{
+    // public symbols which directly imported are visible
+    foo();
+    imports.testimport2a.foo(); // by FQN
+    {
+        alias A = imports.testimport2a;
+        A.foo();
+        static assert(!__traits(compiles, A.bar()));
+        A.baz();
+    }
+
+    // public symbols through private import are invisible
+    static assert(!__traits(compiles, bar()));
+    static assert(!__traits(compiles, imports.testimport2b.bar()));
+    // FQN of privately imported module is invisible
+    static assert(!__traits(compiles, imports.testimport2b.stringof));
+    {
+        static assert(!__traits(compiles, { alias B = imports.testimport2b; }));
+    }
+
+    // public symbols which indirectly imported through public import are visible
+    baz();
+    imports.testimport2c.baz(); // by FQN
+    // FQN of publicly imported module is visible
+    static assert(imports.testimport2c.stringof == "module testimport2c");
+    {
+        alias C = imports.testimport2c;
+        static assert(!__traits(compiles, C.foo()));
+        static assert(!__traits(compiles, C.bar()));
+        C.baz();
+    }
+
+    // Import Declaration itself should not have FQN
+    static assert(!__traits(compiles, imports.testimport2a.imports.testimport2b.bar()));
+    static assert(!__traits(compiles, imports.testimport2a.imports.testimport2c.baz()));
+
+    // Applying Module Scope Operator to package/module FQN
+    .imports.testimport2a.foo();
+    static assert(!__traits(compiles, .imports.testimport2b.bar()));
+    .imports.testimport2c.baz();
+}

ldc-developers/ldc

test/compilable/testimport1.d
@@ -1,0 +1,41 @@
+// PERMUTE_ARGS:
+
+module testimport1;
+
+import imports.imp1a;
+
+class C
+{
+    import imports.imp1b;
+
+    void test()
+    {
+        imports.imp1b.bar();
+        imports.imp1a.foo();
+    }
+}
+
+int global;
+
+void main()
+{
+    // From here, 1a is visible but 1b isn't.
+    imports.imp1a.foo();
+    static assert(!__traits(compiles, imports.imp1b.bar()));
+
+    testimport1.C c;
+    auto y1 = testimport1.global;
+
+    // A declaration always hide same name root of package hierarchy.
+    {
+        int imports;
+        static assert(!__traits(compiles, imports.imp1a.foo()));
+    }
+
+    // FQN access with Module Scope Operator works
+    .imports.imp1a.foo();
+    auto y2 = .testimport1.global;
+
+    // FQN access through class is not allowed
+    static assert(!__traits(compiles, { C.imports.imp1b.bar(); }));
+}

test/compilable/testimport2.d
@@ -1,0 +1,50 @@
+// REQUIRED_ARGS: -Icompilable/extra-files
+// PERMUTE_ARGS:
+// EXTRA_SOURCE: imports/imp2a.d
+// EXTRA_SOURCE: imports/imp2b.d
+// EXTRA_SOURCE: imports/imp2c.d
+
+import imports.imp2a;
+
+void main()
+{
+    // public symbols which directly imported are visible
+    foo();
+    imports.imp2a.foo(); // by FQN
+    {
+        alias A = imports.imp2a;
+        A.foo();
+        static assert(!__traits(compiles, A.bar()));
+        A.baz();
+    }
+
+    // public symbols through private import are invisible
+    static assert(!__traits(compiles, bar()));
+    static assert(!__traits(compiles, imports.imp2b.bar()));
+    // FQN of privately imported module is invisible
+    static assert(!__traits(compiles, imports.imp2b.stringof));
+    {
+        static assert(!__traits(compiles, { alias B = imports.imp2b; }));
+    }
+
+    // public symbols which indirectly imported through public import are visible
+    baz();
+    imports.imp2c.baz(); // by FQN
+    // FQN of publicly imported module is visible
+    static assert(imports.imp2c.stringof == "module imp2c");
+    {
+        alias C = imports.imp2c;
+        static assert(!__traits(compiles, C.foo()));
+        static assert(!__traits(compiles, C.bar()));
+        C.baz();
+    }
+
+    // Import Declaration itself should not have FQN
+    static assert(!__traits(compiles, imports.imp2a.imports.imp2b.bar()));
+    static assert(!__traits(compiles, imports.imp2a.imports.imp2c.baz()));
+
+    // Applying Module Scope Operator to package/module FQN
+    .imports.imp2a.foo();
+    static assert(!__traits(compiles, .imports.imp2b.bar()));
+    .imports.imp2c.baz();
+}