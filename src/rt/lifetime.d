7c83ebb8180c61d91926c7adcd2604aabf14c282
David Nadlinger <code@klickverbot.at>
2014-07-12 01:44:45 +0200

    Revert vararg changes in rt.lifetime.
    This reverts commits 2085da81 and fc7b005c, as they were
    specific to the "simplified" vararg ABI reverted in the
    previous commit. Ideally, the code would require no version
    statements at all, and indeed the va_copy should be in the
    loop for the program to be correct without making the
    assumption that va_list is passed by copy. But as long as
    DMD still requires __va_argsave, there is probably little
    point in fixing this upstream.

--- src/rt/lifetime.d
+++ src/rt/lifetime.d
@@ -1025,28 +1025,43 @@ void[] _d_newarrayOpT(alias op)(const TypeInfo ti, size_t ndims, va_list q)
                     va_list ap2;
                     va_copy(ap2, ap);
                 }
+                else version(PPC)
+                {
+                    va_list ap2;
+                    va_copy(ap2, ap);
+                }
+                else version(PPC64)
+                {
+                    va_list ap2;
+                    va_copy(ap2, ap);
+                }
                 else version(Win64)
                 {
                     va_list ap2;
                     va_copy(ap2, ap);
                 }
-                else version(LDC)
+                else version(ARM)
                 {
                     va_list ap2;
                     va_copy(ap2, ap);
                 }
                 for (size_t i = 0; i < dim; i++)
                 {
-                    version(LDC)
-                    {
-                    }
-                    else version (Win64)
+                    version (Win64)
                     {
                     }
                     else version(X86_64)
                     {
-                        __va_list argsave = *cast(__va_list*)ap;
-                        va_list ap2 = &argsave;
+                        version(LDC)
+                        {
+                            va_list ap2;
+                            va_copy(ap2, ap);
+                        }
+                        else
+                        {
+                            __va_list argsave = *cast(__va_list*)ap;
+                            va_list ap2 = &argsave;
+                        }
                     }
                     (cast(void[]*)p.ptr)[i] = foo(ti.next, ap2, ndims - 1);
                 }
