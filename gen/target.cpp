c9f72ff8238310dc5ebd6725c4372d4d4ce52d14
kai <kai@redstar.de>
2014-07-19 15:35:52 +0200

    Remove magic constants from Target::critsecsize().
    The type of a critical section is returned by DtoMutexType() and can be used to derive the size.
    This is required for correct cross-compilation.

--- gen/target.cpp
+++ gen/target.cpp
@@ -9,15 +9,10 @@
 
 #include "target.h"
 #include "gen/irstate.h"
+#include "gen/tollvm.h"
 #include "mars.h"
 #include "mtype.h"
 #include <assert.h>
-
-#if defined(_MSC_VER)
-#include <windows.h>
-#else
-#include <pthread.h>
-#endif
 
 int Target::ptrsize;
 int Target::realsize;
@@ -58,18 +53,7 @@ unsigned Target::fieldalign (Type* type)
     return type->alignsize();
 }
 
-// sizes based on those from tollvm.cpp:DtoMutexType()
 unsigned Target::critsecsize()
 {
-#if defined(_MSC_VER)
-    // Return sizeof(RTL_CRITICAL_SECTION)
-    return global.params.is64bit ? 40 : 24;
-#else
-    if (global.params.targetTriple.isOSWindows())
-        return global.params.is64bit ? 40 : 24;
-    else if (global.params.targetTriple.getOS() == llvm::Triple::FreeBSD)
-        return sizeof(size_t);
-    else
-        return sizeof(pthread_mutex_t);
-#endif
+    return getTypeStoreSize(DtoMutexType());
 }
