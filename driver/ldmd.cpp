ldc-developers/ldc

commit 0d168ed570189effb51e22394a63027a8390b753

Fix LLVM 3.5 build.
llvm::errc::success was removed during transition to std::errc.

driver/ldmd.cpp
@@ -989,7 +989,7 @@ std::string locateBinary(std::string exeName, const char* argv0)
  */
 static void createOutputDir(const char* dir) {
 #if LDC_LLVM_VER >= 305
-    if (ls::fs::create_directories(dir) != llvm::errc::success)
+    if (!ls::fs::create_directories(dir))
 #else
     bool dirExisted; // ignored
     if (ls::fs::create_directories(dir, dirExisted) != llvm::errc::success)
@@ -1046,8 +1046,7 @@ int main(int argc, char *argv[])
     {
         int rspFd;
         llvm::SmallString<128> rspPath;
-        if (ls::fs::createUniqueFile("ldmd-%%-%%-%%-%%.rsp", rspFd, rspPath) !=
-            llvm::errc::success)
+        if (!ls::fs::createUniqueFile("ldmd-%%-%%-%%-%%.rsp", rspFd, rspPath))
         {
             error("Could not open temporary response file.");
         }
@@ -1072,7 +1071,7 @@ int main(int argc, char *argv[])
         int rc = execute(ldcPath, &newArgs[0]);
 
 #if LDC_LLVM_VER >= 305
-        if (ls::fs::remove(rspPath.str()) != llvm::errc::success)
+        if (!ls::fs::remove(rspPath.str()))
 #else
         bool couldRemove;
         if (ls::fs::remove(rspPath.str(), couldRemove) != llvm::errc::success ||

commit 7524bff42b8189963f734c6d988514314e9f3e6c

fix LLVM 3.5 regression introduced by 0d168ed

driver/ldmd.cpp
@@ -989,7 +989,7 @@ std::string locateBinary(std::string exeName, const char* argv0)
  */
 static void createOutputDir(const char* dir) {
 #if LDC_LLVM_VER >= 305
-    if (!ls::fs::create_directories(dir))
+    if (ls::fs::create_directories(dir))
 #else
     bool dirExisted; // ignored
     if (ls::fs::create_directories(dir, dirExisted) != llvm::errc::success)
@@ -1046,7 +1046,7 @@ int main(int argc, char *argv[])
     {
         int rspFd;
         llvm::SmallString<128> rspPath;
-        if (!ls::fs::createUniqueFile("ldmd-%%-%%-%%-%%.rsp", rspFd, rspPath))
+        if (ls::fs::createUniqueFile("ldmd-%%-%%-%%-%%.rsp", rspFd, rspPath))
         {
             error("Could not open temporary response file.");
         }
@@ -1071,7 +1071,7 @@ int main(int argc, char *argv[])
         int rc = execute(ldcPath, &newArgs[0]);
 
 #if LDC_LLVM_VER >= 305
-        if (!ls::fs::remove(rspPath.str()))
+        if (ls::fs::remove(rspPath.str()))
 #else
         bool couldRemove;
         if (ls::fs::remove(rspPath.str(), couldRemove) != llvm::errc::success ||