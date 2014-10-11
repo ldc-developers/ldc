ldc-developers/ldc

commit caa2f15c8a0f44aa0caf8efb9b0dfb40cbaafc74

Remove VarDeclaration::aggrIndex

gen/classes.cpp
@@ -60,11 +60,11 @@ void DtoResolveClass(ClassDeclaration* cd)
                                    I != E; ++I)
     {
         VarDeclaration* vd = *I;
-
-        if (!isIrFieldCreated(vd))
-            getIrField(vd, true);
-        else
-            IF_LOG Logger::println("class field already exists!!!");
+        IF_LOG {
+            if (isIrFieldCreated(vd))
+                Logger::println("class field already exists");
+        }
+        getIrField(vd, true);
     }
 
     // emit the interfaceInfosZ symbol if necessary

ir/irtypeclass.cpp
@@ -177,8 +177,8 @@ void IrTypeClass::addBaseClassData(
         // advance offset to right past this field
         offset = vd->offset + vd->type->size();
 
-        // create ir field
-        vd->aggrIndex = static_cast<unsigned>(field_index);
+        // set the field index
+        getIrField(vd, true)->setAggrIndex(static_cast<unsigned>(field_index));
         ++field_index;
     }
 

ir/irtypestruct.cpp
@@ -241,7 +241,8 @@ IrTypeStruct* IrTypeStruct::get(StructDeclaration* sd)
         offset = vd->offset + vd->type->size();
 
         // set the field index
-        vd->aggrIndex = (unsigned)field_index++;
+        getIrField(vd, true)->setAggrIndex(static_cast<unsigned>(field_index));
+        ++field_index;
     }
 
     // tail padding?

ir/irvar.cpp
@@ -19,16 +19,14 @@
 
 IrField::IrField(VarDeclaration* v) : IrVar(v)
 {
-    if (v->aggrIndex)
-    {
-        index = v->aggrIndex;
-        unionOffset = 0;
-    }
-    else
-    {
-        index = 0;
-        unionOffset = v->offset;
-    }
+    index = 0;
+    unionOffset = V->offset;
+}
+
+void IrField::setAggrIndex(unsigned aggrIndex)
+{
+    index = aggrIndex;
+    unionOffset = 0;
 }
 
 extern LLConstant* get_default_initializer(VarDeclaration* vd, Initializer* init);

ir/irvar.h
@@ -80,6 +80,7 @@ struct IrParameter : IrLocal
 struct IrField : IrVar
 {
     IrField(VarDeclaration* v);
+    void setAggrIndex(unsigned aggrIndex);
 
     unsigned index;
     unsigned unionOffset;

dmd2/declaration.c
@@ -946,8 +946,6 @@ VarDeclaration::VarDeclaration(Loc loc, Type *type, Identifier *id, Initializer 
     edtor = NULL;
     range = NULL;
 #if IN_LLVM
-    aggrIndex = 0;
-
     nakedUse = false;
 #endif
 }

dmd2/declaration.h
@@ -348,10 +348,6 @@ public:
     void accept(Visitor *v) { v->visit(this); }
 
 #if IN_LLVM
-    /// Index into parent aggregate.
-    /// Set during type generation.
-    unsigned aggrIndex;
-
     /// This var is used by a naked function.
     bool nakedUse;
 

gen/structs.cpp
@@ -61,7 +61,10 @@ void DtoResolveStruct(StructDeclaration* sd, Loc& callerLoc)
                                    I != E; ++I)
     {
         VarDeclaration *vd = *I;
-        assert(!isIrFieldCreated(vd));
+        IF_LOG {
+            if (isIrFieldCreated(vd))
+                Logger::println("struct field already exists");
+        }
         getIrField(vd, true);
     }
 }

commit bff766b379506b32359ac04e1dd4930a5dc0689d

Track member field index in IrTypeAggr, not IrField.
This fixes a regression introduced in caa2f15c8a. The IrDsymbol
metadata is obviously reset in between modules, while IrTypes
are not. Thus, we can never set symbol metadata when resolving
types, as the symbol data will be wrong in all modules following
the one where the type was first resolved.

GitHub: Fixes #739.

gen/classes.cpp
@@ -118,7 +118,7 @@ DValue* DtoNewClass(Loc& loc, TypeClass* tc, NewExp* newexp)
         Logger::println("Resolving outer class");
         LOG_SCOPE;
         DValue* thisval = toElem(newexp->thisexp);
-        size_t idx = getIrField(tc->sym->vthis)->index;
+        unsigned idx = getFieldGEPIndex(tc->sym, tc->sym->vthis);
         LLValue* src = thisval->getRVal();
         LLValue* dst = DtoGEPi(mem, 0, idx);
         IF_LOG Logger::cout() << "dst: " << *dst << "\nsrc: " << *src << '\n';

gen/llvmhelpers.cpp
@@ -30,6 +30,7 @@
 #include "gen/typeinf.h"
 #include "gen/abi.h"
 #include "ir/irmodule.h"
+#include "ir/irtypeaggr.h"
 #include "llvm/MC/MCAsmInfo.h"
 #include "llvm/Target/TargetMachine.h"
 #include "llvm/Transforms/Utils/ModuleUtils.h"
@@ -1937,6 +1938,11 @@ LLValue* DtoIndexAggregate(LLValue* src, AggregateDeclaration* ad, VarDeclaratio
     IF_LOG Logger::println("Indexing aggregate field %s:", vd->toPrettyChars());
     LOG_SCOPE;
 
+    // Make sure the aggregate is resolved, as subsequent code might expect
+    // isIrVarCreated(vd). This is a bit of a hack, we don't actually need this
+    // ourselves, DtoType below would be enough.
+    DtoResolveDsymbol(ad);
+
     // Cast the pointer we got to the canonical struct type the indices are
     // based on.
     LLType* st = DtoType(ad->type);
@@ -1944,23 +1950,36 @@ LLValue* DtoIndexAggregate(LLValue* src, AggregateDeclaration* ad, VarDeclaratio
         st = getPtrToType(st);
     src = DtoBitCast(src, st);
 
-    // gep to the index
-    DtoResolveDsymbol(ad);
-    IrField* field = getIrField(vd);
-    LLValue* val = DtoGEPi(src, 0, field->index);
+    // Look up field to index and any offset to apply.
+    unsigned fieldIndex;
+    unsigned byteOffset;
+    assert(ad->type->ctype->isAggr());
+    static_cast<IrTypeAggr*>(ad->type->ctype)->getMemberLocation(
+        vd, fieldIndex, byteOffset);
 
-    // do we need to offset further? (union area)
-    if (field->unionOffset)
+    LLValue* val = DtoGEPi(src, 0, fieldIndex);
+
+    if (byteOffset)
     {
-        // cast to void*
+        // Cast to void* to apply byte-wise offset.
         val = DtoBitCast(val, getVoidPtrType());
-        // offset
-        val = DtoGEPi1(val, field->unionOffset);
+        val = DtoGEPi1(val, byteOffset);
     }
 
-    // cast it to the right type
+    // Cast the (possibly void*) pointer to the canonical variable type.
     val = DtoBitCast(val, getPtrToType(i1ToI8(DtoType(vd->type))));
 
     IF_LOG Logger::cout() << "Value: " << *val << '\n';
     return val;
+}
+
+unsigned getFieldGEPIndex(AggregateDeclaration* ad, VarDeclaration* vd)
+{
+    unsigned fieldIndex;
+    unsigned byteOffset;
+    assert(ad->type->ctype->isAggr());
+    static_cast<IrTypeAggr*>(ad->type->ctype)->getMemberLocation(
+        vd, fieldIndex, byteOffset);
+    assert(byteOffset == 0 && "Cannot address field by a simple GEP.");
+    return fieldIndex;
 }

gen/llvmhelpers.h
@@ -138,6 +138,13 @@ bool hasUnalignedFields(Type* t);
 /// 'src' is a pointer to the start of the memory of an 'ad' instance.
 LLValue* DtoIndexAggregate(LLValue* src, AggregateDeclaration* ad, VarDeclaration* vd);
 
+/// Returns the index of a given member variable in the resulting LLVM type of
+/// an aggregate.
+///
+/// This is only a valid operation if the field is known to be non-overlapping,
+/// so that no byte-wise offset is needed.
+unsigned getFieldGEPIndex(AggregateDeclaration* ad, VarDeclaration* vd);
+
 ///
 DValue* DtoInlineAsmExpr(Loc& loc, FuncDeclaration* fd, Expressions* arguments);
 

gen/nested.cpp
@@ -40,6 +40,11 @@ static void storeVariable(VarDeclaration *vd, LLValue *dst)
     DtoAlignedStore(value, dst);
 }
 
+static unsigned getVthisIdx(AggregateDeclaration* ad)
+{
+    return getFieldGEPIndex(ad, ad->vthis);
+}
+
 static void DtoCreateNestedContextType(FuncDeclaration* fd);
 
 DValue* DtoNestedVariable(Loc& loc, Type* astype, VarDeclaration* vd, bool byref)
@@ -89,7 +94,7 @@ DValue* DtoNestedVariable(Loc& loc, Type* astype, VarDeclaration* vd, bool byref
         LLValue* val = irfunc->thisArg;
         if (cd->isClassDeclaration())
             val = DtoLoad(val);
-        ctx = DtoLoad(DtoGEPi(val, 0, getIrField(cd->vthis)->index, ".vthis"));
+        ctx = DtoLoad(DtoGEPi(val, 0, getVthisIdx(cd), ".vthis"));
     } else {
         // Otherwise, this is a simple nested function, load from the context
         // argument.
@@ -183,7 +188,7 @@ void DtoResolveNestedContext(Loc& loc, AggregateDeclaration *decl, LLValue *valu
         // our codegen order.
         DtoResolveDsymbol(decl);
 
-        size_t idx = getIrField(decl->vthis)->index;
+        unsigned idx = getVthisIdx(decl);
         LLValue* gep = DtoGEPi(value,0,idx,".vthis");
         DtoStore(DtoBitCast(nest, gep->getType()->getContainedType(0)), gep);
     }
@@ -221,7 +226,7 @@ LLValue* DtoNestedContext(Loc& loc, Dsymbol* sym)
             // function (but without any variables in the nested context).
             return val;
         }
-        val = DtoLoad(DtoGEPi(val, 0, getIrField(ad->vthis)->index, ".vthis"));
+        val = DtoLoad(DtoGEPi(val, 0, getVthisIdx(ad), ".vthis"));
     }
     else
     {
@@ -431,9 +436,9 @@ void DtoCreateNestedContext(FuncDeclaration* fd) {
                 assert(cd->vthis);
                 Logger::println("Indexing to 'this'");
                 if (cd->isStructDeclaration())
-                    src = DtoExtractValue(thisval, getIrField(cd->vthis)->index, ".vthis");
+                    src = DtoExtractValue(thisval, getVthisIdx(cd), ".vthis");
                 else
-                    src = DtoLoad(DtoGEPi(thisval, 0, getIrField(cd->vthis)->index, ".vthis"));
+                    src = DtoLoad(DtoGEPi(thisval, 0, getVthisIdx(cd), ".vthis"));
             } else {
                 src = DtoLoad(src);
             }

gen/toir.cpp
@@ -2591,7 +2591,7 @@ public:
                     cval = getNullPtr(getVoidPtrType());
                 } else {
                     cval = ad->isClassDeclaration() ? DtoLoad(irfn->thisArg) : irfn->thisArg;
-                    cval = DtoLoad(DtoGEPi(cval, 0, getIrField(ad->vthis)->index, ".vthis"));
+                    cval = DtoLoad(DtoGEPi(cval, 0, getFieldGEPIndex(ad, ad->vthis), ".vthis"));
                 }
             }
             else

ir/irtypeaggr.cpp
@@ -27,3 +27,23 @@ IrTypeAggr::IrTypeAggr(AggregateDeclaration * ad)
     diCompositeType(NULL), aggr(ad)
 {
 }
+
+void IrTypeAggr::getMemberLocation(VarDeclaration* var, unsigned& fieldIndex,
+    unsigned& byteOffset) const
+{
+    // Note: The interface is a bit more general than what we actually return.
+    // Specifically, the frontend offset information we use for overlapping
+    // fields is always based at the object start.
+    std::map<VarDeclaration*, unsigned>::const_iterator it =
+        varGEPIndices.find(var);
+    if (it != varGEPIndices.end())
+    {
+        fieldIndex = it->second;
+        byteOffset = 0;
+    }
+    else
+    {
+        fieldIndex = 0;
+        byteOffset = var->offset;
+    }
+}

ir/irtypeaggr.h
@@ -37,6 +37,12 @@ public:
     ///
     IrTypeAggr* isAggr()            { return this; }
 
+    /// Returns the index of the field in the LLVM struct type that corresponds
+    /// to the given member variable, plus the offset to the actual field start
+    /// due to overlapping (union) fields, if any.
+    void getMemberLocation(VarDeclaration* var, unsigned& fieldIndex,
+        unsigned& byteOffset) const;
+
     /// Composite type debug description. This is not only to cache, but also
     /// used for resolving forward references.
     llvm::DIType diCompositeType;
@@ -47,6 +53,16 @@ protected:
 
     /// AggregateDeclaration this type represents.
     AggregateDeclaration* aggr;
+
+    /// Stores the mapping from member variables to field indices in the actual
+    /// LLVM type. If a member variable is not present, this means that it does
+    /// not resolve to a "clean" GEP but extra offsetting due to overlapping
+    /// members is needed (i.e., a union).
+    ///
+    /// We need to keep track of this separately, because there is no way to get
+    /// the field index of a variable in the frontend, it only stores the byte
+    /// offset.
+    std::map<VarDeclaration*, unsigned> varGEPIndices;
 };
 
 #endif

ir/irtypeclass.cpp
@@ -173,7 +173,7 @@ void IrTypeClass::addBaseClassData(
         offset = vd->offset + vd->type->size();
 
         // set the field index
-        getIrField(vd, true)->setAggrIndex(static_cast<unsigned>(field_index));
+        varGEPIndices[vd] = field_index;
         ++field_index;
     }
 

ir/irtypestruct.cpp
@@ -237,7 +237,7 @@ IrTypeStruct* IrTypeStruct::get(StructDeclaration* sd)
         offset = vd->offset + vd->type->size();
 
         // set the field index
-        getIrField(vd, true)->setAggrIndex(static_cast<unsigned>(field_index));
+        t->varGEPIndices[vd] = field_index;
         ++field_index;
     }
 

ir/irvar.cpp
@@ -17,22 +17,6 @@
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 
-IrField::IrField(VarDeclaration* v) : IrVar(v)
-{
-    index = 0;
-    unionOffset = V->offset;
-}
-
-void IrField::setAggrIndex(unsigned aggrIndex)
-{
-    index = aggrIndex;
-    unionOffset = 0;
-}
-
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-
 IrVar *getIrVar(VarDeclaration *decl)
 {
     assert(isIrVarCreated(decl));

ir/irvar.h
@@ -94,11 +94,7 @@ struct IrParameter : IrLocal
 // represents an aggregate field variable
 struct IrField : IrVar
 {
-    IrField(VarDeclaration* v);
-    void setAggrIndex(unsigned aggrIndex);
-
-    unsigned index;
-    unsigned unionOffset;
+    IrField(VarDeclaration* v) : IrVar(v) {};
 };
 
 IrVar *getIrVar(VarDeclaration *decl);