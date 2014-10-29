ldc-developers/ldc

commit 66610543995246cc5d552c83726aed0fbd2c12f2

Linux/PPC64: Statically define ELFv1 for Big Endian and ELFv2 for Little Endian Systems.
This is in preparation for the missing `--with-abi=` command line switch.
ELFv1 and ELFv2 are reserved version identifiers starting with DMD 2.067.

driver/main.cpp
@@ -584,6 +584,9 @@ static void registerPredefinedTargetVersions() {
 #endif
             VersionCondition::addPredefinedGlobalIdent("PPC64");
             registerPredefinedFloatABI("PPC_SoftFloat", "PPC_HardFloat");
+            if (global.params.targetTriple.isOSLinux())
+                VersionCondition::addPredefinedGlobalIdent(global.params.targetTriple.getArch() == llvm::Triple::ppc64
+                                                       ? "ELFv1" : "ELFv2");
             break;
         case llvm::Triple::arm:
 #if LDC_LLVM_VER >= 305

commit 1e8fcfd5d9b951b66be4a3d82db45b09a3b924de

Fix compiler error introduced with 66610543995246cc5d552c83726aed0fbd2c12f2.

driver/main.cpp
@@ -584,7 +584,7 @@ static void registerPredefinedTargetVersions() {
 #endif
             VersionCondition::addPredefinedGlobalIdent("PPC64");
             registerPredefinedFloatABI("PPC_SoftFloat", "PPC_HardFloat");
-            if (global.params.targetTriple.isOSLinux())
+            if (global.params.targetTriple.getOS() == llvm::Triple::Linux)
                 VersionCondition::addPredefinedGlobalIdent(global.params.targetTriple.getArch() == llvm::Triple::ppc64
                                                        ? "ELFv1" : "ELFv2");
             break;