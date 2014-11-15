cbde70318c6cc3cf382798831f1a843414ccd120
kai <kai@redstar.de>
2014-11-15 19:06:15 +0100

    Remove FIXME in template.c

--- dmd2/template.c
+++ dmd2/template.c
@@ -7719,32 +7719,10 @@ Identifier *TemplateInstance::genIdent(Objects *args)
             ea = ea->ctfeInterpret();
             if (ea->op == TOKerror || olderr != global.errors)
                 continue;
-#if !IN_LLVM
+
             /* Use deco that matches what it would be for a function parameter
              */
             buf.writestring(ea->type->deco);
-#else
-            // This is a workaround for DMD bug 7469 (https://issues.dlang.org/show_bug.cgi?id=7469)
-            // FIXME: The bug is resolved in DMD 2.066. Remove in 0.15.0
-
-            // The declared parameter list might be shorter because of variadic arguments
-            if (((TemplateDeclaration*)tempdecl)->parameters->dim > i)
-            {
-                // Use type of parameter, not type of argument
-                TemplateParameter *tp = (*((TemplateDeclaration*)tempdecl)->parameters)[i];
-                assert(tp);
-                TemplateValueParameter *tvp = tp->isTemplateValueParameter();
-                if (tvp && tvp->valType->deco)
-                    buf.writestring(tvp->valType->deco);
-                else
-                    goto Lusetypeofvalue;
-            }
-            else
-            {
-              Lusetypeofvalue:
-                buf.writestring(ea->type->deco);
-            }
-#endif
             ea->toMangleBuffer(&buf);
         }
         else if (sa)
