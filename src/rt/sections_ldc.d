932bc606641ce4628b10a482f9665ebe86d1e825
kai <kai@redstar.de>
2014-11-29 20:18:39 +0100

    Add section support for Solaris.
    Solaris requires special handling because the dl_phdr_info.dlpi_tls_modid field
    is missing.
    Improve ELF support for FreeBSD.

--- src/rt/sections_ldc.d
+++ src/rt/sections_ldc.d
@@ -20,6 +20,7 @@ version (linux) {} else version(LDC):
 
 import core.stdc.stdlib : alloca;
 import rt.minfo;
+debug(PRINTF) import core.stdc.stdio : printf;
 
 version (FreeBSD)
 {
@@ -28,7 +29,15 @@ version (FreeBSD)
     import core.sys.freebsd.sys.elf;
     import core.sys.freebsd.sys.link_elf;
 }
+version (Solaris)
+{
+    version = UseELF;
 
+    import core.sys.solaris.link;
+    import core.sys.solaris.sys.elf;
+}
+
+alias SectionGroup DSO;
 struct SectionGroup
 {
     static int opApply(scope int delegate(ref SectionGroup) dg)
@@ -61,6 +70,16 @@ private:
 
     import rt.util.container.array;
     Array!(void[]) _gcRanges;
+
+    version(Solaris)
+    {
+        size_t _tlsSize;
+    }
+    else version(UseELF)
+    {
+        size_t _tlsMod;
+        size_t _tlsSize;
+    }
 }
 private __gshared SectionGroup globalSectionGroup;
 
@@ -104,38 +123,41 @@ private
     }
     else version (UseELF)
     {
-        nothrow
-        void findDataSection(void[]* data)
-        {
-            static extern(C) nothrow
-            int callback(dl_phdr_info* info, size_t sz, void* arg)
-            {
-                auto range = cast(void[]*) arg;
-                foreach (i, ref phdr; info.dlpi_phdr[0 .. info.dlpi_phnum])
-                {
-                    if (phdr.p_type == PT_LOAD && phdr.p_flags == (PF_W|PF_R))
-                    {
-                        *range = (cast(void*)phdr.p_vaddr)[0 .. phdr.p_memsz];
-                        return 1;
-                    }
-                }
-                return 0;
-            }
-
-            dl_iterate_phdr(&callback, data);
-        }
-
-        struct TLSInfo { size_t moduleId, size; }
-        TLSInfo getTLSInfo(in ref dl_phdr_info info)
+        /************
+         * Scan segments in Linux dl_phdr_info struct and store
+         * the TLS and writeable data segments in *pdso.
+         */
+        void scanSegments(in ref dl_phdr_info info, DSO* pdso)
         {
             foreach (ref phdr; info.dlpi_phdr[0 .. info.dlpi_phnum])
             {
-                if (phdr.p_type == PT_TLS)
+                switch (phdr.p_type)
                 {
-                    return TLSInfo(info.dlpi_tls_modid, phdr.p_memsz);
+                case PT_LOAD:
+                    if (phdr.p_flags & PF_W) // writeable data segment
+                    {
+                        auto beg = cast(void*)(info.dlpi_addr + phdr.p_vaddr);
+                        pdso._gcRanges.insertBack(beg[0 .. phdr.p_memsz]);
+                    }
+                    break;
+
+                case PT_TLS: // TLS segment
+                    assert(!pdso._tlsSize); // is unique per DSO
+                    version(Solaris)
+                    {
+                        pdso._tlsSize = phdr.p_memsz;
+                    }
+                    else
+                    {
+                        pdso._tlsMod = info.dlpi_tls_modid;
+                        pdso._tlsSize = phdr.p_memsz;
+                    }
+                    break;
+
+                default:
+                    break;
                 }
             }
-            assert(0, "Failed to determine TLS size.");
         }
 
         nothrow
@@ -177,33 +199,155 @@ private
             return false;
         }
 
-        struct tls_index
+        version(Solaris)
         {
-            size_t ti_module;
-            size_t ti_offset;
-        }
+            /* Solaris does not support the dl_phdr_info.dlpi_tls_modid field.
+             * The static TLS range is placed immediately preceding the thread
+             * pointer. Accesses to this TLS data is based off of subtractions
+             * from the current thread pointer.
+             * See: https://docs.oracle.com/cd/E26502_01/html/E26507/gentextid-23191.html#chapter8-7
+             */
 
-        extern(C) void* __tls_get_addr(tls_index* ti);
+            /*
+             * The following data structures are private to libc. They are not
+             * required for the implementation but they help in debugging this
+             * stuff.
+             */
+            struct mutex_t
+            {
+                ubyte[24] __fill;
+            }
 
-        void[] getTLSRange(TLSInfo info)
-        {
-            if (info.moduleId == 0) return null;
-            auto ti = tls_index(info.moduleId, 0);
-            return __tls_get_addr(&ti)[0 .. info.size];
+            struct tls_t
+            {
+                void*  tls_data;
+                size_t tls_size;
+            }
+
+            struct tls_metadata_t
+            {
+                mutex_t                                      tls_lock;
+                tls_t                                        tls_modinfo;
+                tls_t                                        static_tls;
+                byte[64 - mutex_t.sizeof -2 * tls_t.sizeof]  tls_pad;
+            }
+
+            struct uberdata_t
+            {
+                byte[10496] __fill;
+                tls_metadata_t tls_metadata;
+                /* incomplete */
+            }
+
+            struct ulwp_t
+            {
+                version(SPARC)
+                {
+                    uint     ul_dinstr;
+                    uint[15] ul_padsparc0;
+                    uint     ul_dsave;
+                    uint     ul_drestore;
+                    uint     ul_dftret;
+                    uint     ul_dreturn;
+                }
+                version(SPARC64)
+                {
+                    uint     ul_dinstr;
+                    uint[15] ul_padsparc0;
+                    uint     ul_dsave;
+                    uint     ul_drestore;
+                    uint     ul_dftret;
+                    uint     ul_dreturn;
+                }
+                ulwp_t* ul_self;
+                version (D_LP64)
+                    ubyte[56] ul_dinstr;
+                else
+                    ubyte[40] ul_dinstr;
+                uberdata_t*  ul_uberdata;
+                tls_t        ul_tls;
+                ulwp_t*      ul_forw;
+                ulwp_t*      ul_back;
+                ulwp_t*      ul_next;
+                ulwp_t*      ul_hash;
+                void*        ul_rval;
+                /* incomplete */
+            }
+
+            // Return the current thread pointer.
+            private ulwp_t* curthread() nothrow
+            {
+                import ldc.llvmasm;
+
+                version (X86_64)
+                {
+                    return __asm!(ulwp_t*)("movq %fs:0, $0", "=r");
+                }
+                else version (X86)
+                {
+                    return __asm!(ulwp_t*)("movl %gs:0, $0", "=r");
+                }
+                else
+                {
+                     static assert(0, "TLS range detection not implemented on Solaris for this architecture.");
+                }
+            }
+
+            // See: http://src.illumos.org/source/xref/illumos-gate/usr/src/cmd/sgs/include/i386/machdep_x86.h#102
+            version (D_LP64)
+                enum M_TLSSTATALIGN = 0x10;
+            else
+                enum M_TLSSTATALIGN = 0x08;
+
+            void[] getTLSRange(DSO* pdso)
+            {
+                // See: http://src.illumos.org/source/xref/illumos-gate/usr/src/cmd/sgs/libld/common/machrel.intel.c#996
+                //     tlsstatsize = S_ROUND(ofl->ofl_tlsphdr->p_memsz, M_TLSSTATALIGN);
+                void* thptr = curthread();
+                size_t sz = (pdso._tlsSize + (M_TLSSTATALIGN-1) + 512) & ~(M_TLSSTATALIGN-1);
+                return (thptr - sz)[0 .. sz];
+            }
         }
-    }
-    else version (Solaris)
-    {
-        extern extern(C) __gshared
+        else
         {
-            int _environ;
-            int _end;
+            struct tls_index
+            {
+                size_t ti_module;
+                size_t ti_offset;
+            }
+
+            extern(C) void* __tls_get_addr(tls_index* ti);
+
+            /* The dynamic thread vector (DTV) pointers may point 0x8000 past the start of
+             * each TLS block. This is at least true for PowerPC and Mips platforms.
+             */
+            version(PPC)
+                enum TLS_DTV_OFFSET = 0x8000;
+            else version(PPC64)
+                enum TLS_DTV_OFFSET = 0x8000;
+            else version(MIPS)
+                enum TLS_DTV_OFFSET = 0x8000;
+            else version(MIPS64)
+                enum TLS_DTV_OFFSET = 0x8000;
+            else
+                enum TLS_DTV_OFFSET = 0x;
+
+            void[] getTLSRange(DSO* pdso)
+            {
+                if (pdso._tlsMod == 0) return null;
+                auto ti = tls_index(pdso._tlsMod, 0);
+                return (__tls_get_addr(&ti)-TLS_DTV_OFFSET)[0 .. pdso._tlsSize];
+            }
         }
     }
 }
 
+/****
+ * Gets called on program startup just before GC is initialized.
+ */
 void initSections()
 {
+    debug(PRINTF) printf("initSections called\n");
     globalSectionGroup.moduleGroup = ModuleGroup(getModuleInfos());
 
     static void pushRange(void* start, void* end)
@@ -248,24 +392,19 @@ void initSections()
     }
     else version (UseELF)
     {
-        // Add data section based on ELF image
-        void[] data = void;
-        findDataSection(&data);
-        globalSectionGroup._gcRanges.insertBack(data);
-
-        // Explicitly add TLS range for main thread.
         dl_phdr_info phdr = void;
         findPhdrForAddr(&globalSectionGroup, &phdr) || assert(0);
-        globalSectionGroup._gcRanges.insertBack(getTLSRange(getTLSInfo(phdr)));
-    }
-    else version (Solaris)
-    {
-        pushRange(&_environ, &_end);
+
+        scanSegments(phdr, &globalSectionGroup);
     }
 }
 
+/***
+ * Gets called on program shutdown just after GC is terminated.
+ */
 void finiSections()
 {
+    debug(PRINTF) printf("finiSections called\n");
     import core.stdc.stdlib : free;
     free(cast(void*)globalSectionGroup.modules.ptr);
 }
@@ -287,8 +426,12 @@ private
     }
 }
 
+/***
+ * Called once per thread; returns array of thread local storage ranges
+ */
 void[] initTLSRanges()
 {
+    debug(PRINTF) printf("initTLSRanges called\n");
     version (OSX)
     {
         void* start = null;
@@ -303,13 +446,11 @@ void[] initTLSRanges()
         // the stack, so we only need to do something for the main thread.
         return null;
     }
-    else version (FreeBSD)
+    else version (UseELF)
     {
-        return null;
-    }
-    else version (Solaris)
-    {
-        static assert(0, "TLS range detection not implemented on Solaris.");
+        auto rng = getTLSRange(&globalSectionGroup);
+        debug(PRINTF) printf("Add range %p %d\n", rng ? rng.ptr : cast(void*)0, rng ? rng.length : 0);
+        return rng;
     }
     else version (Windows)
     {
@@ -323,10 +464,12 @@ void[] initTLSRanges()
 
 void finiTLSRanges(void[] rng)
 {
+    debug(PRINTF) printf("finiTLSRanges called\n");
 }
 
 void scanTLSRanges(void[] rng, scope void delegate(void* pbeg, void* pend) nothrow dg) nothrow
 {
+    debug(PRINTF) printf("scanTLSRanges called (rng = %p %d)\n", rng ? rng.ptr : cast(void*)0, rng ? rng.length : 0);
     if (rng) dg(rng.ptr, rng.ptr + rng.length);
 }
 
