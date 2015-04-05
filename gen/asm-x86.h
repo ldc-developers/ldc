ef41ecbffd9d350ae86241c0576bda7cf308ed64
Kai Nacke <kai@redstar.de>
2015-04-05 18:11:30 +0200

    Implement cent/ucent data types.
    This is a first try at implementing the data types cent and ucent.
    All changes are wrapped in #if WANT_CENT .. #endif. It takes
    advantage of the GCC builtin type __int128 and is currently only
    enabled if compiled with gcc.
    
    As soon as this is stable it should go upstream, too.

--- gen/asm-x86.h
+++ gen/asm-x86.h
@@ -2324,6 +2324,16 @@ struct AsmProcessor {
     if (sc->func->naked) {
       switch (type) {
       case Arg_Integer:
+#if WANT_CENT
+        if (e->type->isunsigned()) {
+          insnTemplate << "$" << static_cast<uint64_t>(e->toUInteger());
+        } else {
+#ifndef ASM_X86_64
+          insnTemplate << "$" << static_cast<uint64_t>(e->toInteger());
+#else
+          insnTemplate << "$" << static_cast<uint64_t>(e->toInteger());
+#endif
+#else
         if (e->type->isunsigned()) {
           insnTemplate << "$" << e->toUInteger();
         } else {
@@ -2331,6 +2341,7 @@ struct AsmProcessor {
           insnTemplate << "$" << static_cast<sinteger_t>(e->toInteger());
 #else
           insnTemplate << "$" << e->toInteger();
+#endif
 #endif
         }
         break;
@@ -2929,8 +2940,13 @@ struct AsmProcessor {
           LOG_SCOPE
           Logger::cout() << "baseReg: " << operand->baseReg << '\n';
           Logger::cout() << "segmentPrefix: " << operand->segmentPrefix << '\n';
+#if WANT_CENT
+          Logger::cout() << "constDisplacement: " << static_cast<uint64_t>(operand->constDisplacement)
+                         << '\n';
+#else
           Logger::cout() << "constDisplacement: " << operand->constDisplacement
                          << '\n';
+#endif
           for (unsigned i = 0; i < operand->symbolDisplacement.dim; i++) {
             Expression *expr =
                 static_cast<Expression *>(operand->symbolDisplacement.data[i]);
@@ -2950,7 +2966,11 @@ struct AsmProcessor {
         if ((operand->segmentPrefix != Reg_Invalid &&
              operand->symbolDisplacement.dim == 0) ||
             operand->constDisplacement) {
+#if WANT_CENT
+          insnTemplate << static_cast<uint64_t>(operand->constDisplacement);
+#else
           insnTemplate << operand->constDisplacement;
+#endif
           if (operand->symbolDisplacement.dim) {
             insnTemplate << '+';
           }
@@ -3703,11 +3723,22 @@ struct AsmProcessor {
     case TOKint64v:
     case TOKuns64v:
 // semantic here?
+#if WANT_CENT
+    case TOKint128v:
+    case TOKuns128v:
+#ifndef ASM_X86_64
+      // %% for tok64 really should use 64bit type
+      e = new IntegerExp(stmt->loc, token->uns128value, Type::tint32);
+#else
+      e = new IntegerExp(stmt->loc, token->uns128value, Type::tint64);
+#endif
+#else
 #ifndef ASM_X86_64
       // %% for tok64 really should use 64bit type
       e = new IntegerExp(stmt->loc, token->uns64value, Type::tint32);
 #else
       e = new IntegerExp(stmt->loc, token->uns64value, Type::tint64);
+#endif
 #endif
       nextToken();
       break;
@@ -3762,8 +3793,15 @@ struct AsmProcessor {
               case TOKuns32v:
               case TOKint64v:
               case TOKuns64v:
+#if WANT_CENT
+              case TOKint128v:
+              case TOKuns128v:
+                if (token->uns128value < 8) {
+                  e = newRegExp(static_cast<Reg>(Reg_ST + token->uns128value));
+#else
                 if (token->uns64value < 8) {
                   e = newRegExp(static_cast<Reg>(Reg_ST + token->uns64value));
+#endif
                 } else {
                   stmt->error("invalid floating point register index");
                   e = Handled;
@@ -3858,10 +3896,18 @@ struct AsmProcessor {
 // FIXME: This printf is not portable. The use of `align` varies from
 // system to system; on i386 using a.out, .align `n` will align on a 2^`n`
 // boundary instead of an `n` boundary
+#if WANT_CENT
+#ifdef HAVE_GAS_BALIGN_AND_P2ALIGN
+      insnTemplate << ".balign\t" << static_cast<uint64_t>(align);
+#else
+      insnTemplate << ".align\t" << static_cast<uint64_t>(align);
+#endif
+#else
 #ifdef HAVE_GAS_BALIGN_AND_P2ALIGN
       insnTemplate << ".balign\t" << align;
 #else
       insnTemplate << ".align\t" << align;
+#endif
 #endif
     } else {
       stmt->error("alignment must be a power of 2, not %u",
@@ -3904,6 +3950,21 @@ struct AsmProcessor {
       case Op_ds:
       case Op_di:
       case Op_dl:
+#if WANT_CENT
+        if (token->value == TOKint32v || token->value == TOKuns32v ||
+            token->value == TOKint64v || token->value == TOKuns64v ||
+            token->value == TOKint128v || token->value == TOKuns128v) {
+          // As per usual with GNU, assume at least 32-bit host
+          if (op != Op_dl) {
+            insnTemplate->printf("%u", (d_uns32) token->uns128value);
+          } else {
+            // Output two .longS.  GAS has .quad, but would have to rely on 'L'
+            // format ..
+            // just need to use HOST_WIDE_INT_PRINT_DEC
+            insnTemplate->printf("%u,%u", (d_uns32) token->uns128value,
+                                 (d_uns32) (token->uns128value >> 32));
+          }
+#else
         if (token->value == TOKint32v || token->value == TOKuns32v ||
             token->value == TOKint64v || token->value == TOKuns64v) {
           // As per usual with GNU, assume at least 32-bit host
@@ -3916,6 +3977,7 @@ struct AsmProcessor {
             insnTemplate->printf("%u,%u", (d_uns32) token->uns64value,
                                  (d_uns32) (token->uns64value >> 32));
           }
+#endif
         } else {
           stmt->error("expected integer constant");
         }
