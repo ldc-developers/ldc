de73a30f051a6ce4307641f57e463a1409a83304
David Nadlinger <code@klickverbot.at>
2015-06-13 19:50:47 +0200

    Factor out inline IR handling code into gen/inlineir.{cpp, h}
    It is the only part that will continue to use the IR linker.

--- gen/functions.cpp
+++ gen/functions.cpp
@@ -8,6 +8,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "gen/functions.h"
+
 #include "aggregate.h"
 #include "declaration.h"
 #include "id.h"
@@ -20,6 +21,7 @@
 #include "gen/arrays.h"
 #include "gen/classes.h"
 #include "gen/dvalue.h"
+#include "gen/inlineir.h"
 #include "gen/irstate.h"
 #include "gen/linkage.h"
 #include "gen/llvm.h"
@@ -31,11 +33,6 @@
 #include "gen/runtime.h"
 #include "gen/tollvm.h"
 #include "ir/irmodule.h"
-#if LDC_LLVM_VER >= 305
-#include "llvm/Linker/Linker.h"
-#else
-#include "llvm/Linker.h"
-#endif
 #if LDC_LLVM_VER >= 303
 #include "llvm/IR/Intrinsics.h"
 #else
@@ -219,108 +216,6 @@ llvm::FunctionType* DtoFunctionType(Type* type, IrFuncTy &irFty, Type* thistype,
     IF_LOG Logger::cout() << "Final function type: " << *irFty.funcType << "\n";
 
     return irFty.funcType;
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////
-
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Support/SourceMgr.h"
-#if LDC_LLVM_VER >= 305
-#include "llvm/AsmParser/Parser.h"
-#else
-#include "llvm/Assembly/Parser.h"
-#endif
-
-LLFunction* DtoInlineIRFunction(FuncDeclaration* fdecl)
-{
-    const char* mangled_name = mangleExact(fdecl);
-    TemplateInstance* tinst = fdecl->parent->isTemplateInstance();
-    assert(tinst);
-
-    Objects& objs = tinst->tdtypes;
-    assert(objs.dim == 3);
-
-    Expression* a0 = isExpression(objs[0]);
-    assert(a0);
-    StringExp* strexp = a0->toStringExp();
-    assert(strexp);
-    assert(strexp->sz == 1);
-    std::string code(static_cast<char*>(strexp->string), strexp->len);
-
-    Type* ret = isType(objs[1]);
-    assert(ret);
-
-    Tuple* a2 = isTuple(objs[2]);
-    assert(a2);
-    Objects& arg_types = a2->objects;
-
-    std::string str;
-    llvm::raw_string_ostream stream(str);
-    stream << "define " << *DtoType(ret) << " @" << mangled_name << "(";
-
-    for(size_t i = 0; ;)
-    {
-        Type* ty = isType(arg_types[i]);
-        //assert(ty);
-        if(!ty)
-        {
-            error(tinst->loc,
-                "All parameters of a template defined with pragma llvm_inline_ir, except for the first one, should be types");
-            fatal();
-        }
-        stream << *DtoType(ty);
-
-        i++;
-        if(i >= arg_types.dim)
-            break;
-
-        stream << ", ";
-    }
-
-    if(ret->ty == Tvoid)
-        code.append("\nret void");
-
-    stream << ")\n{\n" << code <<  "\n}";
-
-    llvm::SMDiagnostic err;
-
-#if LDC_LLVM_VER >= 306
-    std::unique_ptr<llvm::Module> m = llvm::parseAssemblyString(
-        stream.str().c_str(), err, gIR->context());
-#elif LDC_LLVM_VER >= 303
-    llvm::Module* m = llvm::ParseAssemblyString(
-        stream.str().c_str(), NULL, err, gIR->context());
-#else
-    llvm::ParseAssemblyString(
-        stream.str().c_str(), gIR->module, err, gIR->context());
-#endif
-
-    std::string errstr = err.getMessage();
-    if(errstr != "")
-        error(tinst->loc,
-            "can't parse inline LLVM IR:\n%s\n%s\n%s\nThe input string was: \n%s",
-#if LDC_LLVM_VER >= 303
-            err.getLineContents().str().c_str(),
-#else
-            err.getLineContents().c_str(),
-#endif
-            (std::string(err.getColumnNo(), ' ') + '^').c_str(),
-            errstr.c_str(), stream.str().c_str());
-
-#if LDC_LLVM_VER >= 306
-    llvm::Linker(gIR->module).linkInModule(m.get());
-#elif LDC_LLVM_VER >= 303
-    std::string errstr2 = "";
-    llvm::Linker(gIR->module).linkInModule(m, &errstr2);
-    if(errstr2 != "")
-        error(tinst->loc,
-            "Error when linking in llvm inline ir: %s", errstr2.c_str());
-#endif
-
-    LLFunction* fun = gIR->module->getFunction(mangled_name);
-    fun->setLinkage(llvm::GlobalValue::LinkOnceODRLinkage);
-    fun->addFnAttr(LDC_ATTRIBUTE(AlwaysInline));
-    return fun;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
