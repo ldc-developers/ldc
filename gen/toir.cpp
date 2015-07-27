ad2546c6b06265f69ed67ffd829971ce2d87b7de
David Nadlinger <code@klickverbot.at>
2015-07-27 21:03:55 +0200

    toir/AssignExp: Align logic with DMD source to make maintenance/verification easier

--- gen/toir.cpp
+++ gen/toir.cpp
@@ -560,16 +560,19 @@ public:
             return;
         }
 
-        bool canSkipPostblit = true;
-        if ( (e->e2->op != TOKslice && e->e2->isLvalue()) ||
-             (e->e2->op == TOKslice && static_cast<UnaExp*>(e->e2)->e1->isLvalue()) ||
-             (e->e2->op == TOKcast  && static_cast<UnaExp*>(e->e2)->e1->isLvalue()) )
+        // This matches the logic in AssignExp::semantic.
+        // TODO: Should be cached in the frontend to avoid issues with the code
+        // getting out of sync?
+        bool lvalueElem = false;
+        if ((e->e2->op == TOKslice && static_cast<UnaExp*>(e->e2)->e1->isLvalue()) ||
+            (e->e2->op == TOKcast  && static_cast<UnaExp*>(e->e2)->e1->isLvalue()) ||
+            (e->e2->op != TOKslice && e->e2->isLvalue()))
         {
-            canSkipPostblit = false;
+            lvalueElem = true;
         }
 
-        Logger::println("performing normal assignment (canSkipPostblit = %d)", canSkipPostblit);
-        DtoAssign(e->loc, l, r, e->op, canSkipPostblit);
+        Logger::println("performing normal assignment (rhs has lvalue elems = %d)", lvalueElem);
+        DtoAssign(e->loc, l, r, e->op, !lvalueElem);
 
         result = l;
     }
