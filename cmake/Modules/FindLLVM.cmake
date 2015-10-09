992fc7b318308559b7f9dd3f62e751b35101cac0
Kai Nacke <kai@redstar.de>
2015-10-09 22:58:48 +0200

    Autodetect LLVM 3.8.

--- cmake/Modules/FindLLVM.cmake
+++ cmake/Modules/FindLLVM.cmake
@@ -27,7 +27,8 @@
 # We also want an user-specified LLVM_ROOT_DIR to take precedence over the
 # system default locations such as /usr/local/bin. Executing find_program()
 # multiples times is the approach recommended in the docs.
-set(llvm_config_names llvm-config-3.7 llvm-config37
+set(llvm_config_names llvm-config-3.8 llvm-config38
+                      llvm-config-3.7 llvm-config37
                       llvm-config-3.6 llvm-config36
                       llvm-config-3.5 llvm-config35
                       llvm-config-3.4 llvm-config34
