5fce371c91396face1964f85a153e4f32a19ec3b
Martin <noone@nowhere.com>
2015-12-05 19:13:18 +0100

    Streamline generic x86 ABI

--- gen/abi-x86.cpp
+++ gen/abi-x86.cpp
@@ -22,9 +22,12 @@
 
 struct X86TargetABI : TargetABI {
   const bool isOSX;
+  const bool isMSVC;
   IntegerRewrite integerRewrite;
 
-  X86TargetABI() : isOSX(global.params.isOSX) {}
+  X86TargetABI()
+      : isOSX(global.params.isOSX),
+        isMSVC(global.params.targetTriple.isWindowsMSVCEnvironment()) {}
 
   llvm::CallingConv::ID callingConv(llvm::FunctionType *ft, LINK l) override {
     switch (l) {
@@ -62,70 +65,61 @@ struct X86TargetABI : TargetABI {
     }
   }
 
-private:
-  bool returnOSXStructInArg(TypeStruct *t) {
-    // https://developer.apple.com/library/mac/documentation/DeveloperTools/Conceptual/LowLevelABI/Mac_OS_X_ABI_Function_Calls.pdf
-    //
-    // OS X variation on IA-32 for returning structs, page 57 section on
-    // Returning Results:
-    //   "Structures 1 or 2 bytes in size are placed in EAX. Structures 4
-    //    or 8 bytes in size are placed in: EAX and EDX. Structures of
-    //    other sizes are placed at the address supplied by the caller."
-    // Non-POD structs (non-C compatible) should always be returned in an
-    // arg though (yes, sometimes extern(C) functions return these, but C
-    // code does not handle struct lifecycle).
-    size_t sz = t->Type::size();
-    return !t->sym->isPOD() || (sz != 1 && sz != 2 && sz != 4 && sz != 8);
-  }
-
-  bool isMagicCLong(Type *t) {
-    // The frontend has magic structs to express the variable-sized C types
-    // for C++ mangling purposes. We need to pass them like integers, not
-    // on the stack.
-
-    Type *const bt = t->toBasetype();
-    if (bt->ty != Tstruct) {
-      return false;
-    }
-
-    Identifier *id = static_cast<TypeStruct *>(bt)->sym->ident;
-    return (id == Id::__c_long) || (id == Id::__c_ulong);
-  }
-
-public:
   bool returnInArg(TypeFunction *tf) override {
-    if (tf->isref) {
+    if (tf->isref)
       return false;
-    }
 
     Type *rt = tf->next->toBasetype();
-    // D only returns structs on the stack
-    if (tf->linkage == LINKd) {
-      return rt->ty == Tstruct || rt->ty == Tsarray;
-    }
 
-    // other ABI's follow C, which is cdouble and creal returned on the stack
-    // as well as structs (except for some OSX cases).
-    if (isMagicCLong(rt)) {
+    // non-aggregates are returned directly
+    if (!isAggregate(rt))
       return false;
+
+    // extern(D): sret exclusively for all structs and static arrays
+    if (tf->linkage == LINKd && tf->varargs != 1)
+      return rt->ty == Tstruct || rt->ty == Tsarray;
+
+    // extern(C) and all others:
+
+    // special cases for structs
+    if (rt->ty == Tstruct) {
+      // no sret for magic C++ structs
+      if (isMagicCppStruct(rt))
+        return false;
+
+      // force sret for non-POD structs
+      if (!isPOD(rt, tf->linkage))
+        return true;
     }
 
-    if (rt->ty == Tstruct) {
-      return !isOSX || returnOSXStructInArg(static_cast<TypeStruct *>(rt));
+    if (isOSX || isMSVC) {
+      // no sret for remaining aggregates of a power-of-2 size <= 8 bytes
+      return !canRewriteAsInt(rt);
     }
-    return (rt->ty == Tsarray || rt->ty == Tcomplex64 || rt->ty == Tcomplex80);
+
+    return true;
   }
 
   bool passByVal(Type *t) override {
+    // pass all structs and static arrays with the LLVM byval attribute
     return t->toBasetype()->ty == Tstruct || t->toBasetype()->ty == Tsarray;
   }
 
   void rewriteFunctionType(TypeFunction *tf, IrFuncTy &fty) override {
-    // extern(D)
-    if (tf->linkage == LINKd) {
-      // IMPLICIT PARAMETERS
+    // return value:
+    if (!fty.ret->byref) {
+      Type *rt = tf->next->toBasetype(); // for sret, rt == void
+      if (isAggregate(rt) && !isMagicCppStruct(rt) && canRewriteAsInt(rt) &&
+          !integerRewrite.isObsoleteFor(fty.ret->ltype)) {
+        fty.ret->rewrite = &integerRewrite;
+        fty.ret->ltype = integerRewrite.type(fty.ret->type, fty.ret->ltype);
+      }
+    }
 
-      // mark this/nested params inreg
+    // extern(D):
+    if (tf->linkage == LINKd && tf->varargs != 1) {
+
+      // try to pass an implicit argument in a register...
       if (fty.arg_this) {
         Logger::println("Putting 'this' in register");
         fty.arg_this->attrs.clear().add(LLAttribute::InReg);
@@ -138,7 +132,8 @@ public:
         // sret parameter to be in EAX in this situation...
         sret->attrs.add(LLAttribute::InReg).remove(LLAttribute::StructRet);
       }
-      // otherwise try to mark the last param inreg
+
+      // ... otherwise try the last argument
       else if (!fty.args.empty()) {
         // The last parameter is passed in EAX rather than being pushed on the
         // stack if the following conditions are met:
@@ -150,14 +145,15 @@ public:
         Type *lastTy = last->type->toBasetype();
         unsigned sz = lastTy->size();
 
-        if (last->byref && !last->isByVal()) {
-          Logger::println("Putting last (byref) parameter in register");
-          last->attrs.add(LLAttribute::InReg);
-        } else if (!lastTy->isfloating() &&
-                   (sz == 1 || sz == 2 || sz == 4)) // right?
-        {
-          // rewrite the struct into an integer to make inreg work
-          if (lastTy->ty == Tstruct || lastTy->ty == Tsarray) {
+        if (last->byref) {
+          if (!last->isByVal()) {
+            Logger::println("Putting last (byref) parameter in register");
+            last->attrs.add(LLAttribute::InReg);
+          }
+        } else if (!lastTy->isfloating() && (sz == 1 || sz == 2 || sz == 4)) {
+          // may have to rewrite the aggregate as integer to make inreg work
+          if ((lastTy->ty == Tstruct || lastTy->ty == Tsarray) &&
+              !integerRewrite.isObsoleteFor(last->ltype)) {
             last->rewrite = &integerRewrite;
             last->ltype = integerRewrite.type(last->type, last->ltype);
             last->byref = false;
@@ -168,75 +164,40 @@ public:
         }
       }
 
-      // FIXME: tf->varargs == 1 need to use C calling convention and vararg
-      // mechanism to live up to the spec:
-      // "The caller is expected to clean the stack. _argptr is not passed, it
-      // is computed by the callee."
-
-      // EXPLICIT PARAMETERS
+      // all other arguments are passed on the stack, don't rewrite
 
       // reverse parameter order
-      // for non variadics
-      if (!fty.args.empty() && tf->varargs != 1) {
+      if (!fty.args.empty()) {
         fty.reverseParams = true;
       }
+
+      return;
     }
 
-    // extern(C) and all others
-    else {
-      // RETURN VALUE
+    // extern(C) and all others:
 
-      if ((!fty.ret->byref && isMagicCLong(tf->next)) ||
-          tf->next->toBasetype() == Type::tcomplex32) {
-        // __c_long -> i32, cfloat -> i64
-        fty.ret->rewrite = &integerRewrite;
-        fty.ret->ltype = integerRewrite.type(fty.ret->type, fty.ret->ltype);
-      } else if (isOSX) {
-        // value struct returns should be rewritten as an int type to
-        // generate correct register usage (matches clang).
-        // note: sret functions change ret type to void so this won't
-        // trigger for those
-        Type *retTy = fty.ret->type->toBasetype();
-        if (!fty.ret->byref && retTy->ty == Tstruct) {
-          fty.ret->rewrite = &integerRewrite;
-          fty.ret->ltype = integerRewrite.type(fty.ret->type, fty.ret->ltype);
+    // Clang does not pass empty structs, while it seems that GCC does,
+    // at least on Linux x86. We don't know whether the C compiler will
+    // be Clang or GCC, so just assume Clang on OS X and G++ on Linux.
+    if (!isOSX) {
+      return;
+    }
+
+    size_t i = 0;
+    while (i < fty.args.size()) {
+      Type *type = fty.args[i]->type->toBasetype();
+      if (type->ty == Tstruct) {
+        // Do not pass empty structs at all for C++ ABI compatibility.
+        // Tests with clang reveal that more complex "empty" types, for
+        // example a struct containing an empty struct, are not
+        // optimized in the same way.
+        auto sd = static_cast<TypeStruct *>(type)->sym;
+        if (sd->fields.empty()) {
+          fty.args.erase(fty.args.begin() + i);
+          continue;
         }
       }
-
-      // IMPLICIT PARAMETERS
-
-      // EXPLICIT PARAMETERS
-
-      // Clang does not pass empty structs, while it seems that GCC does,
-      // at least on Linux x86. We don't know whether the C compiler will
-      // be Clang or GCC, so just assume Clang on OS X and G++ on Linux.
-      if (isOSX) {
-        size_t i = 0;
-        while (i < fty.args.size()) {
-          Type *type = fty.args[i]->type->toBasetype();
-          if (type->ty == Tstruct) {
-            // Do not pass empty structs at all for C++ ABI compatibility.
-            // Tests with clang reveal that more complex "empty" types, for
-            // example a struct containing an empty struct, are not
-            // optimized in the same way.
-            StructDeclaration *sd = static_cast<TypeStruct *>(type)->sym;
-            if (sd->fields.empty()) {
-              fty.args.erase(fty.args.begin() + i);
-              continue;
-            }
-          }
-          ++i;
-        }
-      }
-
-      for (auto arg : fty.args) {
-        if (!arg->byref && isMagicCLong(arg->type)) {
-          arg->rewrite = &integerRewrite;
-          arg->ltype = integerRewrite.type(arg->type, arg->ltype);
-          arg->byref = false;
-          arg->attrs.clear();
-        }
-      }
+      ++i;
     }
   }
 };
