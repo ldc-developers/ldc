5434a95e350827798af5e30df202b0d5b0a2c484
Joakim <git@joakim.fea.st>
2016-01-31 13:41:59 +0530

    Replace all uses of predefined version Android with CRuntime_Bionic or linux, and use CRuntime_Glibc where appropriate.

--- src/core/sys/posix/signal.d
+++ src/core/sys/posix/signal.d
@@ -2082,7 +2082,7 @@ int pthread_kill(pthread_t, int);
 int pthread_sigmask(int, in sigset_t*, sigset_t*);
 */
 
-version( linux )
+version( CRuntime_Glibc )
 {
     int pthread_kill(pthread_t, int);
     int pthread_sigmask(int, in sigset_t*, sigset_t*);
@@ -2102,7 +2102,7 @@ else version (Solaris)
     int pthread_kill(pthread_t, int);
     int pthread_sigmask(int, in sigset_t*, sigset_t*);
 }
-else version( Android )
+else version( CRuntime_Bionic )
 {
     int pthread_kill(pthread_t, int);
     int pthread_sigmask(int, in sigset_t*, sigset_t*);
