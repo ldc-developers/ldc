9d34d27781b1c94abed729ed96c430fa643e6ec4
Dan Olson <danole@comcast.net>
2016-02-27 21:35:54 +0000

    Fix exception chaining logic for ARM EABI
    Exception chaining logic uses _Unwind_GetCFA to determine if landing
    pads are nested.  Turns out gcc _Unwind_GetCFA for ARM_EABI is a
    partial implementation and can't be used for determing relative call
    frame, so use context stack pointer instead.

--- src/ldc/eh/libunwind.d
+++ src/ldc/eh/libunwind.d
@@ -223,7 +223,18 @@ struct NativeContext
     // As a further optimization step, we could look into caching that
     // result inside _d_exception.
     bool skipCatchComparison()       { return !isSearchPhase() && (actions & _Unwind_Action.HANDLER_FRAME) == 0; }
-    ptrdiff_t getCfaAddress()        { return _Unwind_GetCFA(context); }
+
+    ptrdiff_t getCfaAddress()
+    {
+        // libgcc _Unwind_GetCFA for ARM_EABI is a partial
+        // implementation, only valid during forced unwinds, so use
+        // stack pointer instead.
+        version (ARM_EABI_UNWINDER)
+            return _Unwind_GetGR(context, UNWIND_STACK_REG);
+        else
+            return _Unwind_GetCFA(context);
+    }
+
     Object getThrownObject()         { return exception_struct.exception_object; }
 
     void overrideThrownObject(Object newObject)
