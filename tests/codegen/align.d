4ecfadbc1c3cd630073a18f949d0138d2cb7b353
Martin <noone@nowhere.com>
2016-03-13 21:43:41 +0100

    MSVC: Re-enable alignment attribute for sret params
    Seems to be working with recent LLVM - at least for x64.

--- tests/codegen/align.d
+++ tests/codegen/align.d
@@ -10,14 +10,13 @@ static Inner globalInner;
 
 Outer passAndReturnOuterByVal(Outer arg) { return arg; }
 // CHECK: define{{.*}} void @{{.*}}_D5align23passAndReturnOuterByValFS5align5OuterZS5align5Outer
-/* no sret align attributes for MSVC targets at the moment */
-// C HECK-SAME: %align.Outer* noalias sret align 32 %.sret_arg
+// CHECK-SAME: %align.Outer* noalias sret align 32 %.sret_arg
 /* how the arg is passed by value is ABI-specific, but the pointer must be aligned */
 // CHECK-SAME: align 32 %
 
 Inner passAndReturnInnerByVal(Inner arg) { return arg; }
 // CHECK: define{{.*}} void @{{.*}}_D5align23passAndReturnInnerByValFS5align5InnerZS5align5Inner
-// C HECK-SAME: %align.Inner* noalias sret align 32 %.sret_arg
+// CHECK-SAME: %align.Inner* noalias sret align 32 %.sret_arg
 // CHECK-SAME: align 32 %
 
 void main() {
@@ -49,11 +48,11 @@ void main() {
 
   outer = passAndReturnOuterByVal(outer);
   // CHECK: call{{.*}} void @{{.*}}_D5align23passAndReturnOuterByValFS5align5OuterZS5align5Outer
-  // C HECK-SAME: %align.Outer* noalias sret align 32 %.rettmp
+  // CHECK-SAME: %align.Outer* noalias sret align 32 %.rettmp
   // CHECK-SAME: align 32 %
 
   inner = passAndReturnInnerByVal(inner);
   // CHECK: call{{.*}} void @{{.*}}_D5align23passAndReturnInnerByValFS5align5InnerZS5align5Inner
-  // C HECK-SAME: %align.Inner* noalias sret align 32 %.rettmp1
+  // CHECK-SAME: %align.Inner* noalias sret align 32 %.rettmp1
   // CHECK-SAME: align 32 %
 }
