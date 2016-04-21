8672cf409f6763df800794a85a799bb257761da4
Rainer Schuetze <r.sagitario@gmx.de>
2016-04-21 23:22:58 +0200

    implement check for scoped destruction in closure, fixes fail_compilation/fail10666.d

--- gen/functions.cpp
+++ gen/functions.cpp
@@ -627,6 +627,28 @@ static LinkageWithCOMDAT lowerFuncLinkage(FuncDeclaration *fdecl) {
   return DtoLinkage(fdecl);
 }
 
+// LDC has the same problem with destructors of struct arguments in closures
+// as DMD, so we copy the failure detection
+void verifyScopedDestructionInClosure(FuncDeclaration* fd) {
+  for (size_t i = 0; i < fd->closureVars.dim; i++) {
+    VarDeclaration *v = fd->closureVars[i];
+  
+    // Hack for the case fail_compilation/fail10666.d, until
+    // proper issue https://issues.dlang.org/show_bug.cgi?id=5730 fix will come.
+    bool isScopeDtorParam = v->edtor && (v->storage_class & STCparameter);
+    if (v->needsScopeDtor() || isScopeDtorParam) {
+      // Because the value needs to survive the end of the scope!
+      v->error("has scoped destruction, cannot build closure");
+    }
+    if (v->isargptr) {
+      // See https://issues.dlang.org/show_bug.cgi?id=2479
+      // This is actually a bug, but better to produce a nice
+      // message at compile time rather than memory corruption at runtime
+      v->error("cannot reference variadic arguments from closure");
+    }
+  }
+}
+
 void DtoDefineFunction(FuncDeclaration *fd) {
   IF_LOG Logger::println("DtoDefineFunction(%s): %s", fd->toPrettyChars(),
                          fd->loc.toChars());
@@ -716,6 +738,9 @@ void DtoDefineFunction(FuncDeclaration *fd) {
       return;
     }
   }
+
+  if (fd->needsClosure())
+      verifyScopedDestructionInClosure(fd);
 
   assert(fd->ident != Id::empty);
 
