ec13fdd76bc6fc1b4d9cd991849f8be84b13dbe7
Johan Engelen <jbc.engelen@gmail.com>
2016-04-24 15:10:38 +0200

    [PGO] Add test for -singleobj compilation.

---
+++ tests/PGO/singleobj.d
@@ -0,0 +1,15 @@
+// Tests simulaneous compilation into singleobj
+
+// RUN: %ldc -fprofile-instr-generate=%t.profraw %S/inputs/singleobj_input.d -c -of=%t%obj \
+// RUN:   &&  %ldc -fprofile-instr-generate=%t.profraw %t%obj -run %s \
+// RUN:   &&  %profdata merge %t.profraw -o %t.profdata \
+// RUN:   &&  %ldc -singleobj -c -output-ll -of=%t2.ll -fprofile-instr-use=%t.profdata %S/inputs/singleobj_input.d %s
+
+void foo() {}
+void bar() {}
+
+void main() {
+  foo();
+  bar();
+  bar();
+}
