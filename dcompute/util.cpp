8fd7949eada810ed66c215d34d17c62b34bfb38a
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2016-07-16 13:09:24 +0800

    for real this time!

--- dcompute/util.cpp
+++ dcompute/util.cpp
@@ -17,11 +17,21 @@
 bool isFromDCompute_Types(Dsymbol *sym)
 {
     IF_LOG Logger::println("isFromDCompute_Types(%s)", sym->toPrettyChars());
-    auto moduleDecl = sym->getModule()->md;
+    LOG_SCOPE
+    auto mod = sym->getModule();
+    if (!mod)
+        return false;
+    IF_LOG Logger::println("mod = %s",mod->toPrettyChars());
+    auto moduleDecl = mod->md;
     if (!moduleDecl)
+        return false;
+    IF_LOG Logger::println("moduleDecl = %p",moduleDecl);
+    if (!moduleDecl->packages)
         return false;
     if (moduleDecl->packages->dim != 2)
         return false;
+    
+    IF_LOG Logger::println("moduleDecl.packages.dim = %d",moduleDecl->packages->dim);
     if (strcmp("dcompute", (*moduleDecl->packages)[0]->string))
         return false;
     if (strcmp("types", (*moduleDecl->packages)[1]->string))
@@ -32,7 +42,10 @@ bool isFromDCompute_Types(Dsymbol *sym)
 bool isFromDCompute_Attributes(Dsymbol *sym)
 {
     IF_LOG Logger::println("isFromDCompute_Attributes(%s)", sym->toPrettyChars());
-    auto moduleDecl = sym->getModule()->md;
+    auto mod = sym->getModule();
+    if (!mod)
+        return false;
+    auto moduleDecl = mod->md;
     if (!moduleDecl)
         return false;
     if (moduleDecl->packages->dim != 2)
