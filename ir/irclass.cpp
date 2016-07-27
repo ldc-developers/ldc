c6ba2dc29b445892921a53a57c5b4f0408b2362b
Johan Engelen <jbc.engelen@gmail.com>
2016-07-27 20:20:07 +0200

    Fix a few clang warnings. [NFC]

--- ir/irclass.cpp
+++ ir/irclass.cpp
@@ -371,8 +371,9 @@ llvm::GlobalVariable *IrAggr::getInterfaceVtbl(BaseClass *b, bool new_instance,
 
       // Create a dummy FuncDeclaration with enough information to satisfy the
       // DIBuilder
-      FuncDeclaration *thunkFd = reinterpret_cast<FuncDeclaration *>(memcpy(
-          new char[sizeof(FuncDeclaration)], fd, sizeof(FuncDeclaration)));
+      FuncDeclaration *thunkFd = reinterpret_cast<FuncDeclaration *>(
+          memcpy(new char[sizeof(FuncDeclaration)], (void *)fd,
+                 sizeof(FuncDeclaration)));
       thunkFd->ir = new IrDsymbol();
       auto thunkFunc = getIrFunc(thunkFd, true); // create the IrFunction
       thunkFunc->func = thunk;
