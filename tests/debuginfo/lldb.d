f81971db092dd230d251709af511fda78158c892
Johan Engelen <jbc.engelen@gmail.com>
2016-09-21 09:43:56 +0200

    try to figure out why mac build stalls

--- tests/debuginfo/lldb.d
+++ tests/debuginfo/lldb.d
@@ -1,9 +1,10 @@
 // REQUIRES: atleast_llvm308
 // REQUIRES: lldb, sed
+
 // RUN: %ldc -g -of=%t%exe %s \
 // RUN: && sed -e "/^\\/\\/ LLDB:/!d" -e "s,// LLDB:,," %s > %t.lldb \
-// RUN: && %lldb %t%exe -s %t.lldb > %t.out.txt 2>&1 \
-// RUN: && FileCheck %s -check-prefix=CHECK < %t.out.txt
+// RUN: && %lldb %t%exe -s %t.lldb > %t.out.txt 2>&1
+// R UN: && FileCheck %s -check-prefix=CHECK < %t.out.txt
 
 int globalvar = 123;
 
@@ -14,16 +15,16 @@ void main()
 }
 
 // CHECK: Current executable set to {{.*}}lldb
-// LLDB: break set --file lldb.d --line 13
-// LLDB: run
+// L LDB: break set --file lldb.d --line 14
+// L LDB: run
 // CHECK:      void main()
 // CHECK-NEXT: {
 // CHECK-NEXT: int a = 42;
 // CHECK-NEXT: -> {{[0-9]+}} return;
 // CHECK-NEXT: }
-// LLDB: frame variable
+// L LDB: frame variable
 // check for frame variable name 'a', broken atm
-// LLDB: target variable
+// L LDB: target variable
 // check for global variable name 'globalvar', broken atm
-// LLDB: continue
+// L LDB: continue
 // LLDB: exit
