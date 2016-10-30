c3ef81173e50b98cc10f383195365c1fa7c36f28
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2016-10-30 16:19:05 +0800

    Don't attempt to link generated compute files

--- driver/linker.cpp
+++ driver/linker.cpp
@@ -144,8 +144,14 @@ void insertBitcodeFiles(llvm::Module &M, llvm::LLVMContext &Ctx,
 //////////////////////////////////////////////////////////////////////////////
 
 static void appendObjectFiles(std::vector<std::string> &args) {
-  for (unsigned i = 0; i < global.params.objfiles->dim; i++)
-    args.push_back((*global.params.objfiles)[i]);
+  for (unsigned i = 0; i < global.params.objfiles->dim; i++) {
+    const char *p = static_cast<const char *>(global.params.objfiles->data[i]);
+    llvm::SmallString<24> s(p);
+    if (s.endswith(".spv") || s.endswith(".ptx")) {
+      continue;
+    }
+    args.push_back(p);
+  }
 
   if (global.params.targetTriple->isWindowsMSVCEnvironment()) {
     if (global.params.resfile)
