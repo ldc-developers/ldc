73c98399fe84e2377f7e075d58bdf92fcb0fa899
Johan Engelen <jbc.engelen@gmail.com>
2017-01-09 22:56:33 +0100

    Put debuginfo on GEP instead of on the alloca with an offset.
    Resolves issue 1933

--- tests/debuginfo/nested.d
+++ tests/debuginfo/nested.d
@@ -12,7 +12,12 @@ void encloser(int arg0, int arg1)
     // CHECK-LABEL: define {{.*}}encloser{{.*}}nested
     void nested(int nes_i)
     {
-        // CHECK: @llvm.dbg.declare{{.*}}%nestedFrame{{.*}}arg1
+        // CHECK: %arg0 = getelementptr inbounds %nest.encloser
+        // CHECK: @llvm.dbg.declare{{.*}}%arg0
+        // CHECK: %arg1 = getelementptr inbounds %nest.encloser
+        // CHECK: @llvm.dbg.declare{{.*}}%arg1
+        // CHECK: %enc_n = getelementptr inbounds %nest.encloser
+        // CHECK: @llvm.dbg.declare{{.*}}%enc_n
         arg0 = arg1 = enc_n = nes_i; // accessing arg0, arg1 and enc_n from a nested function turns them into closure variables
 
         // nes_i and arg1 have the same parameter index in the generated IR, if both get declared as
