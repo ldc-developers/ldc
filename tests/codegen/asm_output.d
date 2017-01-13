6369abcd40d4aa48e648c04b6b9261084f3e4faa
Johan Engelen <jbc.engelen@gmail.com>
2017-01-13 20:54:00 +0100

    Properly mark lit tests that require X86. Should fix these tests on ARM.

--- tests/codegen/asm_output.d
+++ tests/codegen/asm_output.d
@@ -1,10 +1,9 @@
 // RUN: %ldc -c -output-ll -of=%t.ll %s && FileCheck %s --check-prefix LLVM < %t.ll
 // RUN: %ldc -c -output-s  -of=%t.s %s && FileCheck %s --check-prefix ASM < %t.s
 
-int main() {
+int foo() {
     return 42;
 // Try to keep these very simple checks independent of architecture:
-// LLVM:  ret i32 42 
-// ASM:  $42
-// ASM:  ret
+// LLVM:  ret i32 42
+// ASM:  {{(\$|#)}}42
 }
