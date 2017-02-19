1d4c6fa72c265e915bca26c23760635ae46eb9af
Martin <noone@nowhere.com>
2017-02-19 02:37:41 +0100

    Don't pre-define versions D_SIMD and D_AVX
    LDC doesn't support DMD's core.simd interface.

--- driver/main.cpp
+++ driver/main.cpp
@@ -775,14 +775,14 @@ void registerPredefinedTargetVersions() {
     VersionCondition::addPredefinedGlobalIdent("D_PIC");
   }
 
+  /* LDC doesn't support DMD's core.simd interface.
   if (arch == llvm::Triple::x86 || arch == llvm::Triple::x86_64) {
-    if (traitsTargetHasFeature("sse2")) {
+    if (traitsTargetHasFeature("sse2"))
       VersionCondition::addPredefinedGlobalIdent("D_SIMD");
-    }
-    if (traitsTargetHasFeature("avx")) {
+    if (traitsTargetHasFeature("avx"))
       VersionCondition::addPredefinedGlobalIdent("D_AVX");
-    }
   }
+  */
 
   // parse the OS out of the target triple
   // see http://gcc.gnu.org/install/specific.html for details
