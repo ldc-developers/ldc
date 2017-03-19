7d87a82d37d80106813fca7abe722757bdc5b8b2
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2017-03-19 11:53:25 +0800

    Apparently they go the other way round

--- driver/dcomputecodegenerator.cpp
+++ driver/dcomputecodegenerator.cpp
@@ -40,8 +40,8 @@ DComputeCodeGenManager::createComputeTarget(const std::string &s) {
       return createCUDATarget(ctx, v);
     }
   }
-#define STR(x) XSTR(x)
 #define XSTR(x) #x
+#define STR(x) XSTR(x)
   error(Loc(),
         "unrecognised or invalid DCompute targets: the format is ocl-xy0 "
         "for OpenCl x.y and cuda-xy0 for CUDA CC x.y. Valid versions "
