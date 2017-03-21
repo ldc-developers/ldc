e100890252dd28c491b73aa57460380de88d06c6
Johan Engelen <jbc.engelen@gmail.com>
2017-03-21 19:51:48 +0100

    Add test for byte[4] == byte[] : memcmp should be optimized out.

--- tests/codegen/array_equals_memcmp_dyn.d
+++ tests/codegen/array_equals_memcmp_dyn.d
@@ -1,18 +1,24 @@
 // Tests that dynamic array (in)equality is optimized to a memcmp call when valid.
 
 // RUN: %ldc -c -output-ll -of=%t.ll %s && FileCheck %s --check-prefix=LLVM < %t.ll
+// RUN: %ldc -c -output-s -O3 -of=%t.s  %s && FileCheck %s --check-prefix=ASM  < %t.s
 // RUN: %ldc -O3 -run %s
 
 module mod;
 
 // LLVM-LABEL: define{{.*}} @{{.*}}static_dynamic
+// ASM-LABEL: static_dynamic{{.*}}:
 bool static_dynamic(bool[4] a, bool[] b)
 {
     // LLVM: call i32 @memcmp(
+
+    // Also test that LLVM recognizes and optimizes-out the call to memcmp for 4 byte arrays:
+    // ASM-NOT: memcmp
     return a == b;
 }
 
 // LLVM-LABEL: define{{.*}} @{{.*}}inv_dynamic_dynamic
+// ASM-LABEL: inv_dynamic_dynamic{{.*}}:
 bool inv_dynamic_dynamic(bool[] a, bool[] b)
 {
     // LLVM: call i32 @memcmp(
