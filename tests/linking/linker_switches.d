77c1abcbd71717f2cef5d9b37a5b9d5d49449946
Adrian Matoga <adrian@matoga.info>
2017-05-10 14:04:42 +0000

    Add a test case for -Xcc vs -L vs pragma(lib)

---
+++ tests/linking/linker_switches.d
@@ -0,0 +1,13 @@
+// Test if global order of flags passed with -Xcc, -L and pragma(lib) is preserved
+
+// REQUIRES: Linux
+
+// RUN: %ldc %s -of=%t -Xcc -DOPT1,-DOPT2 -L-L/usr/lib -L--defsym -Lfoo=5 -Xcc -DOPT3 -v | FileCheck %s
+
+// CHECK: -DOPT1 -DOPT2 -L/usr/lib -Xlinker --defsym -Xlinker foo=5 -DOPT3 {{.*}}-lpthread
+
+pragma(lib, "pthread");
+
+void main()
+{
+}
