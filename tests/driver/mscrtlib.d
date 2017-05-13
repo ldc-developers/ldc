f9bc41c44e25326a2b31343324eaaa2eeaa0b039
Martin <noone@nowhere.com>
2017-05-14 00:21:57 +0200

    MSVC: Re-test runnable/eh.d with all 4 C runtime variants

---
+++ tests/driver/mscrtlib.d
@@ -0,0 +1,10 @@
+// On Windows, re-test dmd-testsuite's runnable\eh.d with all 4 MS C runtime variants.
+// In case of failures, it's very likely that the EH terminate hook in druntime's
+// ldc.eh.msvc.msvc_eh_terminate() needs to be adapted.
+
+// REQUIRES: Windows
+
+// RUN: %ldc -mscrtlib=libcmt                 -run "%S\..\d2\dmd-testsuite\runnable\eh.d"
+// RUN: %ldc -mscrtlib=libcmtd -link-debuglib -run "%S\..\d2\dmd-testsuite\runnable\eh.d"
+// RUN: %ldc -mscrtlib=msvcrt  -link-debuglib -run "%S\..\d2\dmd-testsuite\runnable\eh.d"
+// RUN: %ldc -mscrtlib=msvcrtd                -run "%S\..\d2\dmd-testsuite\runnable\eh.d"
