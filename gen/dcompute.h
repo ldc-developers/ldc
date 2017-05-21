646f7f9452338450027df17557ae1c00b0f5cac5
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2017-05-21 09:29:18 +0800

    add gen/dcompute.*

---
+++ gen/dcompute.h
@@ -0,0 +1,32 @@
+//===-- gen/dcompute.h ------------------------------------------*- C++ -*-===//
+//
+//                         LDC – the LLVM D compiler
+//
+// This file is distributed under the BSD-style LDC license. See the LICENSE
+// file for details.
+//
+//===----------------------------------------------------------------------===//
+// Functionality related to ldc.dcompute in druntime
+//===----------------------------------------------------------------------===//
+
+#ifndef LDC_GEN_DCOMPUTETYPES_H
+#define LDC_GEN_DCOMPUTETYPES_H
+
+#include "ddmd/aggregate.h"
+#include "ddmd/mtype.h"
+
+#include "llvm/ADT/Optional.h"
+
+class Dsymbol;
+class Type;
+
+
+bool isFromLDC_DCompute(Dsymbol *sym);
+
+struct DcomputePointer {
+    int addrspace;
+    Type* type;
+    DcomputePointer(int as,Type* ty) : addrspace(as),type(ty) {}
+};
+llvm::Optional<DcomputePointer> toDcomputePointer(StructDeclaration *sd);
+#endif
