29b19736fd41c2fbe75a981f5a5ef5b40bc0a5a3
Nicholas Wilson <thewilsonator@users.noreply.github.com>
2017-05-27 13:36:53 +0800

    Add `__dcompute_reflect` to ldc.dcompute

--- src/ldc/dcompute.d
+++ src/ldc/dcompute.d
@@ -8,6 +8,31 @@
 
 module ldc.dcompute;
 
+enum ReflectTarget : uint
+{
+    // These numbers MUST match DcomputeTarget::target in LDC.
+    Host = 0,
+    OpenCL = 1,
+    CUDA = 2,
+}
+/**
+ * The pseudo conditional compilation function.
+ * returns true if t and _version match the target and it's version.
+ * think __ctfe but per target, codegen time conditional compilation.
+ * as oppsed to compiletime/runtime.
+ * arguments MUST be compiletime constants
+ * valid values of _version are for OpenCL 100 110 120 200 210
+ * and for CUDA are x*100 + y*10 for x any valid values of sm x.y
+ * use 0 as a wildcard to match any version.
+ 
+ * This is mostly used for selecting the correct intrinsic for the
+ * given target and version, but could also be used to tailor for
+ * performance characteristics. See dcompute.std.index for an example
+ * of how to select intrinsics.
+ */
+pure nothrow @nogc
+extern(C) bool __dcompute_reflect(ReflectTarget t, uint _version = 0);
+
 ///Readability aliases for compute
 enum CompileFor : int
 {
@@ -49,19 +74,19 @@ struct compute
 private struct _kernel {}
 immutable kernel = _kernel();
 
-/**
- * DCompute has the notion of adress spaces, provide by the magic struct below.
- * The numbers are for the DCompute virtual addess space and are translated into
- * the correct address space for each DCompute backend (SPIRV, NVPTX).
- * The table below shows the equivalent annotation between DCompute OpenCL and CUDA
- *
- *   DCompute   OpenCL      Cuda
- *   Global     __global    __device__
- *   Shared     __local     __shared__
+/++
+ + DCompute has the notion of adress spaces, provide by the magic struct below.
+ + The numbers are for the DCompute virtual addess space and are translated into
+ + the correct address space for each DCompute backend (SPIRV, NVPTX).
+ + The table below shows the equivalent annotation between DCompute OpenCL and CUDA
+ +
+ +   DCompute   OpenCL      Cuda
+ +   Global     __global    __device__
+ +   Shared     __local     __shared__
  *   Constant   __constant  __constant__
- *   Private    __private   __local__
- *   Generic    __generic   (no qualifier)
- */
+ +   Private    __private   __local__
+ +   Generic    __generic   (no qualifier)
+ +/
 struct Pointer(AddrSpace as, T)
 {
     T* ptr;
