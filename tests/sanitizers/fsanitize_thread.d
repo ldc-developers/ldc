a0e2074f585ad169ac6ae64b00c893808e25f7f2
Johan Engelen <jbc.engelen@gmail.com>
2017-06-05 23:01:12 +0200

    Fix Win32 test

--- tests/sanitizers/fsanitize_thread.d
+++ tests/sanitizers/fsanitize_thread.d
@@ -2,7 +2,7 @@
 
 // RUN: %ldc -c -output-ll -g -fsanitize=thread -of=%t.ll %s && FileCheck %s < %t.ll
 
-// CHECK-LABEL: define i32 {{.*}}D16fsanitize_thread3foo
+// CHECK-LABEL: define {{.*}}D16fsanitize_thread3foo
 int foo(int i) {
     // CHECK: call {{.*}}_tsan_func_entry
     return i + 1;
