9d962a37823caa5e3ac82c05225b87c821f6e9b5
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2017-07-07 14:41:09 +0800

    Disable debug info for now
    Until I get it working.

--- gen/dibuilder.cpp
+++ gen/dibuilder.cpp
@@ -77,12 +77,18 @@ llvm::StringRef uniqueIdent(Type* t) {
 
 
 bool ldc::DIBuilder::mustEmitFullDebugInfo() {
-  // only for -g and -gc
+  // only for -g and -gc but not dcompute (yet)
+
+  if (IR->dcomputetarget) return false;
+
   return global.params.symdebug == 1 || global.params.symdebug == 2;
 }
 
 bool ldc::DIBuilder::mustEmitLocationsDebugInfo() {
-  // for -g -gc and -gline-tables-only
+  // for -g -gc and -gline-tables-onlybut not dcompute (yet)
+    
+  if (IR->dcomputetarget) return false;
+
   return (global.params.symdebug > 0) || global.params.outputSourceLocations;
 }
 
