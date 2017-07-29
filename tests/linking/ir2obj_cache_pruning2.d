c5354d677f9fb1f8346de34b8f0b25d150bb76b6
Johan Engelen <jbc.engelen@gmail.com>
2017-07-29 21:38:29 +0200

    Make cache pruning test more robust by sleeping longer between invokes. (#2249)
    I think there is a spurious case where things happen right before / after second elapses. This should fix that.

--- tests/linking/ir2obj_cache_pruning2.d
+++ tests/linking/ir2obj_cache_pruning2.d
@@ -31,8 +31,8 @@ void main()
 
     version (SLEEP)
     {
-        // Sleep for 2 seconds, so we are sure that the cache object file timestamps are "aging".
+        // Sleep for 4 seconds, so we are sure that the cache object file timestamps are "aging".
         import core.thread;
-        Thread.sleep( dur!"seconds"(2) );
+        Thread.sleep( dur!"seconds"(4) );
     }
 }
