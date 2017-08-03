e36daef7d6d8e3b06211877d8c19387cf911bf96
Johan Engelen <jbc.engelen@gmail.com>
2017-08-03 18:26:47 +0200

    Add test for multiple -fsanitize parameters

--- tests/sanitizers/fsanitize_address.d
+++ tests/sanitizers/fsanitize_address.d
@@ -2,6 +2,7 @@
 
 // RUN: %ldc -c -output-ll -g -fsanitize=address -of=%t.ll %s && FileCheck %s < %t.ll
 // RUN: %ldc -c -output-ll -g -fsanitize=address,thread -of=%t.tsan.ll %s && FileCheck %s --check-prefix=wTSAN < %t.tsan.ll
+// RUN: %ldc -c -output-ll -g -fsanitize=address -fsanitize=thread -of=%t.tsan.ll %s && FileCheck %s --check-prefix=wTSAN < %t.tsan.ll
 
 void foo() {
     // wTSAN: call {{.*}}_tsan_func_entry
