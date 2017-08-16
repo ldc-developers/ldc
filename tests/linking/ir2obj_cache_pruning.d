fe4a6aca31ee07c09e32817df1a5cad9ec473650
Johan Engelen <jbc.engelen@gmail.com>
2017-08-16 22:56:47 +0200

    Lit: stop using `%T` and use `%t-dir` instead.
    The %T substitution will be removed from Lit.
    Resolves issue #2274

--- tests/linking/ir2obj_cache_pruning.d
+++ tests/linking/ir2obj_cache_pruning.d
@@ -1,12 +1,12 @@
 // Test recognition of -cache-prune-* commandline flags
 
-// RUN: %ldc %s -cache=%T/prunecache1 -cache-prune
-// RUN: %ldc %s -cache=%T/prunecache1 -cache-prune-interval=10 -cache-prune-maxbytes=10000
-// RUN: %ldc %s -cache=%T/prunecache1 -cache-prune -cache-prune-interval=0
-// RUN: %ldc %s -cache=%T/prunecache1 -cache-prune -cache-prune-maxbytes=10000
-// RUN: %ldc %s -cache=%T/prunecache1 -cache-prune -cache-prune-expiration=10000
-// RUN: %ldc %s -cache=%T/prunecache1 -cache-prune-maxpercentage=50
-// RUN: %ldc %s -cache=%T/prunecache1 -cache-prune -cache-prune-maxpercentage=150
+// RUN: %ldc %s -cache=%t-dir -cache-prune
+// RUN: %ldc %s -cache=%t-dir -cache-prune-interval=10 -cache-prune-maxbytes=10000
+// RUN: %ldc %s -cache=%t-dir -cache-prune -cache-prune-interval=0
+// RUN: %ldc %s -cache=%t-dir -cache-prune -cache-prune-maxbytes=10000
+// RUN: %ldc %s -cache=%t-dir -cache-prune -cache-prune-expiration=10000
+// RUN: %ldc %s -cache=%t-dir -cache-prune-maxpercentage=50
+// RUN: %ldc %s -cache=%t-dir -cache-prune -cache-prune-maxpercentage=150
 
 void main()
 {
--- tests/linking/ir2obj_caching_flags1.d
+++ tests/linking/ir2obj_caching_flags1.d
@@ -3,36 +3,36 @@
 // Note that the NO_HIT tests should change the default setting of the tested flag.
 
 // Create and then empty the cache for correct testing when running the test multiple times.
-// RUN: %ldc %s -c -of=%t%obj -cache=%T/flag1cache \
-// RUN:   && %prunecache -f %T/flag1cache --max-bytes=1 \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -g                               -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache                                  -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -O                               -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -O3                              -vv | FileCheck --check-prefix=MUST_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -O2                              -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -O4                              -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc -O5 %s -c -of=%t%obj -cache=%T/flag1cache                              -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -Os                              -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -Oz                              -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -disable-d-passes                -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -disable-simplify-drtcalls       -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -disable-simplify-libcalls       -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -disable-gc2stack                -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -enable-inlining                 -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -unit-at-a-time=false            -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -strip-debug                     -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -disable-loop-unrolling          -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -disable-loop-vectorization      -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -disable-slp-vectorization       -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -vectorize-loops                 -vv | FileCheck --check-prefix=NO_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -v -wi -d                        -vv | FileCheck --check-prefix=MUST_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -D -H -I. -J.                    -vv | FileCheck --check-prefix=MUST_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -d-version=Irrelevant            -vv | FileCheck --check-prefix=MUST_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -unittest                        -vv | FileCheck --check-prefix=MUST_HIT %s \
-// RUN:   && %ldc %s               -cache=%T/flag1cache -lib                             -vv | FileCheck --check-prefix=MUST_HIT %s \
-// RUN:   && %ldc                  -cache=%T/flag1cache -vv -run %s                          | FileCheck --check-prefix=COULD_HIT %s \
-// RUN:   && %ldc                  -cache=%T/flag1cache -vv -run %s a b                      | FileCheck --check-prefix=MUST_HIT %s \
-// RUN:   && %ldc %s -c -of=%t%obj -cache=%T/flag1cache -g                               -vv | FileCheck --check-prefix=MUST_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir
+// RUN: %prunecache -f %t-dir --max-bytes=1
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -g                               -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir                                  -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -O                               -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -O3                              -vv | FileCheck --check-prefix=MUST_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -O2                              -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -O4                              -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc -O5 %s -c -of=%t%obj -cache=%t-dir                              -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -Os                              -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -Oz                              -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -disable-d-passes                -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -disable-simplify-drtcalls       -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -disable-simplify-libcalls       -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -disable-gc2stack                -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -enable-inlining                 -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -unit-at-a-time=false            -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -strip-debug                     -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -disable-loop-unrolling          -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -disable-loop-vectorization      -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -disable-slp-vectorization       -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -vectorize-loops                 -vv | FileCheck --check-prefix=NO_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -v -wi -d                        -vv | FileCheck --check-prefix=MUST_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -D -H -I. -J.                    -vv | FileCheck --check-prefix=MUST_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -d-version=Irrelevant            -vv | FileCheck --check-prefix=MUST_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -unittest                        -vv | FileCheck --check-prefix=MUST_HIT %s
+// RUN: %ldc %s               -cache=%t-dir -lib                             -vv | FileCheck --check-prefix=MUST_HIT %s
+// RUN: %ldc                  -cache=%t-dir -vv -run %s                          | FileCheck --check-prefix=COULD_HIT %s
+// RUN: %ldc                  -cache=%t-dir -vv -run %s a b                      | FileCheck --check-prefix=MUST_HIT %s
+// RUN: %ldc %s -c -of=%t%obj -cache=%t-dir -g                               -vv | FileCheck --check-prefix=MUST_HIT %s
 // The last test is a MUST_HIT test (hits with the first compile invocation), to make sure that the cache wasn't pruned somehow which could effectively disable some NO_HIT tests.
 
 // MUST_HIT: Cache object found!
