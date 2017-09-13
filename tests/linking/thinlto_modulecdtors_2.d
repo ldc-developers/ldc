ccda8ef2f908e29634f4b96aebe299ba61000133
Fatmice <rdirective@gmail.com>
2017-09-13 09:48:19 -0500

    -Removed changes to thinlto_modulecdtors.d and thinlto_modulecdtors_2.d as they weren't ment for this PR -Reshape INCLUDE_INSTALL_DIR

--- tests/linking/thinlto_modulecdtors_2.d
+++ tests/linking/thinlto_modulecdtors_2.d
@@ -1,7 +1,7 @@
 // REQUIRES: atleast_llvm309
 // REQUIRES: LTO
 
-// RUN: %ldc -flto=thin -O3 -Xcc -fuse-ld=gold %S/inputs/thinlto_ctor.d -run %s | FileCheck --check-prefix=EXECUTE %s
+// RUN: %ldc -flto=thin -O3 %S/inputs/thinlto_ctor.d -run %s | FileCheck --check-prefix=EXECUTE %s
 
 // EXECUTE: ctor
 // EXECUTE: main
