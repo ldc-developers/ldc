68ecac11c23ad5b03f8129950de66d3331eea2a8
Ivan <ivan.butygin@gmail.com>
2017-10-15 19:16:01 +0300

    Some documentation

--- runtime/jit-rt/d/ldc/runtimecompile.d
+++ runtime/jit-rt/d/ldc/runtimecompile.d
@@ -3,14 +3,51 @@ module ldc.runtimecompile;
 version(LDC_RuntimeCompilation)
 {
 
+/// Dynamic compiler settings
 struct CompilerSettings
 {
+  /// The Optimization Level - Specify the basic optimization level.
+  ///    0 = -O0, 1 = -O1, 2 = -O2, 3 = -O3
   uint optLevel = 0;
+
+  /// SizeLevel - How much we're optimizing for size.
+  ///    0 = none, 1 = -Os, 2 = -Oz
   uint sizeLevel = 0;
+
+  /// Optional progress handler, dynamic compiler will report compilation stages through it
+  /// Signature is (in char[] action, in char[] object)
+  /// Actual format of reports is not specified and must be used for debugging
+  /// purposes only
   void delegate(in char[], in char[]) progressHandler = null;
+
+  /// Optional dump handler, dynamic compiler will report final optimized module through it
+  /// This function can be called multiple times during compilation and user must concatenate all
+  /// reported parts manually
+  /// Actual format of dump is not specified and must be used for debugging
+  /// purposes only
   void delegate(in char[]) dumpHandler = null;
 }
 
+/++
+ + Compile all dynamic code.
+ + This function must be called before any calls to @runtimeCompile functions and
+ + after any changes to @runtimeCompile variables
+ +
+ + Consecutive calls to this function do nothing
+ +
+ + This function is not thread-safe
+ +
+ + Example:
+ + ---
+ + import ldc.attributes, ldc.runtimecompile, std.stdio;
+ +
+ + @runtimeCompile int foo() { return value * 42; }
+ +
+ + void main() {
+ +   compileDynamicCode();
+ +   writeln(foo());
+ + }
+ +/
 void compileDynamicCode(in CompilerSettings settings = CompilerSettings.init)
 {
   Context context;
