03b8d7a80e223a3fcf84b03ff8ce317fe233f133
kinke <kinke@users.noreply.github.com>
2017-10-17 19:45:36 +0200

    string -> char fixup

--- gen/asm-x86.h
+++ gen/asm-x86.h
@@ -2535,7 +2535,7 @@ struct AsmProcessor {
     case FPInt_Types:
       switch (ptrtype) {
       case Short_Ptr:
-        type_suffix = "s";
+        type_suffix = 's';
         break;
       case Int_Ptr:
         type_suffix = 'l';
