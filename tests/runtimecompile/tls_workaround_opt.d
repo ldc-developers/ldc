174b2807aa51ec001d661f465f4e342f3a1b2d1c
Ivan <ivan.butygin@gmail.com>
2017-11-05 21:25:37 +0300

    simple test for `runtime-compile-tls-workaround` option

---
+++ tests/runtimecompile/tls_workaround_opt.d
@@ -0,0 +1,18 @@
+
+// tls without workaround broken on all platforms
+// just test ldc accept this option
+// RUN: %ldc -enable-runtime-compile -runtime-compile-tls-workaround=0 -run %s
+// RUN: %ldc -enable-runtime-compile -runtime-compile-tls-workaround=1 -run %s
+
+import ldc.attributes;
+import ldc.runtimecompile;
+
+@runtimeCompile void foo()
+{
+}
+
+void main(string[] args)
+{
+  compileDynamicCode();
+  foo();
+}
