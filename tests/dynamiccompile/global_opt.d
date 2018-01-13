3cf9c3872918728100b2296c1ab44d2f5b99c8f8
Ivan Butygin <hardcode84@gmail.com>
2018-01-13 13:57:30 +0300

    @dynamicCompileConst optimization test (#2492)

---
+++ tests/dynamiccompile/global_opt.d
@@ -0,0 +1,44 @@
+
+// Asm is output disabled with old llvm versions
+// REQUIRES: atleast_llvm500
+// RUN: %ldc -enable-dynamic-compile -run %s
+
+import std.array;
+import std.string;
+import ldc.attributes;
+import ldc.dynamic_compile;
+
+@dynamicCompileConst __gshared int value = 0;
+
+@dynamicCompile int foo()
+{
+  return (value + 5) / 2;
+}
+
+void main(string[] args)
+{
+  auto dump = appender!(char[])();
+  CompilerSettings settings;
+  settings.optLevel = 2;
+  settings.dumpHandler = (DumpStage stage, in char[] str)
+  {
+    if (DumpStage.FinalAsm == stage)
+    {
+      dump.put(str);
+    }
+  };
+  value = 7;
+  compileDynamicCode(settings);
+
+  // Function return value will be reduced to constant
+  // search for this value in asm
+  assert(indexOf(dump.data, "6") != -1);
+  assert(foo() == 6);
+
+  dump.clear();
+
+  value = 3;
+  compileDynamicCode(settings);
+  assert(indexOf(dump.data, "4") != -1);
+  assert(foo() == 4);
+}
