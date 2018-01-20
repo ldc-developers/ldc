b7d54c39f0ba2b3525d474c9219c7b60137fa67c
Johan Engelen <jbc.engelen@gmail.com>
2018-01-20 14:45:20 +0100

    Make base LLVM version available in LDC code.

--- driver/ldc-version.h
+++ driver/ldc-version.h
@@ -15,6 +15,7 @@ namespace ldc {
 extern const char *const ldc_version;
 extern const char *const dmd_version;
 extern const char *const llvm_version;
+extern const char *const llvm_version_base; /// the base LLVM version without svn/git suffix
 extern const char *const built_with_Dcompiler_version;
 
 }
