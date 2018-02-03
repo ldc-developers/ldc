eabb4dbb360c3ee852f67befe2c0123a6dd66292
Martin <noone@nowhere.com>
2018-02-03 22:02:00 +0100

    Fix druntime tests for BUILD_SHARED_LIBS=ON

--- runtime/StandaloneDRuntimeTests.cmake
+++ runtime/StandaloneDRuntimeTests.cmake
@@ -12,37 +12,33 @@ endmacro()
 if(MULTILIB AND "${TARGET_SYSTEM}" MATCHES "APPLE")
     # KLUDGE: The library target is a custom command for multilib builds (lipo),
     # so cannot use TARGET_FILE directly. Should stash away that name instead.
-    set(druntime_path "${CMAKE_BINARY_DIR}/lib${LIB_SUFFIX}/libdruntime-ldc.a")
     set(shared_druntime_path "${CMAKE_BINARY_DIR}/lib${LIB_SUFFIX}/libdruntime-ldc${SHARED_LIB_SUFFIX}.dylib")
+    if(${BUILD_SHARED_LIBS} STREQUAL "ON")
+        set(druntime_path ${shared_druntime_path})
+    else()
+        set(druntime_path "${CMAKE_BINARY_DIR}/lib${LIB_SUFFIX}/libdruntime-ldc.a")
+    endif()
 else()
-    set(druntime_path "$<TARGET_FILE:druntime-ldc>")
     set(shared_druntime_path "$<TARGET_FILE:druntime-ldc${SHARED_LIB_SUFFIX}>")
+    if(${BUILD_SHARED_LIBS} STREQUAL "ON")
+        set(druntime_path ${shared_druntime_path})
+    else()
+        set(druntime_path "$<TARGET_FILE:druntime-ldc>")
+    endif()
+endif()
+
+if("${TARGET_SYSTEM}" MATCHES "FreeBSD")
+    set(linkdl "")
+else()
+    set(linkdl "LINKDL=-L-ldl")
 endif()
 
 get_subdirs(testnames ${PROJECT_SOURCE_DIR}/druntime/test)
-
-# 'shared' is a special case
-if(NOT ${BUILD_SHARED_LIBS} STREQUAL "OFF")
-    set(outdir ${PROJECT_BINARY_DIR}/druntime-test-shared)
-    add_test(NAME clean-druntime-test-shared
-        COMMAND ${CMAKE_COMMAND} -E remove_directory ${outdir}
-    )
-    if("${TARGET_SYSTEM}" MATCHES "FreeBSD")
-        set(linkflags "")
-    else()
-        set(linkflags "LINKDL=-L-ldl")
-    endif()
-    add_test(NAME druntime-test-shared
-        COMMAND make -C ${PROJECT_SOURCE_DIR}/druntime/test/shared
-            ROOT=${outdir} DMD=${LDMD_EXE_FULL}
-            MODEL=default DRUNTIMESO=${shared_druntime_path}
-            CFLAGS=-Wall\ -Wl,-rpath,${CMAKE_BINARY_DIR}/lib${LIB_SUFFIX} ${linkflags}
-    )
-    set_tests_properties(druntime-test-shared
-        PROPERTIES DEPENDS clean-druntime-test-shared
-    )
+if(${BUILD_SHARED_LIBS} STREQUAL "OFF")
+    list(REMOVE_ITEM testnames shared)
+elseif(${BUILD_SHARED_LIBS} STREQUAL "ON")
+    list(REMOVE_ITEM testnames cycles)
 endif()
-list(REMOVE_ITEM testnames shared)
 
 foreach(name ${testnames})
     set(outdir ${PROJECT_BINARY_DIR}/druntime-test-${name})
@@ -51,8 +47,10 @@ foreach(name ${testnames})
     )
     add_test(NAME druntime-test-${name}
         COMMAND make -C ${PROJECT_SOURCE_DIR}/druntime/test/${name}
-            ROOT=${outdir} DMD=${LDMD_EXE_FULL}
-            MODEL=default DRUNTIME=${druntime_path}
+            ROOT=${outdir} DMD=${LDMD_EXE_FULL} MODEL=default
+            DRUNTIME=${druntime_path} DRUNTIMESO=${shared_druntime_path}
+            CFLAGS=-Wall\ -Wl,-rpath,${CMAKE_BINARY_DIR}/lib${LIB_SUFFIX}
+            ${linkdl}
     )
     set_tests_properties(druntime-test-${name}
         PROPERTIES DEPENDS clean-druntime-test-${name}
