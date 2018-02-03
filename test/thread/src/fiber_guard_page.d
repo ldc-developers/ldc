1bc6c7fa95537b52afe4cd5c1f218d35aad9d888
Martin <noone@nowhere.com>
2018-02-03 15:29:51 +0100

    Adapt tests to LDC

--- test/thread/src/fiber_guard_page.d
+++ test/thread/src/fiber_guard_page.d
@@ -1,5 +1,6 @@
 import core.thread;
 import core.sys.posix.sys.mman;
+import ldc.attributes;
 
 // this should be true for most architectures
 // (taken from core.thread)
@@ -8,6 +9,7 @@ version = StackGrowsDown;
 enum stackSize = 4096;
 
 // Simple method that causes a stack overflow
+@optStrategy("none")
 void stackMethod()
 {
     // Over the stack size, so it overflows the stack
@@ -18,21 +20,34 @@ void main()
 {
     auto test_fiber = new Fiber(&stackMethod, stackSize);
 
+    auto getPrivateFiberField(string id)()
+    {
+        static size_t getIndex(string id)()
+        {
+            static foreach (i, field; Fiber.tupleof)
+            {
+                static if (field.stringof == id)
+                    return i;
+            }
+            assert(0);
+        }
+
+        enum i = getIndex!id();
+        return test_fiber.tupleof[i];
+    }
+
     // allocate a page below (above) the fiber's stack to make stack overflows possible (w/o segfaulting)
     version (StackGrowsDown)
     {
-        static assert(__traits(identifier, test_fiber.tupleof[8]) == "m_pmem");
-        auto stackBottom = test_fiber.tupleof[8];
+        auto stackBottom = getPrivateFiberField!"m_pmem"();
         auto p = mmap(stackBottom - 8 * stackSize, 8 * stackSize,
                       PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
         assert(p !is null, "failed to allocate page");
     }
     else
     {
-        auto m_sz = test_fiber.tupleof[7];
-        auto m_pmem = test_fiber.tupleof[8];
-        static assert(__traits(identifier, test_fiber.tupleof[7]) == "m_size");
-        static assert(__traits(identifier, test_fiber.tupleof[8]) == "m_pmem");
+        auto m_sz = getPrivateFiberField!"m_sz"();
+        auto m_pmem = getPrivateFiberField!"m_pmem"();
 
         auto stackTop = m_pmem + m_sz;
         auto p = mmap(stackTop, 8 * stackSize,
