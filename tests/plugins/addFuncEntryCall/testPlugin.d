6292afbd3b566096a8307a8ae5e2ab8650b09a68
Johan Engelen <jbc.engelen@gmail.com>
2018-02-05 23:10:31 +0100

    Path to plugin needs to be specified, apparently cwd is not included in dlopen path?

--- tests/plugins/addFuncEntryCall/testPlugin.d
+++ tests/plugins/addFuncEntryCall/testPlugin.d
@@ -1,7 +1,7 @@
 // REQUIRES: Plugins
 
 // RUN: make -f %S/Makefile
-// RUN: %ldc -c -output-ll -plugin=addFuncEntryCallPass.so -of=%t.ll %s
+// RUN: %ldc -c -output-ll -plugin=./addFuncEntryCallPass.so -of=%t.ll %s
 // RUN: FileCheck %s < %t.ll
 
 // CHECK: define {{.*}}testfunction
