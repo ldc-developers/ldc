dc47264359525c80fb8365bde33b837dd249c6e0
Johan Engelen <jbc.engelen@gmail.com>
2018-03-30 13:52:57 +0200

    Clarify that error messages relate only to DMD-style asm.

--- gen/asmstmt.cpp
+++ gen/asmstmt.cpp
@@ -108,12 +108,14 @@ Statement *asmSemantic(AsmStatement *s, Scope *sc) {
   llvm::Triple const &t = *global.params.targetTriple;
   if (!(t.getArch() == llvm::Triple::x86 ||
         t.getArch() == llvm::Triple::x86_64)) {
-    s->error("inline asm is not supported for the \"%s\" architecture",
-             t.getArchName().str().c_str());
+    s->error(
+        "DMD-style inline asm is not supported for the \"%s\" architecture",
+        t.getArchName().str().c_str());
     err = true;
   }
   if (!global.params.useInlineAsm) {
-    s->error("inline asm is not allowed when the -noasm switch is used");
+    s->error(
+        "DMD-style inline asm is not allowed when the -noasm switch is used");
     err = true;
   }
   if (err) {
