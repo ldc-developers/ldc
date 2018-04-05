aaf7fd6f3946a4cc7f06b7ad276f0daa008f3748
Johan Engelen <jbc.engelen@gmail.com>
2018-04-05 21:56:38 +0200

    Try to test asm output in another way for more platform independence (#2634)

--- tests/ir/asm_output.d
+++ tests/ir/asm_output.d
@@ -1,9 +1,10 @@
 // RUN: %ldc -c -output-ll -of=%t.ll %s && FileCheck %s --check-prefix LLVM < %t.ll
 // RUN: %ldc -c -output-s  -of=%t.s %s && FileCheck %s --check-prefix ASM < %t.s
 
-int main() {
-    return 42;
-// Try to keep these very simple checks independent of architecture:
+// Try to keep these very simple checks independent of architecture.
+
+// ASM: foofoofoofoo:
+extern(C) int foofoofoofoo() {
 // LLVM:  ret i32 42
-// ASM:  {{(\$|#|.long )}}42
+    return 42;
 }
