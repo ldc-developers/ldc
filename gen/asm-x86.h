7b3f7afdeb2d88b493fd4ee03c144b83a8087a8d
Martin Kinkelin <kinke@users.noreply.github.com>
2018-04-15 18:57:53 +0200

    MSVC: Fix `extended ptr` semantics in DMD-style inline asm (#2653)
    Don't map Extended_Ptr to Double_Ptr for MSVC targets with 64-bit reals.
    Instead, map reals to Double_Ptr directly, thereby still allowing to
    work with 80-bit extended precision values via `extended ptr`.

--- gen/asm-x86.h
+++ gen/asm-x86.h
@@ -2560,12 +2560,7 @@ struct AsmProcessor {
         type_suffix = 'l';
         break;
       case Extended_Ptr:
-        // MS C runtime: real = 64-bit double
-        if (global.params.targetTriple->isWindowsMSVCEnvironment()) {
-          type_suffix = 'l';
-        } else {
-          type_suffix = 't';
-        }
+        type_suffix = 't';
         break;
       default:
         return false;
@@ -3197,7 +3192,8 @@ struct AsmProcessor {
           // Tfloat64
           TY ty = v->type->toBasetype()->ty;
           operand->dataSizeHint =
-              ty == Tfloat80 || ty == Timaginary80
+              (ty == Tfloat80 || ty == Timaginary80) &&
+                      !global.params.targetTriple->isWindowsMSVCEnvironment()
                   ? Extended_Ptr
                   : static_cast<PtrType>(v->type->size(Loc()));
         }
@@ -3618,7 +3614,9 @@ struct AsmProcessor {
     case TOKfloat64:
       return Double_Ptr;
     case TOKfloat80:
-      return Extended_Ptr;
+      return global.params.targetTriple->isWindowsMSVCEnvironment()
+                 ? Double_Ptr
+                 : Extended_Ptr;
     case TOKidentifier:
       for (int i = 0; i < N_PtrNames; i++) {
         if (tok->ident == ptrTypeIdentTable[i]) {
