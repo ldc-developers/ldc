1ab0ffda40752bb5e722ad747586853546d3ba9a
Johan Engelen <jbc.engelen@gmail.com>
2018-04-27 16:00:28 +0200

    Use long dash as elsewhere

--- driver/cl_options-llvm.cpp
+++ driver/cl_options-llvm.cpp
@@ -1,6 +1,6 @@
 //===-- cl_options-llvm.cpp -----------------------------------------------===//
 //
-//                         LDC - the LLVM D compiler
+//                         LDC – the LLVM D compiler
 //
 // This file is distributed under the BSD-style LDC license. See the LICENSE
 // file for details.
