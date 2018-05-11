deec68d64251d52601259ba52ecbdd8d6a4fbbdf
Martin <noone@nowhere.com>
2018-05-11 19:14:33 +0200

    SemaphoreCI: Switch to Docker and Ubuntu 18.04

---
+++ .semaphoreci/runInDocker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+IMAGE=ubuntu:18.04
+
+docker pull $IMAGE
+docker run --rm \
+	-v $PWD:/ldc \
+	$IMAGE \
+	sh -c "cd /ldc && \
+	./.semaphoreci/setup.sh && \
+	./.semaphoreci/$1.sh"
--- .semaphoreci/setup.sh
+++ .semaphoreci/setup.sh
@@ -3,19 +3,23 @@
 set -euxo pipefail
 
 HOST_LDC_VERSION=1.9.0
-ls -al $SEMAPHORE_CACHE_DIR
-rm -rf $SEMAPHORE_CACHE_DIR/{ldc2-*,llvm*}
-ls -al $SEMAPHORE_CACHE_DIR
-git submodule update --init
-sudo apt-get update
-install-package ninja-build g++-4.9-multilib libcurl3:i386 libconfig++8-dev
+
+export DEBIAN_FRONTEND=noninteractive
+dpkg --add-architecture i386
+apt-get -q update
+apt-get -yq install git-core cmake ninja-build g++-multilib libcurl4 libcurl4:i386 curl gdb python-pip unzip zip
 pip install --user lit
-wget https://cmake.org/files/v3.11/cmake-3.11.1-Linux-x86_64.sh && sudo sh cmake-*.sh --prefix=/usr --skip-license && rm cmake-*.sh
-export CC=gcc-4.9
-export CXX=g++-4.9
-sudo update-alternatives --install /usr/bin/ld ld /usr/bin/ld.gold 99
-wget -O llvm.tar.xz https://github.com/ldc-developers/llvm/releases/download/ldc-v6.0.0/llvm-6.0.0-linux-x86_64-withAsserts.tar.xz && mkdir llvm-x64 && tar -xf llvm.tar.xz --strip 1 -C llvm-x64 && rm llvm.tar.xz
-wget https://github.com/ldc-developers/ldc/releases/download/v$HOST_LDC_VERSION/ldc2-$HOST_LDC_VERSION-linux-x86_64.tar.xz && tar -xf ldc2-*.tar.xz && rm ldc2-*.tar.xz
+update-alternatives --install /usr/bin/ld ld /usr/bin/ld.gold 99
+curl -L -o llvm.tar.xz https://github.com/ldc-developers/llvm/releases/download/ldc-v6.0.0/llvm-6.0.0-linux-x86_64-withAsserts.tar.xz && \
+	mkdir llvm-x64 && \
+	tar -xf llvm.tar.xz --strip 1 -C llvm-x64 && \
+	rm llvm.tar.xz
+curl -L -o ldc.tar.xz https://github.com/ldc-developers/ldc/releases/download/v$HOST_LDC_VERSION/ldc2-$HOST_LDC_VERSION-linux-x86_64.tar.xz && \
+	mkdir ldc-x64 && \
+	tar -xf ldc.tar.xz --strip 1 -C ldc-x64 && \
+	rm ldc.tar.xz
+
+git submodule update --init
 mkdir build
 cd build
-cmake -G Ninja -DMULTILIB=ON -DLDC_INSTALL_LTOPLUGIN=ON -DLDC_INSTALL_LLVM_RUNTIME_LIBS=ON -DLLVM_ROOT_DIR=$PWD/../llvm-x64 -DD_COMPILER=$PWD/../ldc2-$HOST_LDC_VERSION-linux-x86_64/bin/ldmd2 ..
+cmake -G Ninja -DMULTILIB=ON -DLDC_INSTALL_LTOPLUGIN=ON -DLDC_INSTALL_LLVM_RUNTIME_LIBS=ON -DLLVM_ROOT_DIR=$PWD/../llvm-x64 -DD_COMPILER=$PWD/../ldc-x64/bin/ldmd2 ..
