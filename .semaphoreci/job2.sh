84ff777a5c4065430a194227e2c211ec8ea17542
Martin <noone@nowhere.com>
2018-05-11 23:37:14 +0200

    SemaphoreCI: Distribute defaultlib unittests among jobs
    And make the first job attempt all tests instead of exiting after the
    first error.

--- .semaphoreci/job2.sh
+++ .semaphoreci/job2.sh
@@ -5,5 +5,15 @@ set -euxo pipefail
 free -m
 cd build
 ninja -j2
-ninja -j1 all-test-runners
-ctest -j4 --output-on-failure -E "dmd-testsuite|ldc2-unittest|lit-tests"
+
+# Build & run defaultlib release unittests.
+ninja -j1 \
+	druntime-test-runner \
+	druntime-test-runner_32 \
+	druntime-test-runner-shared \
+	druntime-test-runner-shared_32 \
+	phobos2-test-runner \
+	phobos2-test-runner_32 \
+	phobos2-test-runner-shared \
+	phobos2-test-runner-shared_32
+ctest -j4 --output-on-failure -E "ldc2-unittest|lit-tests|dmd-testsuite|-debug"
