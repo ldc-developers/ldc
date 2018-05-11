4f355d812428ddeda17f787279495aee22daff8b
Martin <noone@nowhere.com>
2018-05-11 20:14:32 +0200

    SemaphoreCI: Try using Ubuntu's LLVM 6 dev package
    As our prebuilt LDC-LLVM package isn't compatible (built on Ubuntu 14.04
    with gcc 6).

--- .semaphoreci/setup.sh
+++ .semaphoreci/setup.sh
@@ -7,13 +7,9 @@ HOST_LDC_VERSION=1.9.0
 export DEBIAN_FRONTEND=noninteractive
 dpkg --add-architecture i386
 apt-get -q update
-apt-get -yq install git-core cmake ninja-build g++-multilib libcurl4 libcurl4:i386 curl gdb python-pip unzip zip
+apt-get -yq install git-core cmake ninja-build g++-multilib llvm-dev zlib1g-dev libcurl4 libcurl4:i386 curl gdb python-pip unzip zip
 pip install --user lit
 update-alternatives --install /usr/bin/ld ld /usr/bin/ld.gold 99
-curl -L -o llvm.tar.xz https://github.com/ldc-developers/llvm/releases/download/ldc-v6.0.0/llvm-6.0.0-linux-x86_64-withAsserts.tar.xz && \
-	mkdir llvm-x64 && \
-	tar -xf llvm.tar.xz --strip 1 -C llvm-x64 && \
-	rm llvm.tar.xz
 curl -L -o ldc.tar.xz https://github.com/ldc-developers/ldc/releases/download/v$HOST_LDC_VERSION/ldc2-$HOST_LDC_VERSION-linux-x86_64.tar.xz && \
 	mkdir ldc-x64 && \
 	tar -xf ldc.tar.xz --strip 1 -C ldc-x64 && \
@@ -22,4 +18,4 @@ curl -L -o ldc.tar.xz https://github.com/ldc-developers/ldc/releases/download/v$
 git submodule update --init
 mkdir build
 cd build
-cmake -G Ninja -DMULTILIB=ON -DLDC_INSTALL_LTOPLUGIN=ON -DLDC_INSTALL_LLVM_RUNTIME_LIBS=ON -DLLVM_ROOT_DIR=$PWD/../llvm-x64 -DD_COMPILER=$PWD/../ldc-x64/bin/ldmd2 ..
+cmake -G Ninja -DMULTILIB=ON -DLDC_INSTALL_LTOPLUGIN=ON -DLDC_INSTALL_LLVM_RUNTIME_LIBS=ON -DD_COMPILER=$PWD/../ldc-x64/bin/ldmd2 ..
