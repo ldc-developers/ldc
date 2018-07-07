8ec1840d0a40366af413fa98ccbd9183e0fa9857
Martin Kinkelin <noone@nowhere.com>
2018-07-07 21:06:40 +0200

    Add lit-test for compiling & linking directly to WebAssembly

---
+++ tests/baremetal/wasm.d
@@ -0,0 +1,10 @@
+// Compile and link directly to WebAssembly.
+
+// REQUIRES: target_WebAssembly
+// RUN: %ldc -mtriple=wasm32-unknown-unknown-wasm -link-internally %s %baremetal_args
+
+extern(C): // no mangling, no arguments order reversal
+
+void _start() {}
+
+double add(double a, double b) { return a + b; }
