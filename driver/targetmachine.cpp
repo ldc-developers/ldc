d02dc5c7d345f160311000a683c891ac1afd5894
Martin Kinkelin <kinke@users.noreply.github.com>
2018-10-06 21:20:07 +0200

    Android: Prevent LLVM 7+ defaulting to incompatible emutls (#2863)

--- driver/targetmachine.cpp
+++ driver/targetmachine.cpp
@@ -528,6 +528,15 @@ createTargetMachine(const std::string targetTriple, const std::string arch,
     targetOptions.DataSections = true;
   }
 
+#if LDC_LLVM_VER >= 700
+  // On Android, we depend on a custom TLS emulation scheme implemented in our
+  // LLVM fork. LLVM 7+ enables regular emutls by default; prevent that.
+  if (triple.getEnvironment() == llvm::Triple::Android) {
+    targetOptions.EmulatedTLS = false;
+    targetOptions.ExplicitEmulatedTLS = true;
+  }
+#endif
+
   const std::string finalFeaturesString =
       llvm::join(features.begin(), features.end(), ",");
 
