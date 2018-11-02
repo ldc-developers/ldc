0e00487efe7eb5b58c09ecf18e3a6f6d9c85d871
Martin Kinkelin <noone@nowhere.com>
2018-11-02 02:53:39 +0100

    FindLLVM.cmake: Add llvm-config for v7.0 and v8.0

--- cmake/Modules/FindLLVM.cmake
+++ cmake/Modules/FindLLVM.cmake
@@ -28,7 +28,9 @@
 # We also want an user-specified LLVM_ROOT_DIR to take precedence over the
 # system default locations such as /usr/local/bin. Executing find_program()
 # multiples times is the approach recommended in the docs.
-set(llvm_config_names llvm-config-6.0 llvm-config60
+set(llvm_config_names llvm-config-8.0 llvm-config80
+                      llvm-config-7.0 llvm-config70
+                      llvm-config-6.0 llvm-config60
                       llvm-config-5.0 llvm-config50
                       llvm-config-4.0 llvm-config40
                       llvm-config-3.9 llvm-config39
