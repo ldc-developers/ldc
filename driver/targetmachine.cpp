0617ac83ff8cd11dded31aaaee0ed5f284aea0f2
myfreeweb <greg@unrelenting.technology>
2018-11-03 14:40:41 +0300

    Emit constructors as .init_array instead of .ctors when appropriate (#2883)
    AArch64 only supports .init_array, for example. The reason this wasn't discovered
    earlier is that BFD and gold linkers automatically convert .ctors to .init_array
    as a performance optimization.
    
    Cherry-pick from issue #2883

--- driver/targetmachine.cpp
+++ driver/targetmachine.cpp
@@ -469,6 +469,23 @@ createTargetMachine(const std::string targetTriple, const std::string arch,
     break;
   }
 
+  // Taken from clang's lib/Driver/ToolChains/Gnu.cpp
+  if (triple.getArch() == llvm::Triple::aarch64 ||
+      triple.getArch() == llvm::Triple::aarch64_be ||
+      (triple.getOS() == llvm::Triple::FreeBSD &&
+       triple.getOSMajorVersion() >= 12) ||
+      triple.getOS() == llvm::Triple::NaCl ||
+      (triple.getVendor() == llvm::Triple::MipsTechnologies &&
+       !triple.hasEnvironment()) ||
+      triple.getOS() == llvm::Triple::Solaris
+#if LDC_LLVM_VER >= 400
+      || triple.getArch() == llvm::Triple::riscv32
+      || triple.getArch() == llvm::Triple::riscv64
+#endif
+      ) {
+    targetOptions.UseInitArray = true;
+  }
+
   // Right now, we only support linker-level dead code elimination on Linux
   // using the GNU toolchain (based on ld's --gc-sections flag). The Apple ld
   // on OS X supports a similar flag (-dead_strip) that doesn't require
