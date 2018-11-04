d0eb1af5476d60cd9ac039270b32ada2f35fd15f
Martin Kinkelin <kinke@users.noreply.github.com>
2018-11-04 23:19:01 +0100

    Fixup header too...

--- driver/cl_options.h
+++ driver/cl_options.h
@@ -77,7 +77,7 @@ extern cl::opt<std::string> mABI;
 extern FloatABI::Type floatABI;
 extern cl::opt<bool> linkonceTemplates;
 extern cl::opt<bool> disableLinkerStripDead;
-extern cl::opt<bool> defaultToHiddenVisibility;
+extern cl::opt<ubyte> defaultToHiddenVisibility;
 
 // Math options
 extern bool fFastMath;
