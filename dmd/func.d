2014f66cfd364e0f1f172a9fa234ee12d59e719b
Martin Kinkelin <noone@nowhere.com>
2018-12-09 00:22:25 +0100

    Merge v2.083.1

--- dmd/func.d
+++ dmd/func.d
@@ -2906,7 +2906,8 @@ FuncDeclaration resolveFuncCall(const ref Loc loc, Scope* sc, Dsymbol s,
         {
             assert(fd);
 
-            if (fd.checkDisabled(loc, sc))
+            // remove when deprecation period of class allocators and deallocators is over
+            if (fd.isNewDeclaration() && fd.checkDisabled(loc, sc))
                 return null;
 
             bool hasOverloads = fd.overnext !is null;
