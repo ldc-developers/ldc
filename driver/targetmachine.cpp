3df77e9bf1c8acee4b0bc0ce66766461a3318361
Greg V <greg@unrelenting.technology>
2018-12-14 01:42:42 +0300

    Always use PIC on FreeBSD, just like on Linux
    This fixes errors like 'can't create dynamic relocation R_AARCH64_...'

--- driver/targetmachine.cpp
+++ driver/targetmachine.cpp
@@ -423,6 +423,10 @@ createTargetMachine(const std::string targetTriple, const std::string arch,
       // these OSes.
       // On Android, PIC is default as well.
       relocModel = llvm::Reloc::PIC_;
+    } else if (triple.isOSFreeBSD()) {
+      // We default to PIC code to avoid linking issues on FreeBSD, especially
+      // on aarch64.
+      relocModel = llvm::Reloc::PIC_;
     } else {
       // ARM for other than Darwin or Android defaults to static
       switch (triple.getArch()) {
