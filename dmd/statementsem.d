0fc069bf683b5fc75a39c7e8a8e974f4493f481e
Martin Kinkelin <noone@nowhere.com>
2019-06-20 10:03:51 +0200

    Merge v2.086.1

--- dmd/statementsem.d
+++ dmd/statementsem.d
@@ -2599,7 +2599,7 @@ else
         ss._body = ss._body.statementSemantic(sc);
         sc.inLoop = inLoopSave;
 
-        if (conditionError || ss._body.isErrorStatement())
+        if (conditionError || (ss._body && ss._body.isErrorStatement()))
         {
             sc.pop();
             return setError();
@@ -2674,7 +2674,7 @@ version (IN_LLVM)
         {
             ss.hasNoDefault = 1;
 
-            if (!ss.isFinal && !ss._body.isErrorStatement())
+            if (!ss.isFinal && (!ss._body || !ss._body.isErrorStatement()))
                 ss.error("`switch` statement without a `default`; use `final switch` or add `default: assert(0);` or add `default: break;`");
 
             // Generate runtime error if the default is hit
