3d00a4b5a67482760642b48e69e193985661caea
Ivan <ivan.butygin@gmail.com>
2019-06-30 18:42:22 +0300

    fix static array handling

--- tests/dynamiccompile/bind_opt.d
+++ tests/dynamiccompile/bind_opt.d
@@ -42,6 +42,12 @@ int zzz(int a, int b, int c)
   return a + b * 10 + c * 100;
 }
 
+@dynamicCompileEmit
+int baz(int[4] a)
+{
+    return a[0] + a[1] * 10 + a[2] * 100 + a[3] * 1000;
+}
+
 void main(string[] args)
 {
   auto dump = appender!string();
@@ -62,20 +68,24 @@ void main(string[] args)
   stdout.flush();
 
   auto elem = Bar(Foo(1,3),5);
+  int[4] arr = [2,6,8,9];
 
   auto f = ldc.dynamic_compile.bind(&foo, 1, 2, 3);
   auto b = ldc.dynamic_compile.bind(&bar, Bar(Foo(4,5),6));
   auto z = ldc.dynamic_compile.bind(&foo, 7, 8, 9);
   auto e = ldc.dynamic_compile.bind(&elem.get, 7);
+  auto a = ldc.dynamic_compile.bind(&baz, arr);
 
   compileDynamicCode(settings);
-  
+
   assert(321 == f());
   assert(654 == b());
   assert(987 == z());
   assert(7531 == e());
+  assert(9862 == a());
   assert(indexOf(dump.data, "321") != -1);
   assert(indexOf(dump.data, "654") != -1);
   assert(indexOf(dump.data, "987") != -1);
   //assert(indexOf(dump.data, "7531") != -1); // TODO: doesn't properly optimized yet
+  assert(indexOf(dump.data, "9862") != -1);
 }
