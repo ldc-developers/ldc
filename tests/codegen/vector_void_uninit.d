cfa2dd2ecd8be6fc14ca3a14b196ec7b5caaa1a3
Stefanos Baziotis <sdi1600105@di.uoa.gr>
2019-08-14 00:52:09 +0300

    Improved test case

--- tests/codegen/vector_void_uninit.d
+++ tests/codegen/vector_void_uninit.d
@@ -1,7 +1,13 @@
-// RUN: %ldc -c %s
+// RUN: %ldc -c -output-ll -of=%t.ll %s && FileCheck %s < %t.ll
 
 void foo()
 {
-    alias V = __vector(void[16]);
-    V v;
+// CHECK: %v1 = alloca <2 x i8>, align 2
+// CHECK-SAME: size/byte = 2]
+// CHECK: %v2 = alloca <4 x i8>, align 4
+// CHECK-SAME: size/byte = 4]
+// CHECK-COUNT-2: store i8 0, i8*
+// CHECK-COUNT-4: store i8 0, i8*
+    __vector(void[2]) v1;
+    __vector(void[4]) v2;
 }
