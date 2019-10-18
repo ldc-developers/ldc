b461dac7ea71fc240c13943d038122e29d855631
Martin Kinkelin <noone@nowhere.com>
2019-10-18 03:52:06 +0200

    Fix front-end C++ headers

--- dmd/globals.h
+++ dmd/globals.h
@@ -244,6 +244,7 @@ struct Param
     // Linker stuff
     Array<const char *> objfiles;
     Array<const char *> linkswitches;
+    Array<bool> linkswitchIsForCC;
     Array<const char *> libfiles;
     Array<const char *> dllfiles;
     DString deffile;
