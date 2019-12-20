f9ce9a79e38f73a82e7a3d1a8fc140dee0b74bbb
Roberto Rosmaninho <robertogrosmaninho@outlook.com>
2019-12-20 11:25:47 -0300

    Dialect new Ops: CmpOp(std)

---
+++ gen/MLIR/Passes.h
@@ -0,0 +1,27 @@
+//
+// Created by Roberto Rosmaninho on 18/12/19.
+//
+
+// =============================================================================
+//
+// This file exposes the entry points to create compiler passes for D.
+//
+//===----------------------------------------------------------------------===/
+
+#ifndef LDC_PASSES_H
+#define LDC_PASSES_H
+
+#include <memory>
+
+namespace mlir{
+  class Pass;
+
+  namespace D{
+    /// Create a pass for lowering to operations in the `Affine` and `Std`
+    /// dialects, for a subset of the D IR (e.g. matmul).
+    std::unique_ptr<mlir::Pass> createLowerToAffinePass();
+  } // end namespace D
+} // end namespace mlir
+
+
+#endif //LDC_PASSES_H
