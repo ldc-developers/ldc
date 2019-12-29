80225d1368c49382622a609e4e5cce8f9d643fc0
Fangrui Song <i@maskray.me>
2019-12-28 19:02:59 -0800

    Add assembly tests for "frame-pointer"

---
+++ tests/codegen/frame_pointer_x86.d
@@ -0,0 +1,25 @@
+// REQUIRES: target_X86
+
+// RUN: %ldc -c -mtriple=x86_64 -output-s -of=%t.s %s
+// RUN: FileCheck %s --check-prefixes=COMMON,FP < %t.s
+// RUN: %ldc -c -mtriple=x86_64 -output-s -of=%t.s %s -O2
+// RUN: FileCheck %s --check-prefixes=COMMON,NO_FP < %t.s
+// RUN: %ldc -c -mtriple=x86_64 -output-s -of=%t.s %s -O2 %disable_fp_elim
+// RUN: FileCheck %s --check-prefixes=COMMON,FP < %t.s
+// RUN: %ldc -c -mtriple=x86_64 -output-s -of=%t.s %s %enable_fp_elim
+// RUN: FileCheck %s --check-prefixes=COMMON,NO_FP < %t.s
+
+// COMMON-LABEL: _D17frame_pointer_x8613inlineAsmLeafFZv:
+// COMMON:       pushq %rbp
+// COMMON-LABEL: _D17frame_pointer_x8616inlineAsmNonLeafFZv:
+// COMMON:       pushq %rbp
+
+// COMMON-LABEL: _D17frame_pointer_x863fooFZv:
+// FP:           pushq %rbp
+// NO_FP-NOT:    pushq %rbp
+
+void externalFunc();
+void inlineAsmLeaf() { asm { nop; } }
+void inlineAsmNonLeaf() { asm { nop; } externalFunc(); }
+
+void foo() {}
