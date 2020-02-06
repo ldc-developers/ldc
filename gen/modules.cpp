7ed6803154fd072606f2f235b110053d8cb5c4fb
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2020-02-06 05:14:24 -0300

    Removing unnecessary whitespaces

--- gen/modules.cpp
+++ gen/modules.cpp
@@ -543,17 +543,14 @@ void loadInstrProfileData(IRState *irs) {
     irs->PGOReader = std::move(readerOrErr.get());
 
     if (!irs->module.getProfileSummary(
-
 #if LDC_LLVM_VER >= 900
             /*is context sensitive profile=*/false
 #endif
             )) {
-
       // Don't reset the summary. There is only one profile data file per LDC
       // invocation so the summary must be the same as the one that is already
       // set.
       irs->module.setProfileSummary(
-
 #if LDC_LLVM_VER >= 900
           irs->PGOReader->getSummary(/*is context sensitive profile=*/false)
               .getMD(irs->context()),
@@ -562,7 +559,6 @@ void loadInstrProfileData(IRState *irs) {
           irs->PGOReader->getSummary().getMD(irs->context())
 #endif
       );
-
     }
   }
 }
