e9561e198485a516b1add91aa1b0e052eef82cf6
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2020-02-24 12:55:20 +0800

    llvm config names.

--- cmake/Modules/FindLLVM.cmake
+++ cmake/Modules/FindLLVM.cmake
@@ -30,7 +30,8 @@
 # We also want an user-specified LLVM_ROOT_DIR to take precedence over the
 # system default locations such as /usr/local/bin. Executing find_program()
 # multiples times is the approach recommended in the docs.
-set(llvm_config_names llvm-config-10.0 llvm-config100 llvm-config-10
+set(llvm_config_names llvm-config-11.0 llvm-config110 llvm-config-11
+                      llvm-config-10.0 llvm-config100 llvm-config-10
                       llvm-config-9.0 llvm-config90 llvm-config-9
                       llvm-config-8.0 llvm-config80 llvm-config-8
                       llvm-config-7.0 llvm-config70 llvm-config-7
