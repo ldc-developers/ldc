948604c6d06c8a8629988e5314104762a9c22685
Johan Engelen <jbc.engelen@gmail.com>
2020-03-17 20:37:55 +0100

    CMake: Fix finding MLIR.

--- cmake/Modules/FindMLIR.cmake
+++ cmake/Modules/FindMLIR.cmake
@@ -1,68 +1,54 @@
 # - Try to find MLIR project at LLVM
 #
 # The following are set after configuration is done:
-#   MLIR_FOUND
-#   MLIR_ROOT_DIRS
-#   MLIR_INCLUDE_DIRS
-#   MLIR_BUILD_INCLUDE_DIR
-project(ldc)
+#   MLIR_FOUND   TRUE if MLIR installation was found
+#   MLIR_ROOT_DIR
+#   MLIR_INCLUDE_DIR
+#   MLIR_LIB_DIR
+#   MLIR_LIBRARIES
+#   MLIR_TABLEGEN  The mlir-tblgen executable
 
-find_path(MLIR_ROOT_DIR NAMES "LICENSE.TXT" HINTS ${LLVM_ROOT_DIR}/../mlir)
+set(MLIR_FOUND "FALSE")
 
-#Used to get the main header files
-find_path(MLIR_INCLUDE_DIR NAMES "Parser.h" HINTS ${MLIR_ROOT_DIR}/include/mlir)
+# We only want to find an MLIR version that is compatible with our LLVM version,
+# so for now only look in the same installation dir as LLVM.
+find_program(MLIR_TABLEGEN
+    NAMES mlir-tblgen
+    PATHS ${MLIR_ROOT_DIR}/bin ${LLVM_ROOT_DIR}/bin NO_DEFAULT_PATH
+    DOC "Path to mlir-tblgen tool.")
 
-#Lib directories
-find_path(MLIR_LIB_DIR NAMES "CMakeLists.txt" HINTS ${MLIR_ROOT_DIR}/lib/IR)
+if(NOT MLIR_TABLEGEN)
+    message(STATUS "Could not find mlir-tblgen. Try manually setting MLIR_ROOT_DIR or MLIR_TABLEGEN.")
+else()
+    set(MLIR_FOUND "TRUE")
+    message(STATUS "Found mlir-tblgen: ${MLIR_TABLEGEN}")
+    get_filename_component(MLIR_BIN_DIR ${MLIR_TABLEGEN} DIRECTORY CACHE)
+    get_filename_component(MLIR_ROOT_DIR "${MLIR_BIN_DIR}/.." ABSOLUTE CACHE)
+    set(MLIR_INCLUDE_DIR ${MLIR_ROOT_DIR}/include)
+    set(MLIR_LIB_DIR     ${MLIR_ROOT_DIR}/lib)
 
-#Used to get StandardOps.h.inc
-find_path(MLIR_BUILD_INCLUDE_DIR NAMES "cmake_install.cmake"
-        HINTS ${LLVM_ROOT_DIR}/tools/mlir/include/mlir)
+    # To be done: add the required MLIR libraries. Hopefully we don't have to manually list all MLIR libs.
+    set(MLIR_LIBRARIES "")
 
-message(STATUS "MLIR Dir: ${MLIR_ROOT_DIR}")
-message(STATUS "MLIR Include Dir: ${MLIR_INCLUDE_DIR}/..")
-message(STATUS "MLIR Lib Dir: ${MLIR_LIB_DIR}/..")
-message(STATUS "MLIR Build Include Dir: ${MLIR_BUILD_INCLUDE_DIR}/..")
+    # XXX: This function is untested and will need adjustment.
+    function(mlir_tablegen)
+        cmake_parse_arguments(
+         ARG
+         "NAME"
+         "TARGET;OUTS;FLAG;SRCS"
+         ${ARGN}
+         )
 
-set(MLIR_ROOT_DIRS ${MLIR_ROOT_DIR})
-set(MLIR_INCLUDE_DIRS ${MLIR_INCLUDE_DIR}/..)
-set(MLIR_BUILD_INCLUDE_DIRS ${MLIR_BUILD_INCLUDE_DIR}/..)
-set(MLIR_LIB_DIRS ${MLIR_LIB_DIR}/..)
+        MESSAGE(STATUS "Setting target for Ops_" ${ARG_TARGET})
 
-if(EXISTS ${MLIR_ROOT_DIR})
-#Resources to automatically generate Ops.h.inc and Ops.cpp.inc, assuming that
-#mlir-tblgen is already built and is on the usual directory at llvm dir
-set(MLIR_MAIN_SRC_DIR ${MLIR_INCLUDE_DIR}/.. )
-set(MLIR_TABLEGEN_EXE ${LLVM_ROOT_DIR}/bin/mlir-tblgen)
-set(MLIR_INCLUDE_DIR ${MLIR_BUILD_INCLUDE_DIR}/..)
-
-function(mlir_tablegen)
-    cmake_parse_arguments(
-     ARG
-     "NAME"
-     "TARGET;OUTS;FLAG;SRCS"
-     ${ARGN}
-     )
-
-    MESSAGE(STATUS "Setting target for Ops_" ${ARG_TARGET})
-
-    set(TABLEGEN_OUTPUT ${TABLEGEN_OUTPUT} ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_SRCS}
-            PARENT_SCOPE)
-    #mlir-tblgen ops.td --gen-op-* -I*-o=ops.*.inc
-    add_custom_command(
-            OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_OUTS}
-            COMMAND ${MLIR_TABLEGEN_EXE} ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_SRCS} -I${MLIR_MAIN_SRC_DIR} -I${MLIR_INCLUDE_DIR}  -o=${CMAKE_CURRENT_SOURCE_DIR}/${ARG_OUTS}
-            ARGS ${ARG_FLAG}
-    )
-    add_custom_target(Ops_${ARG_TARGET} ALL DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_OUTS})
-endfunction()
-
-# Handle the QUIETLY and REQUIRED arguments and set the MLIR_FOUND to TRUE
-# if all listed variables are TRUE
-include(FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args(MLIR DEFAULT_MSG MLIR_ROOT_DIRS MLIR_INCLUDE_DIRS
-                                            MLIR_BUILD_INCLUDE_DIRS MLIR_LIB_DIRS)
-
-mark_as_advanced(MLIR_ROOT_DIRS MLIR_INCLUDE_DIRS MLIR_BUILD_INCLUDE_DIRS MLIR_LIB_DIRS)
+        set(TABLEGEN_OUTPUT ${TABLEGEN_OUTPUT} ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_SRCS}
+                PARENT_SCOPE)
+        #mlir-tblgen ops.td --gen-op-* -I*-o=ops.*.inc
+        add_custom_command(
+                OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_OUTS}
+                COMMAND ${MLIR_TABLEGEN} ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_SRCS} -I${MLIR_INCLUDE_DIR}  -o=${CMAKE_CURRENT_SOURCE_DIR}/${ARG_OUTS}
+                ARGS ${ARG_FLAG}
+        )
+        add_custom_target(Ops_${ARG_TARGET} ALL DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_OUTS})
+    endfunction()
 endif()
