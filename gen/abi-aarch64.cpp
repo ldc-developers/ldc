31b70d97dea05a242d459bf01959a72e3039dbe8
Martin Kinkelin <noone@nowhere.com>
2020-03-28 20:42:55 +0100

    Revise AArch64 ABI

--- gen/abi-aarch64.cpp
+++ gen/abi-aarch64.cpp
@@ -21,6 +21,8 @@
 #include "gen/abi-generic.h"
 
 /**
+ * Based on https://github.com/ARM-software/abi-aa/blob/master/aapcs64/aapcs64.rst.
+ *
  * The AAPCS64 uses a special native va_list type:
  *
  * typedef struct __va_list {
@@ -40,10 +42,15 @@
 struct AArch64TargetABI : TargetABI {
 private:
   const bool isDarwin;
-  IndirectByvalRewrite byvalRewrite;
+  IndirectByvalRewrite indirectByvalRewrite;
+  ImplicitByvalRewrite byvalRewrite;
   HFVAToArray hfvaToArray;
   CompositeToArray64 compositeToArray64;
   IntegerRewrite integerRewrite;
+
+  RegCount &getRegCount(IrFuncTy &fty) {
+    return reinterpret_cast<RegCount &>(fty.tag);
+  }
 
   bool isAAPCS64VaList(Type *t) {
     return !isDarwin && t->ty == Tstruct &&
@@ -53,8 +60,8 @@ private:
 
   bool passIndirectlyByValue(Type *t) {
     t = t->toBasetype();
-    return t->ty == Tsarray ||
-           (t->ty == Tstruct && t->size() > 16 && !isHFVA(t));
+    return (t->ty == Tstruct || t->ty == Tsarray) &&
+           (!isPOD(t) || (t->size() > 16 && !isHFVA(t)));
   }
 
 public:
@@ -66,56 +73,122 @@ public:
     }
 
     Type *rt = tf->next->toBasetype();
-
-    if (!isPOD(rt))
-      return true;
-
     return passIndirectlyByValue(rt);
   }
 
   bool passByVal(TypeFunction *, Type *) override { return false; }
 
   void rewriteFunctionType(IrFuncTy &fty) override {
-    Type *rt = fty.ret->type->toBasetype();
-    if (!fty.ret->byref && rt->ty != Tvoid) {
-      rewriteArgument(fty, *fty.ret, true);
+    RegCount &regCount = getRegCount(fty);
+    regCount = RegCount(8, 8); // initialize
+
+    // return value
+    if (!fty.ret->byref && fty.ret->type->toBasetype()->ty != Tvoid) {
+      RegCount dummy = regCount;
+      rewriteArgument(fty, *fty.ret, dummy, /*isReturnVal=*/true);
     }
 
-    for (auto arg : fty.args) {
-      if (!arg->byref) {
-        rewriteArgument(fty, *arg);
-      }
+    // implicit parameters taking up GP registers
+    /* the sret pointer is passed in a dedicated register (x8)
+    if (fty.arg_sret)
+      regCount.gp_regs--;
+    */
+    if (fty.arg_this || fty.arg_nest)
+      regCount.gp_regs--;
+    if (fty.arg_objcSelector)
+      regCount.gp_regs--;
+    if (fty.arg_arguments)
+      regCount.gp_regs -= 2; // slice
+
+    int begin = 0, end = fty.args.size(), step = 1;
+    if (fty.reverseParams) {
+      begin = end - 1;
+      end = -1;
+      step = -1;
     }
+    for (int i = begin; i != end; i += step) {
+      IrFuncTyArg &arg = *fty.args[i];
+
+      if (arg.byref) {
+        if (regCount.gp_regs > 0 && !arg.isByVal())
+          regCount.gp_regs--;
+
+        continue;
+      }
+
+      rewriteArgument(fty, arg, regCount);
+    }
+
+    // regCount (fty.tag) is now in the state after all implicit & formal args,
+    // ready to serve as initial state for each vararg call site, see below
   }
 
-  void rewriteArgument(IrFuncTy &fty, IrFuncTyArg &arg, bool isReturnVal) {
-    const auto t = arg.type->toBasetype();
+  void rewriteVarargs(IrFuncTy &fty,
+                      std::vector<IrFuncTyArg *> &args) override {
+    // use a dedicated RegCount copy for each call site and initialize it with
+    // fty.tag
+    RegCount regCount = getRegCount(fty);
 
-    if (t->ty != Tstruct && t->ty != Tsarray)
-      return;
-
-    if (!isReturnVal && isAAPCS64VaList(t)) {
-      // compiler magic: pass va_list args implicitly by reference
-      arg.byref = true;
-      arg.ltype = arg.ltype->getPointerTo();
-    } else if (!isReturnVal && passIndirectlyByValue(t)) {
-      byvalRewrite.applyTo(arg);
-    }
-    // Rewrite HFAs only because union HFAs are turned into IR types that are
-    // non-HFA and messes up register selection
-    else if (t->ty == Tstruct && isHFVA(t, &arg.ltype)) {
-      hfvaToArray.applyTo(arg, arg.ltype);
-    } else {
-      if (isReturnVal) {
-        integerRewrite.applyTo(arg);
-      } else {
-        compositeToArray64.applyTo(arg);
-      }
+    for (auto arg : args) {
+      if (!arg->byref) // don't rewrite ByVal arguments
+        rewriteArgument(fty, *arg, regCount);
     }
   }
 
   void rewriteArgument(IrFuncTy &fty, IrFuncTyArg &arg) override {
-    rewriteArgument(fty, arg, false);
+    llvm_unreachable("Please use the other overload explicitly.");
+  }
+
+  void rewriteArgument(IrFuncTy &fty, IrFuncTyArg &arg, RegCount &regCount,
+                       bool isReturnVal = false) {
+    LLType *originalLType = arg.ltype;
+    Type *t = arg.type->toBasetype();
+
+    // compiler magic: pass va_list args implicitly by reference
+    if (!isReturnVal && isAAPCS64VaList(t)) {
+      arg.byref = true;
+      arg.ltype = arg.ltype->getPointerTo();
+      if (regCount.gp_regs > 0)
+        regCount.gp_regs--;
+
+      return;
+    }
+
+    // non-PODs and bigger aggregates are passed as pointer to hidden copy
+    if (passIndirectlyByValue(t)) {
+      indirectByvalRewrite.applyTo(arg);
+      if (regCount.gp_regs > 0)
+        regCount.gp_regs--;
+
+      return;
+    }
+
+    LLType *hfvaType = nullptr;
+    if (isHFVA(t, &hfvaType)) {
+      // pass in SIMD registers (if enough are available for the whole aggregate)
+      hfvaToArray.applyToIfNotObsolete(arg, hfvaType);
+    } else if (t->ty == Tstruct || t->ty == Tsarray) {
+      // pass remaining structs in 1 or 2 GP registers (if enough are available)
+      if (canRewriteAsInt(t)) {
+        integerRewrite.applyToIfNotObsolete(arg);
+      } else {
+        compositeToArray64.applyToIfNotObsolete(arg);
+      }
+      /* if 16-bytes aligned, the 1st GP register must be an even one
+      if ((regCount.gp_regs & 1) && DtoAlignment(t) >= 16)
+        regCount.gp_regs--;
+      */
+    }
+
+    if (regCount.trySubtract(arg) == RegCount::ArgumentWouldFitInPartially) {
+      // pass the LL aggregate with byval attribute to prevent LLVM from passing
+      // it partially in registers, partially in memory
+      assert(originalLType->isAggregateType());
+      IF_LOG Logger::cout()
+          << "Passing byval to prevent register/memory mix: "
+          << arg.type->toChars() << " (" << *originalLType << ")\n";
+      byvalRewrite.applyTo(arg);
+    }
   }
 
   Type *vaListType() override {
