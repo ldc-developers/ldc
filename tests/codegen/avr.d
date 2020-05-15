bfda6cc8bdf4aef62b3beae2279622755fdf366b
Martin Kinkelin <noone@nowhere.com>
2020-05-15 15:03:39 +0200

    [squash] add tests

--- tests/codegen/avr.d
+++ tests/codegen/avr.d
@@ -1,9 +1,26 @@
-// REQUIRES: atleast_llvm400, target_AVR
+// REQUIRES: atleast_llvm800, target_AVR
 
 // RUN: %ldc -mtriple=avr -betterC -output-ll -of=%t.ll %s && FileCheck %s < %t.ll
 
+
+// test predefined versions:
+
 version (AVR) {} else static assert(0);
 version (D_SoftFloat) {} else static assert(0);
+
+
+// test address space of global ctor/dtor function pointers:
+
+// CHECK: @llvm.global_ctors = appending global [1 x { i32, void () addrspace(1)*, i8* }]
+// CHECK-SAME: @_D3avr4ctorFZv
+pragma(crt_constructor)
+void ctor() {}
+
+// CHECK: @llvm.global_dtors = appending global [1 x { i32, void () addrspace(1)*, i8* }]
+// CHECK-SAME: @_D3avr4dtorFZv
+pragma(crt_destructor)
+void dtor() {}
+
 
 // make sure TLS globals are emitted as regular __gshared globals:
 
