53b93e40eddd2913db85ccd0a18230e43d16157f
Martin Kinkelin <noone@nowhere.com>
2020-05-21 18:02:15 +0200

    core.math: Adopt fast ldexp implementation from libmir
    On Win64 and its double-precision real, it seems to be roughly 14x
    faster than Microsoft's C ldexp on my box (i5-3550).
    
    On Ubuntu 20.04, the speed-up is something like 9x, again for double
    precision. Double and single precision versions aren't enabled yet, but
    will be with LDC v1.23 (already available in upstream druntime master).

--- src/core/math.d
+++ src/core/math.d
@@ -138,9 +138,175 @@ extern (C) real rndtonl(real x);
 
 version (LDC)
 {
+    pragma(inline, true):
+
+    // Implementation from libmir:
+    // https://github.com/libmir/mir-core/blob/master/source/mir/math/ieee.d
+    private T ldexpImpl(T)(const T n, int exp) @trusted pure nothrow
+    {
+        enum RealFormat { ieeeSingle, ieeeDouble, ieeeExtended, ieeeQuadruple }
+
+             static if (T.mant_dig ==  24) enum realFormat = RealFormat.ieeeSingle;
+        else static if (T.mant_dig ==  53) enum realFormat = RealFormat.ieeeDouble;
+        else static if (T.mant_dig ==  64) enum realFormat = RealFormat.ieeeExtended;
+        else static if (T.mant_dig == 113) enum realFormat = RealFormat.ieeeQuadruple;
+        else static assert(false, "Unsupported format for " ~ T.stringof);
+
+        version (LittleEndian)
+        {
+            enum MANTISSA_LSB = 0;
+            enum MANTISSA_MSB = 1;
+        }
+        else
+        {
+            enum MANTISSA_LSB = 1;
+            enum MANTISSA_MSB = 0;
+        }
+
+        static if (realFormat == RealFormat.ieeeExtended)
+        {
+            alias S = int;
+            alias U = ushort;
+            enum sig_mask = U(1) << (U.sizeof * 8 - 1);
+            enum exp_shft = 0;
+            enum man_mask = 0;
+            version (LittleEndian)
+                enum idx = 4;
+            else
+                enum idx = 0;
+        }
+        else
+        {
+            static if (realFormat == RealFormat.ieeeQuadruple || realFormat == RealFormat.ieeeDouble && double.sizeof == size_t.sizeof)
+            {
+                alias S = long;
+                alias U = ulong;
+            }
+            else
+            {
+                alias S = int;
+                alias U = uint;
+            }
+            static if (realFormat == RealFormat.ieeeQuadruple)
+                alias M = ulong;
+            else
+                alias M = U;
+            enum sig_mask = U(1) << (U.sizeof * 8 - 1);
+            enum uint exp_shft = T.mant_dig - 1 - (T.sizeof > U.sizeof ? U.sizeof * 8 : 0);
+            enum man_mask = (U(1) << exp_shft) - 1;
+            enum idx = T.sizeof > U.sizeof ? MANTISSA_MSB : 0;
+        }
+        enum exp_mask = (U.max >> (exp_shft + 1)) << exp_shft;
+        enum int exp_msh = exp_mask >> exp_shft;
+        enum intPartMask = man_mask + 1;
+
+        import core.checkedint : adds;
+        alias _expect = llvm_expect;
+
+        enum norm_factor = 1 / T.epsilon;
+        T vf = n;
+
+        auto u = (cast(U*)&vf)[idx];
+        int e = (u & exp_mask) >> exp_shft;
+        if (_expect(e != exp_msh, true))
+        {
+            if (_expect(e == 0, false)) // subnormals input
+            {
+                bool overflow;
+                vf *= norm_factor;
+                u = (cast(U*)&vf)[idx];
+                e = int((u & exp_mask) >> exp_shft) - (T.mant_dig - 1);
+            }
+            bool overflow;
+            exp = adds(exp, e, overflow);
+            if (_expect(overflow || exp >= exp_msh, false)) // infs
+            {
+                static if (realFormat == RealFormat.ieeeExtended)
+                {
+                    return vf * T.infinity;
+                }
+                else
+                {
+                    u &= sig_mask;
+                    u ^= exp_mask;
+                    static if (realFormat == RealFormat.ieeeExtended)
+                    {
+                        version (LittleEndian)
+                            auto mp = cast(ulong*)&vf;
+                        else
+                            auto mp = cast(ulong*)((cast(ushort*)&vf) + 1);
+                        *mp = 0;
+                    }
+                    else
+                    static if (T.sizeof > U.sizeof)
+                    {
+                        (cast(U*)&vf)[MANTISSA_LSB] = 0;
+                    }
+                }
+            }
+            else
+            if (_expect(exp > 0, true)) // normal
+            {
+                u = cast(U)((u & ~exp_mask) ^ (cast(typeof(U.init + 0))exp << exp_shft));
+            }
+            else // subnormal output
+            {
+                exp = 1 - exp;
+                static if (realFormat != RealFormat.ieeeExtended)
+                {
+                    auto m = u & man_mask;
+                    if (exp > T.mant_dig)
+                    {
+                        exp = T.mant_dig;
+                        static if (T.sizeof > U.sizeof)
+                            (cast(U*)&vf)[MANTISSA_LSB] = 0;
+                    }
+                }
+                u &= sig_mask;
+                static if (realFormat == RealFormat.ieeeExtended)
+                {
+                    version (LittleEndian)
+                        auto mp = cast(ulong*)&vf;
+                    else
+                        auto mp = cast(ulong*)((cast(ushort*)&vf) + 1);
+                    if (exp >= ulong.sizeof * 8)
+                        *mp = 0;
+                    else
+                        *mp >>>= exp;
+                }
+                else
+                {
+                    m ^= intPartMask;
+                    static if (T.sizeof > U.sizeof)
+                    {
+                        int exp2 = exp - int(U.sizeof) * 8;
+                        if (exp2 < 0)
+                        {
+                            (cast(U*)&vf)[MANTISSA_LSB] = ((cast(U*)&vf)[MANTISSA_LSB] >> exp) ^ (m << (U.sizeof * 8 - exp));
+                            m >>>= exp;
+                            u ^= cast(U) m;
+                        }
+                        else
+                        {
+                            exp = exp2;
+                            (cast(U*)&vf)[MANTISSA_LSB] = (exp < U.sizeof * 8) ? m >> exp : 0;
+                        }
+                    }
+                    else
+                    {
+                        m >>>= exp;
+                        u ^= cast(U) m;
+                    }
+                }
+            }
+            (cast(U*)&vf)[idx] = u;
+        }
+        return vf;
+    }
+
     static if (isRealX87)
     {
-        pragma(inline, true)
+        // Roughly 20% faster than ldexpImpl() on an i5-3550 CPU.
         real ldexp(real n, int exp) @safe pure nothrow
         {
             real r = void;
@@ -159,17 +325,11 @@ version (LDC)
     }
     else
     {
-        // use a fake-pure C ldexp[l] (might actually touch C errno)
-        static if (real.sizeof == double.sizeof)
-        {
-            extern(C) real ldexp(real n, int exp) @safe pure nothrow;
-        }
-        else
-        {
-            private extern(C) real ldexpl(real n, int exp) @safe pure nothrow;
-            alias ldexp = ldexpl;
-        }
+        real ldexp(real n, int exp) @safe pure nothrow { return ldexpImpl(n, exp); }
     }
+
+    //float  ldexp(float  n, int exp) @safe pure nothrow { return ldexpImpl(n, exp); }
+    //double ldexp(double n, int exp) @safe pure nothrow { return ldexpImpl(n, exp); }
 }
 else
 real ldexp(real n, int exp) @safe pure nothrow;    /* intrinsic */
