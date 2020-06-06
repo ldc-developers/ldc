4842953049275881c46b65f194b0ef4e0cb4d249
Martin Kinkelin <noone@nowhere.com>
2020-06-07 00:09:45 +0200

    Switch to a white list of archs wrt. -m32/-m64 in cc cmdlines
    According to the gcc 10.1 cmdline docs.

--- driver/tool.cpp
+++ driver/tool.cpp
@@ -89,17 +89,20 @@ void appendTargetArgsForGcc(std::vector<std::string> &args) {
 
   const auto &triple = *global.params.targetTriple;
   const auto arch64 = triple.get64BitArchVariant().getArch();
-  const auto arch32 = triple.get32BitArchVariant().getArch();
 
-  // Only specify -m32/-m64 for architectures where the two variants actually
-  // exist (as e.g. the GCC ARM toolchain doesn't recognize the switches).
-  if (arch64 == Triple::UnknownArch || arch32 == Triple::UnknownArch ||
-      arch64 == Triple::aarch64 || arch64 == Triple::aarch64_be) {
+  switch (arch64) {
+  // Specify -m32/-m64 for architectures where gcc supports those flags.
+  case Triple::x86_64:
+  case Triple::ppc64:
+  case Triple::ppc64le:
+  case Triple::sparcv9:
+  case Triple::nvptx64:
+    args.push_back(triple.isArch64Bit() ? "-m64" : "-m32");
     return;
-  }
 
   // MIPS does not have -m32/-m64 but requires -mabi=.
-  if (arch64 == Triple::mips64 || arch64 == Triple::mips64el) {
+  case Triple::mips64:
+  case Triple::mips64el:
     switch (getMipsABI()) {
     case MipsABI::EABI:
       args.push_back("-mabi=eabi");
@@ -120,11 +123,11 @@ void appendTargetArgsForGcc(std::vector<std::string> &args) {
     case MipsABI::Unknown:
       break;
     }
-
     return;
-  }
 
-  args.push_back(triple.isArch64Bit() ? "-m64" : "-m32");
+  default:
+    break;
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////////
