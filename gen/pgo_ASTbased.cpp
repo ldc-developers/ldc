b214dd07daeea3f4fcbaed9cd7f96cd11edc3f69
Johan Engelen <jbc.engelen@gmail.com>
2020-07-21 13:53:41 +0200

    Fix discarding of function hash for PGO, see Clang PR: https://reviews.llvm.org/D79961 Note that we do not include versioning code for our PGO files: PGO files from older LDC versions are not guarantee to work with newer versions of LDC. The user will see warnings about different function hashes, so he is aware the profile is not applied for those functions. Hash clashes may occur (old version hash corresponding to different function newer version hash), which is unfortunate but I think we do not need to go to the length of solving that as it only concerns optimization, not miscompilation. (it is similar that we don't guarantee object files from older compiler to work with newer compiler due to potential ABI changes which may also be silent)

--- gen/pgo_ASTbased.cpp
+++ gen/pgo_ASTbased.cpp
@@ -135,8 +135,11 @@ public:
       return Working;
 
     // Check for remaining work in Working.
-    if (Working)
-      MD5.update(Working);
+    if (Working) {
+      using namespace llvm::support;
+      uint64_t Swapped = endian::byte_swap<uint64_t, little>(Working);
+      MD5.update(llvm::makeArrayRef((uint8_t *)&Swapped, sizeof(Swapped)));
+    }
 
     // Finalize the MD5 and return the hash.
     llvm::MD5::MD5Result Result;
