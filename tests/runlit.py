97c7128831760d47be9c191b6b8d40e732a3fc47
Martin Kinkelin <noone@nowhere.com>
2020-10-26 00:44:10 +0100

    Fix lit-test regression with lit v0.11

--- tests/runlit.py
+++ tests/runlit.py
@@ -5,13 +5,13 @@ from __future__ import print_function
 
 if __name__=='__main__':
     try:
-        import lit
+        import lit.main
     except ImportError:
         import sys
         sys.exit('Package lit cannot be imported.\n' \
-                 'Lit can be installed using: \'python -m pip install lit\'\n' \
+                 'Lit can be installed using: \'python -m pip install -U lit\'\n' \
                  '(Python versions older than 2.7.9 or 3.4 do not have pip installed, see:\n' \
                  'https://pip.pypa.io/en/latest/installing/)')
 
     print("Lit version: ", lit.__version__)
-    lit.main()
+    lit.main.main()
