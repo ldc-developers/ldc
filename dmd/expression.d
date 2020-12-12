228fa67ba9a3cd561548096a77b198d45adad5f5
Martin Kinkelin <noone@nowhere.com>
2020-12-13 00:47:51 +0100

    Merge upstream stable (dlang/dmd@d3091c204e)

--- dmd/expression.d
+++ dmd/expression.d
@@ -1272,11 +1272,16 @@ extern (C++) /* IN_LLVM abstract */ class Expression : ASTNode
         if (v.storage_class & STC.manifest)
             return false; // ...or manifest constants
 
+        // accessing empty structs is pure
         if (v.type.ty == Tstruct)
         {
             StructDeclaration sd = (cast(TypeStruct)v.type).sym;
-            if (sd.hasNoFields)
-                return false;
+            if (sd.members) // not opaque
+            {
+                sd.determineSize(v.loc);
+                if (sd.hasNoFields)
+                    return false;
+            }
         }
 
         bool err = false;
