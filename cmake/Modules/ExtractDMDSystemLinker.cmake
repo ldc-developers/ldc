e3b46fc6a9131f53b82398f81a149d32aafc1e81
Martin Kinkelin <noone@nowhere.com>
2021-02-07 20:12:30 +0100

    CMake: Improve error handling for ExtractDMDSystemLinker

--- cmake/Modules/ExtractDMDSystemLinker.cmake
+++ cmake/Modules/ExtractDMDSystemLinker.cmake
@@ -37,8 +37,8 @@ execute_process(
     ERROR_STRIP_TRAILING_WHITESPACE
 )
 
-if(result_code)
-    message(FATAL_ERROR "Failed to link empty D program using '${D_COMPILER} ${cmdflags}'")
+if(NOT "${result_code}" STREQUAL "0")
+    message(FATAL_ERROR "Failed to link empty D program using '${D_COMPILER} ${D_COMPILER_FLAGS} ${DFLAGS_BASE}':\n${stderr}")
 endif()
 
 if("${D_COMPILER_ID}" STREQUAL "GDMD")
