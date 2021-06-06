f4b168d0bdcfa0fbe4815f5e5599eda1b47ee8b1
Johan Engelen <jbc.engelen@gmail.com>
2021-06-06 02:42:55 +0200

    MSan: Add support for `-fsanitize-memory-track-origins=`

--- tests/sanitizers/msan_uninitialized.d
+++ tests/sanitizers/msan_uninitialized.d
@@ -1,14 +1,15 @@
 // Test unitialized data access with MemorySanitizer
 
-// REQUIRES: MSan
+// REQUIRES: MSan, atleast_llvm800
 
 // RUN: %ldc -g -fsanitize=memory -run %s
-// RUN: %ldc -g -fsanitize=memory -d-version=BUG %s -of=%t%exe
+// RUN: %ldc -g -fsanitize=memory -fsanitize-memory-track-origins=2 -d-version=BUG %s -of=%t%exe
 // RUN: not %t%exe 2>&1 | FileCheck %s
 
 // CHECK: MemorySanitizer: use-of-uninitialized-value
-// TODO: line information is not shown correctly for this test.
 
+// CHECK: Uninitialized value was created by an allocation of {{.*}} in the stack frame of function '_Dmain'
+// CHECK-NEXT: #0 {{.*}} in _Dmain {{.*}}msan_uninitialized.d:[[@LINE+1]]
 int main()
 {
     version (BUG)
