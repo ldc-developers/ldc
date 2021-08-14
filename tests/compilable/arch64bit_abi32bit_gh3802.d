a6cc5cde38188bac9367bec660ff6762ffca52d7
Martin Kinkelin <noone@nowhere.com>
2021-08-14 18:47:41 +0200

    Extend test for issue #3802

--- tests/compilable/arch64bit_abi32bit_gh3802.d
+++ tests/compilable/arch64bit_abi32bit_gh3802.d
@@ -1,12 +1,31 @@
 // Tests compilation for 64-bit architecture with 32-bit word size ABI.
-// Triple examples: mips64el-linux, x86_64-linux-gnux32, aarch64-linux-gnu_ilp32
+// Triple examples: mips64el-linux-gnuabin32, x86_64-linux-gnux32
 // Targeting x86 because that's the most widely available target in our CI/developer systems.
 
 // REQUIRES: target_X86
-// RUN: %ldc -mtriple=x86_64-linux-gnux32 -c %s
+// RUN: %ldc -mtriple=x86_64-linux-gnux32 -O -c %s
+
+static assert(size_t.sizeof == 4);
+static assert(ptrdiff_t.sizeof == 4);
+
+version (D_LP64) static assert(0);
 
 bool equals(string lhs, string rhs)
 {
     foreach (const i; 0 .. lhs.length) {}
     return false;
+}
+
+// test _d_array_slice_copy optimization IR pass (requires -O)
+auto test_ArraySliceCopyOpt()
+{
+    static void copy(int[] a, int[] b)
+    {
+        a[] = b[];
+    }
+
+    int[2] a = [1, 2];
+    int[2] b = [3, 4];
+    copy(a, b);
+    return a;
 }
