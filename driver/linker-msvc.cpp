e0b288bff3117f0075936b2d0f068d8ec2740957
Nicholas Wilson <iamthewilsonator@hotmail.com>
2021-09-17 10:47:51 +0800

    use `using` rather than `#define`

--- driver/linker-msvc.cpp
+++ driver/linker-msvc.cpp
@@ -37,8 +37,8 @@ void addMscrtLibs(bool useInternalToolchain, std::vector<std::string> &args) {
   // Pick one of the 4 variants matching the selected main UCRT lib.
 
 #if LDC_LLVM_VER < 1400
-#define contains_insensitive contains_lower
-#define endswith_insensitive endswith_lower
+using contains_insensitive = contains_lower;
+using endswith_insensitive = endswith_lower;
 #endif
   if (useInternalToolchain) {
     assert(mscrtlibName.contains_insensitive("vcruntime"));
