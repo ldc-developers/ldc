5d1080c26d961a854b0f3be5435b6ec290b6cb90
Johan Engelen <jbc.engelen@gmail.com>
2022-03-03 03:53:28 +0100

    Fix @ldc.attributes.noSanitize compilation with `-w -de`. (#3930)

--- tests/sanitizers/attr_nosanitize.d
+++ tests/sanitizers/attr_nosanitize.d
@@ -1,7 +1,7 @@
 // Test ldc.attributes.noSanitize UDA
 
-// RUN: %ldc -c -output-ll -fsanitize=address,thread -of=%t.normal.ll     %s -d-version=NORMAL     && FileCheck %s --check-prefix=NORMAL     < %t.normal.ll
-// RUN: %ldc -c -output-ll -fsanitize=address,thread -of=%t.nosanitize.ll %s -d-version=NOSANITIZE && FileCheck %s --check-prefix=NOSANITIZE < %t.nosanitize.ll
+// RUN: %ldc -c -w -de -output-ll -fsanitize=address,thread -of=%t.normal.ll     %s -d-version=NORMAL     && FileCheck %s --check-prefix=NORMAL     < %t.normal.ll
+// RUN: %ldc -c -w -de -output-ll -fsanitize=address,thread -of=%t.nosanitize.ll %s -d-version=NOSANITIZE && FileCheck %s --check-prefix=NOSANITIZE < %t.nosanitize.ll
 
 // RUN: %ldc -wi -c -fsanitize=address -d-version=WARNING %s 2>&1 | FileCheck %s --check-prefix=WARNING
 
