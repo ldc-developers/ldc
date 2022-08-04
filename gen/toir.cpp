8f4c3ff2878d4c253d03acb6cd924c9435890e1e
Martin Kinkelin <noone@nowhere.com>
2022-08-04 22:12:14 +0200

    Support bit fields in AssignExp codegen

--- gen/toir.cpp
+++ gen/toir.cpp
@@ -478,6 +478,51 @@ public:
       }
     }
 
+    // Handle assignments to bit fields (no lvalues).
+    if (auto dve = e->e1->isDotVarExp()) {
+      if (auto bf = dve->var->isBitFieldDeclaration()) {
+        const auto aggr = dve->e1;
+
+        AggregateDeclaration *ad;
+        const auto at = aggr->type->toBasetype();
+        if (auto ts = at->isTypeStruct()) {
+          ad = ts->sym;
+        } else if (auto tc = at->isTypeClass()) {
+          ad = tc->sym;
+        } else {
+          llvm_unreachable("Unknown aggregate type being indexed.");
+        }
+
+        const auto sizeInBits = (bf->bitOffset + bf->fieldWidth + 7) / 8 * 8;
+        const auto intType = LLIntegerType::get(gIR->context(), sizeInBits);
+
+        const auto aggrPtr = DtoLVal(aggr);
+        const auto val = DtoRVal(e->e2);
+
+        const auto ptr = DtoBitCast(DtoIndexAggregate(aggrPtr, ad, bf),
+                                    getPtrToType(intType));
+
+        const auto mask =
+            llvm::APInt::getLowBitsSet(sizeInBits, bf->fieldWidth);
+        const auto oldVal =
+            gIR->ir->CreateAlignedLoad(intType, ptr, LLMaybeAlign(1));
+        const auto maskedOldVal =
+            gIR->ir->CreateAnd(oldVal, ~(mask << bf->bitOffset));
+
+        auto bfVal = gIR->ir->CreateZExtOrTrunc(val, intType);
+        bfVal = gIR->ir->CreateAnd(bfVal, mask);
+        if (auto n = bf->bitOffset)
+          bfVal = gIR->ir->CreateShl(bfVal, n);
+
+        const auto newVal = gIR->ir->CreateOr(maskedOldVal, bfVal);
+        gIR->ir->CreateAlignedStore(newVal, ptr, LLMaybeAlign(1));
+
+        // return the untainted rhs rvalue
+        result = new DImValue(e->type, val);
+        return;
+      }
+    }
+
     // The front-end sometimes rewrites a static-array-lhs to a slice, e.g.,
     // when initializing a static array with an array literal.
     // Use the static array as lhs in that case.
@@ -488,8 +533,6 @@ public:
           se->type->toBasetype()->nextOf() == sliceeBaseType->nextOf())
         rewrittenLhsStaticArray = toElem(se->e1, true);
     }
-
-    // TODO: bit fields!
 
     DValue *const lhs = (rewrittenLhsStaticArray ? rewrittenLhsStaticArray
                                                  : toElem(e->e1, true));
