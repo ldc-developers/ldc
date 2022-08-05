e8a41f475acd20d8305c6a2e7c2c86de88b1d29f
Martin Kinkelin <noone@nowhere.com>
2022-08-05 23:10:27 +0200

    [revise write_struct_literal(), mainly wrt. logging]

--- gen/toir.cpp
+++ gen/toir.cpp
@@ -115,28 +115,23 @@ static void write_struct_literal(Loc loc, LLValue *mem, StructDeclaration *sd,
     const auto vd = data[i].field;
     const auto expr = data[i].expr;
 
-    IF_LOG Logger::println("initializing field: %s %s (+%u)",
-                           vd->type->toChars(), vd->toChars(), vd->offset);
-    LOG_SCOPE
-
     // initialize any padding so struct comparisons work
     if (vd->offset != offset) {
-      assert(vd->offset > offset);
+      if (vd->offset < offset) {
+        error(loc, "ICE: overlapping initializers for struct literal");
+        fatal();
+      }
       voidptr = write_zeroes(voidptr, offset, vd->offset);
       offset = vd->offset;
     }
 
-    // get a pointer to this field
-    LLValue *ptr = DtoIndexAggregate(mem, sd, vd);
-
     if (vd->isBitFieldDeclaration()) {
       // collect all bit fields at the current byte offset
-      LLSmallVector<BitFieldDeclaration *, 8> bitFields;
+      size_t j; // endIndex
       unsigned maxSizeInBytes = 0;
-      for (size_t j = i; j < data.size(); ++j) {
+      for (j = i; j < data.size(); ++j) {
         if (auto bf = data[j].field->isBitFieldDeclaration()) {
           if (bf->offset == vd->offset) {
-            bitFields.push_back(bf);
             const auto sizeInBytes = (bf->bitOffset + bf->fieldWidth + 7) / 8;
             if (sizeInBytes > maxSizeInBytes)
               maxSizeInBytes = sizeInBytes;
@@ -146,35 +141,57 @@ static void write_struct_literal(Loc loc, LLValue *mem, StructDeclaration *sd,
         break;
       }
 
+      IF_LOG Logger::println("initializing bit field group: (+%u, %u bytes)",
+                             vd->offset, maxSizeInBytes);
+      LOG_SCOPE
+
+      // get a pointer to this field
+      const auto ptr = DtoIndexAggregate(mem, sd, vd);
+
       // merge all initializers to a single value
       const auto intType =
           LLIntegerType::get(gIR->context(), maxSizeInBytes * 8);
       LLValue *val = LLConstant::getNullValue(intType);
-      for (size_t j = 0; j < bitFields.size(); ++j) {
-        const auto bf = bitFields[j];
-        const auto bfExpr = data[i + j].expr;
+      for (size_t k = i; k < j; ++k) {
+        const auto bf = data[k].field->isBitFieldDeclaration();
+        assert(bf);
+        const auto bfExpr = data[k].expr;
         assert(bfExpr);
+
+        IF_LOG Logger::println("bit field: %s %s (bit offset %u, width %u): %s",
+                               bf->type->toChars(), bf->toChars(),
+                               bf->bitOffset, bf->fieldWidth,
+                               bfExpr->toChars());
+        LOG_SCOPE
+
         auto bfVal = DtoRVal(bfExpr);
         bfVal = gIR->ir->CreateZExtOrTrunc(bfVal, intType);
         const auto mask =
             llvm::APInt::getLowBitsSet(intType->getBitWidth(), bf->fieldWidth);
         bfVal = gIR->ir->CreateAnd(bfVal, mask);
-        bfVal = gIR->ir->CreateShl(bfVal, bf->bitOffset);
+        if (auto n = bf->bitOffset)
+          bfVal = gIR->ir->CreateShl(bfVal, n);
         val = gIR->ir->CreateOr(val, bfVal);
       }
 
+      IF_LOG Logger::cout() << "merged IR value: " << *val << '\n';
       gIR->ir->CreateAlignedStore(val, DtoBitCast(ptr, getPtrToType(intType)),
                                   LLMaybeAlign(1));
       offset += maxSizeInBytes;
 
-      i += bitFields.size() - 1; // skip the other bit fields
+      i = j - 1; // skip the other bit fields of the group
     } else {
+      IF_LOG Logger::println("initializing field: %s %s (+%u)",
+                             vd->type->toChars(), vd->toChars(), vd->offset);
+      LOG_SCOPE
+
+      // get a pointer to this field
+      const auto ptr = DtoIndexAggregate(mem, sd, vd);
       DLValue field(vd->type, DtoBitCast(ptr, DtoPtrToType(vd->type)));
 
       // initialize the field
       if (expr) {
         IF_LOG Logger::println("expr = %s", expr->toChars());
-
         // try to construct it in-place
         if (!toInPlaceConstruction(&field, expr)) {
           DtoAssign(loc, &field, toElem(expr), EXP::blit);
@@ -197,8 +214,13 @@ static void write_struct_literal(Loc loc, LLValue *mem, StructDeclaration *sd,
   }
 
   // initialize trailing padding
-  if (sd->structsize != offset)
+  if (sd->structsize != offset) {
+    if (sd->structsize < offset) {
+      error(loc, "ICE: struct literal size exceeds struct size");
+      fatal();
+    }
     voidptr = write_zeroes(voidptr, offset, sd->structsize);
+  }
 }
 
 namespace {
@@ -983,7 +1005,6 @@ public:
       if (auto bf = vd->isBitFieldDeclaration()) {
         result = new DBitFieldLValue(e->type, ptr, bf);
       } else {
-        // Cast the (possibly void*) pointer to the canonical variable type.
         ptr = DtoBitCast(ptr, DtoPtrToType(e->type));
         result = new DLValue(e->type, ptr);
       }
