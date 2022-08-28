a1ab0eb30a0a3d1f398ddb562db3b34b02dcf53d
Nicholas Wilson <iamthewilsonator@hotmail.com>
2022-08-28 18:36:04 +0800

    Use typed GEP in `functions.cpp`

--- gen/functions.cpp
+++ gen/functions.cpp
@@ -1291,7 +1291,7 @@ void DtoDefineFunction(FuncDeclaration *fd, bool linkageAvailableExternally) {
         LLType *targetThisType = thismem->getType();
         thismem = DtoBitCast(thismem, getVoidPtrType());
         auto off = DtoConstInt(-fd->interfaceVirtual->offset);
-        thismem = DtoGEP1(thismem, off);
+        thismem = DtoGEP1(llvm::Type::getInt8Ty(gIR->context()), thismem, off);
         thismem = DtoBitCast(thismem, targetThisType);
       }
       thismem = DtoAllocaDump(thismem, 0, "this");
