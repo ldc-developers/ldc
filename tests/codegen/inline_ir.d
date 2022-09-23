acaf23f51e04b8faeb7940a2d04bbac99cf4a957
Nicholas Wilson <iamthewilsonator@hotmail.com>
2022-09-23 15:05:22 +0800

    Opaque Pointers: `codegen/inline_ir.d`

--- tests/codegen/inline_ir.d
+++ tests/codegen/inline_ir.d
@@ -1,13 +1,19 @@
 // RUN: %ldc -c -output-ll -of=%t.ll %s && FileCheck %s < %t.ll
 
 import ldc.llvmasm;
-
+import ldc.intrinsics;
+static if (LLVM_atleast!1500)
+{
+    alias __irEx!("", "store i32 %1, ptr %0, !nontemporal !0", "!0 = !{i32 1}", void, int*, int) nontemporalStore;
+    alias __irEx!("!0 = !{i32 1}", "%i = load i32, ptr %0, !nontemporal !0\nret i32 %i", "", int, const int*) nontemporalLoad;
+}
+else {
 alias __irEx!("", "store i32 %1, i32* %0, !nontemporal !0", "!0 = !{i32 1}", void, int*, int) nontemporalStore;
 alias __irEx!("!0 = !{i32 1}", "%i = load i32, i32* %0, !nontemporal !0\nret i32 %i", "", int, const int*) nontemporalLoad;
-
+}
 int foo(const int* src)
 {
-  // CHECK: %{{.*}} = load i32, i32* {{.*}} !nontemporal ![[METADATA:[0-9]+]]
+  // CHECK: %{{.*}} = load i32, {{i32\*|ptr}} {{.*}} !nontemporal ![[METADATA:[0-9]+]]
   return nontemporalLoad(src);
 }
 
