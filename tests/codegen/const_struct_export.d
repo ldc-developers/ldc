0341af7983ff4b0f78603ad3452e1b6d71a97c3a
Nicholas Wilson <iamthewilsonator@hotmail.com>
2022-09-23 09:44:42 +0800

    Opaque Pointers: `codegen/const_struct_export.d`

--- tests/codegen/const_struct_export.d
+++ tests/codegen/const_struct_export.d
@@ -1,9 +1,9 @@
 // RUN: %ldc -c -output-ll -of=%t.ll %s && FileCheck %s < %t.ll
 
-// CHECK:     @.immutablearray{{.*}} = internal constant [2 x void ()*] {{.*}}exportedFunction
-// CHECK-NOT: @.immutablearray{{.*}} [2 x void ()*] {{.*}}importedFunction
-// CHECK:     @.immutablearray{{.*}} = internal constant [2 x i32*] {{.*}}exportedVariable
-// CHECK-NOT: @.immutablearray{{.*}} [2 x i32*] {{.*}}importedVariable
+// CHECK:     @.immutablearray{{.*}} = internal constant [2 x {{void \(\)\*|ptr}}] {{.*}}exportedFunction
+// CHECK-NOT: @.immutablearray{{.*}} [2 x {{void \(\)\*|ptr}}] {{.*}}importedFunction
+// CHECK:     @.immutablearray{{.*}} = internal constant [2 x {{i32\*|ptr}}] {{.*}}exportedVariable
+// CHECK-NOT: @.immutablearray{{.*}} [2 x {{i32\*|ptr}}] {{.*}}importedVariable
 
 export void exportedFunction() {}
 export void importedFunction();
@@ -13,8 +13,8 @@ export immutable int importedVariable;
 void foo () {
     immutable auto exportedFuncs = [ &exportedFunction, &exportedFunction ];
     immutable auto importedFuncs = [ &importedFunction, &importedFunction ];
-    // CHECK: store void ()* @{{.*}}D19const_struct_export16importedFunctionFZv
+    // CHECK: store {{void \(\)\*|ptr}} @{{.*}}D19const_struct_export16importedFunctionFZv
     immutable auto exportedVars = [ &exportedVariable, &exportedVariable ];
     immutable auto importedVars = [ &importedVariable, &importedVariable ];
-    // CHECK: store i32* @{{.*}}D19const_struct_export16importedVariable
+    // CHECK: store {{i32\*|ptr}} @{{.*}}D19const_struct_export16importedVariable
 }
