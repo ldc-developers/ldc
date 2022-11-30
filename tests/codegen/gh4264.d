b67710ee6883665ee19199e2d73e936ee0bad04a
Zachary Yedidia <zyedidia@gmail.com>
2022-11-29 17:21:15 -0800

    Also check generated getelementptr

--- tests/codegen/gh4264.d
+++ tests/codegen/gh4264.d
@@ -1,6 +1,7 @@
-// RUN: %ldc -run %s
+// RUN: %ldc -run %s && %ldc -output-ll -of=%t.ll %s && FileCheck %s < %t.ll
 
 enum offset = 0xFFFF_FFFF_0000_0000UL;
 void main() {
+    // CHECK: %1 = getelementptr inbounds i8,{{.*}}_Dmain{{.*}}, i64 -4294967296
     assert((cast(ulong)&main) != (cast(ulong)&main + offset));
 }
