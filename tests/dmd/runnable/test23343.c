71bbd5bc7f8886f348d645ad685552f8b3274667
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2023-01-21 20:02:37 +0100

    Disable runnable/test23343.c for now (Mac-only test)

--- tests/dmd/runnable/test23343.c
+++ tests/dmd/runnable/test23343.c
@@ -1,4 +1,6 @@
-/* DISABLED: win32 win64 linux32 linux64 freebsd32 freebsd64 osx32 dragonflybsd32 netbsd32
+/* DISABLED: win32 win64 linux32 linux64 freebsd32 freebsd64 osx32 dragonflybsd32 netbsd32 LDC
+ * LDC: this was apparently hacked around in DMD, requiring the glue layer to
+        know about this ImportC special case and only apply it for Mac targets...
  */
 
 /* https://issues.dlang.org/show_bug.cgi?id=23343
