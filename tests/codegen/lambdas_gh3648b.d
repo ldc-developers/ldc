a80dd003cd41af83b2725fad6d73fea952dd9716
Martin Kinkelin <kinke@users.noreply.github.com>
2023-06-08 02:39:38 +0200

    Handle lambda mangle collisions by emitting lambdas with internal linkage (#4415)
    See https://issues.dlang.org/show_bug.cgi?id=23722.

--- tests/codegen/lambdas_gh3648b.d
+++ tests/codegen/lambdas_gh3648b.d
@@ -1,4 +1,4 @@
-// Tests that *imported* lambdas and contained globals are emitted as linkonce_odr.
+// Tests that *imported* lambdas and contained globals are emitted with internal linkage.
 
 // RUN: %ldc -output-ll -of=%t.ll %s -I%S && FileCheck %s < %t.ll
 
@@ -10,17 +10,17 @@ void foo()
     bar_inlined();
 }
 
-// the global variables should be defined as linkonce_odr:
-// CHECK: _D14lambdas_gh36489__lambda5FZ10global_bari{{.*}} = linkonce_odr {{(hidden )?}}thread_local global
-// CHECK: _D14lambdas_gh36489__lambda6FZ18global_bar_inlinedOi{{.*}} = linkonce_odr {{(hidden )?}}global
+// the global variables should be defined as internal:
+// CHECK: _D14lambdas_gh36489__lambda5FZ10global_bari{{.*}} = internal thread_local global
+// CHECK: _D14lambdas_gh36489__lambda6FZ18global_bar_inlinedOi{{.*}} = internal global
 
 // foo() should only call one lambda:
 // CHECK: define {{.*}}_D15lambdas_gh3648b3fooFZv
 // CHECK-NEXT: call {{.*}}__lambda5
 // CHECK-NEXT: ret void
 
-// bar() should be defined as linkonce_odr:
-// CHECK: define linkonce_odr {{.*}}__lambda5
+// bar() should be defined as internal:
+// CHECK: define internal {{.*}}__lambda5
 
 // bar_inlined() should NOT have made it to the .ll:
 // CHECK-NOT: define {{.*}}__lambda6
