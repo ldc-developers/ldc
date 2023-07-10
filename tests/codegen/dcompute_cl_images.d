823b9200d6e670e1623b3ae6d84f698da137207c
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2023-07-10 13:26:56 +0200

    Re-enable lit-test codegen/dcompute_cl_images.d for LLVM 16

--- tests/codegen/dcompute_cl_images.d
+++ tests/codegen/dcompute_cl_images.d
@@ -1,7 +1,7 @@
 // REQUIRES: target_SPIRV
 
-// FIXME: hits an assertion with recent SPIRV-LLVM-Translator, see https://github.com/ldc-developers/ldc/pull/4010#issuecomment-1191820165
-// XFAIL: atleast_llvm1500
+// FIXME: hits an assertion with SPIRV-LLVM-Translator for LLVM 15, see https://github.com/ldc-developers/ldc/pull/4010#issuecomment-1191820165
+// XFAIL: atleast_llvm1500 && atmost_llvm1509
 
 // RUN: %ldc -c -mdcompute-targets=ocl-220 -m64 -I%S/inputs -mdcompute-file-prefix=%t -output-ll -output-o %s && FileCheck %s < %t_ocl220_64.ll
 @compute(CompileFor.deviceOnly) module dcompute_cl_images;
