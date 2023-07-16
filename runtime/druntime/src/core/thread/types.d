292ca6bd60d1281b95aa3e40b49174bb42969282
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2023-07-16 15:03:26 +0200

    druntime: Fix little core.thread regression for non-x86[_64] targets

--- runtime/druntime/src/core/thread/types.d
+++ runtime/druntime/src/core/thread/types.d
@@ -39,6 +39,14 @@ version (GNU)
     else
         enum isStackGrowingDown = false;
 }
+else version (LDC)
+{
+    // The only LLVM targets as of LLVM 16 with stack growing *upwards* are
+    // apparently NVPTX and AMDGPU, both without druntime support.
+    // Note that there's an analogous `version = StackGrowsDown` in
+    // core.thread.fiber.
+    enum isStackGrowingDown = true;
+}
 else
 {
     version (X86) enum isStackGrowingDown = true;
