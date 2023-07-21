b0d8748e62b4ce6e85e2089addb4873529e1f60f
Johan Engelen <jbc.engelen@gmail.com>
2023-07-22 00:51:08 +0200

    Add ldc-build-plugin tool, and make use of it in plugins lit tests.

--- tests/plugins/visitor_example.d
+++ tests/plugins/visitor_example.d
@@ -5,7 +5,7 @@
 // UNSUPPORTED: Darwin && host_X86
 
 // RUN: split-file %s %t --leading-lines
-// RUN: %ldc -g %t/plugin.d %plugin_compile_flags -of=%t/plugin%so
+// RUN: %buildplugin %t/plugin.d -of=%t/plugin%so
 // RUN: %ldc -wi -c -o- --plugin=%t/plugin%so %t/testcase.d 2>&1 | FileCheck %t/testcase.d
 
 //--- plugin.d
