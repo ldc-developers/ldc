0d0c7fdb2faefffd7f4780ffe116b55d0de5f9f6
ryuukk <44361234+ryuukk@users.noreply.github.com>
2023-07-30 17:17:16 +0200

    Do not prefix static libs in pragma -l

--- gen/declarations.cpp
+++ gen/declarations.cpp
@@ -419,13 +419,17 @@ public:
         std::string arg = ("/DEFAULTLIB:\"" + name + "\"").str();
         gIR->addLinkerOption(llvm::StringRef(arg));
       } else {
-        size_t const n = name.size() + 3;
-        char *arg = static_cast<char *>(mem.xmalloc(n));
-        arg[0] = '-';
-        arg[1] = 'l';
-        memcpy(arg + 2, name.data(), name.size());
-        arg[n - 1] = 0;
-        global.params.linkswitches.push(arg);
+        if (name.endswith(".a")) {
+          global.params.linkswitches.push(arg);
+        } else {
+          size_t const n = name.size() + 3;
+          char *arg = static_cast<char *>(mem.xmalloc(n));
+          arg[0] = '-';
+          arg[1] = 'l';
+          memcpy(arg + 2, name.data(), name.size());
+          arg[n - 1] = 0;
+          global.params.linkswitches.push(arg);
+        }
 
         if (triple.isOSBinFormatMachO()) {
           // embed linker directive in Mach-O object file too
