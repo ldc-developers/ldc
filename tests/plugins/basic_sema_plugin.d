6d9bbf181a411f8d38c578edf44c8cce889b7ee5
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2023-08-16 17:34:31 +0200

    GH Actions: Export ~all symbols from prebuilt LTO'd macOS binaries
    Fixes #4462 for our official packages.

--- tests/plugins/basic_sema_plugin.d
+++ tests/plugins/basic_sema_plugin.d
@@ -1,8 +1,5 @@
 // REQUIRES: Plugins
 
-// For some reason this test fails with missing symbol linking issues (or crash) with macOS on Intel x86 (but not for all CI testers...)
-// UNSUPPORTED: Darwin && host_X86
-
 // RUN: split-file %s %t --leading-lines
 // RUN: %buildplugin %t/plugin.d -of=%t/plugin%so --buildDir=%t/build
 // RUN: %ldc -wi -c -o- --plugin=%t/plugin%so %t/testcase.d 2>&1 | FileCheck %t/testcase.d
--- tests/plugins/visitor_example.d
+++ tests/plugins/visitor_example.d
@@ -1,9 +1,6 @@
 // REQUIRES: Plugins
 // REQUIRES: ABI_compatible_with_host_D
 
-// For some reason this test fails with missing symbol linking issues (or crash) with macOS on Intel x86.
-// UNSUPPORTED: Darwin && host_X86
-
 // RUN: split-file %s %t --leading-lines
 // RUN: %buildplugin %t/plugin.d -of=%t/plugin%so --buildDir=%t/build
 // RUN: %ldc -wi -c -o- --plugin=%t/plugin%so %t/testcase.d 2>&1 | FileCheck %t/testcase.d
