a5f03df1957e242aba1485f263628ccf70f210f8
Nicholas Wilson <iamthewilsonator@hotmail.com>
2023-09-20 20:17:06 +0800

    Update dcompute_check_action.d

--- tests/codegen/dcompute_check_action.d
+++ tests/codegen/dcompute_check_action.d
@@ -1,7 +1,7 @@
 // Check that we ignore the conversion of `assert(x)` -> `assert(x,_d_assert_fail(...))`
 // and dont give any errors for the generated code (which is ignored by codegen).
 // REQUIRES: target_NVPTX
-// RUN: %ldc -c -mdcompute-targets=cuda-350 -m64 -output-ll -mdcompute-file-prefix=check_action -output-o -checkaction=context
+// RUN: %ldc -c %s -mdcompute-targets=cuda-350 -m64 -output-ll -mdcompute-file-prefix=check_action -output-o -checkaction=context
 // xRUN: FileCheck %s --check-prefix=LL < dcompute_check_action.ll
 
 @compute(CompileFor.deviceOnly) module dcompute_check_action;
