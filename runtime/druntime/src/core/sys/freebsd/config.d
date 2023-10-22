9928895c548af3789c7fcc7947d5e9b3ac152ae9
Gleb Popov <6yearold@gmail.com>
2023-10-22 16:11:17 +0300

    Upstream FreeBSD-specific patches (#4515)
    * CMake: Populate C_SYSTEM_LIBS for FreeBSD
    
    * druntime: Update FreeBSD versions
    
    * driver: Use PIC relocation model on FreeBSD

--- runtime/druntime/src/core/sys/freebsd/config.d
+++ runtime/druntime/src/core/sys/freebsd/config.d
@@ -14,9 +14,10 @@ public import core.sys.posix.config;
 // NOTE: When adding newer versions of FreeBSD, verify all current versioned
 // bindings are still compatible with the release.
 
-     version (FreeBSD_14) enum __FreeBSD_version = 1400000;
-else version (FreeBSD_13) enum __FreeBSD_version = 1301000;
-else version (FreeBSD_12) enum __FreeBSD_version = 1203000;
+     version (FreeBSD_15) enum __FreeBSD_version = 1500000;
+else version (FreeBSD_14) enum __FreeBSD_version = 1400000;
+else version (FreeBSD_13) enum __FreeBSD_version = 1302000;
+else version (FreeBSD_12) enum __FreeBSD_version = 1204000;
 else version (FreeBSD_11) enum __FreeBSD_version = 1104000;
 else version (FreeBSD_10) enum __FreeBSD_version = 1004000;
 else version (FreeBSD_9)  enum __FreeBSD_version = 903000;
