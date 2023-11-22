5408ceee9bb054424c0439de4bf4a981c9479ef4
Johan Engelen <jbc.engelen@gmail.com>
2023-11-22 23:52:46 +0100

    Fix llvm version typo in test dcompute_cl_addrspaces_new.d

--- tests/codegen/dcompute_cl_addrspaces_new.d
+++ tests/codegen/dcompute_cl_addrspaces_new.d
@@ -1,6 +1,6 @@
 // See GH issue #2709
 
-// REQUIRES: target_SPIRV && llvm_atleast1600
+// REQUIRES: target_SPIRV && atleast_llvm1600
 // RUN: %ldc -c -mdcompute-targets=ocl-220 -m64 -mdcompute-file-prefix=addrspace_new -output-ll -output-o %s && FileCheck %s --check-prefix=LL < addrspace_new_ocl220_64.ll \
 // RUN: && %llc addrspace_new_ocl220_64.ll -mtriple=spirv64-unknown-unknown -O0 -o - | FileCheck %s --check-prefix=SPT
 @compute(CompileFor.deviceOnly) module dcompute_cl_addrspaces_new;
