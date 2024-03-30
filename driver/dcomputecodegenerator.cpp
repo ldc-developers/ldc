963a7a2513c062ee4230f6cacfeb1f47ebc67b1b
Johan Engelen <jbc.engelen@gmail.com>
2024-03-30 12:24:08 +0100

    Improve error message when dcompute target is not supported. (#4609)

--- driver/dcomputecodegenerator.cpp
+++ driver/dcomputecodegenerator.cpp
@@ -28,44 +28,48 @@ DComputeCodeGenManager::~DComputeCodeGenManager() {}
 
 DComputeTarget *
 DComputeCodeGenManager::createComputeTarget(const std::string &s) {
+  if (s.substr(0, 4) == "ocl-") {
 #if LDC_LLVM_SUPPORTED_TARGET_SPIRV
 #define OCL_VALID_VER_INIT 100, 110, 120, 200, 210, 220
-  const std::array<int, 6> valid_ocl_versions = {{OCL_VALID_VER_INIT}};
+    const std::array<int, 6> valid_ocl_versions = {{OCL_VALID_VER_INIT}};
 
-  if (s.substr(0, 4) == "ocl-") {
     const int v = atoi(s.c_str() + 4);
     if (std::find(valid_ocl_versions.begin(), valid_ocl_versions.end(), v) !=
         valid_ocl_versions.end()) {
       return createOCLTarget(ctx, v);
     }
-  }
+#else
+    error(Loc(), "LDC was not built with OpenCl DCompute support.");
 #endif
+  }
 
+  if (s.substr(0, 5) == "cuda-") {
 #if LDC_LLVM_SUPPORTED_TARGET_NVPTX
 #define CUDA_VALID_VER_INIT 100, 110, 120, 130, 200, 210, 300, 350, 370,\
  500, 520, 600, 610, 620, 700, 720, 750, 800
-  const std::array<int, 18> valid_cuda_versions = {{CUDA_VALID_VER_INIT}};
+    const std::array<int, 18> valid_cuda_versions = {{CUDA_VALID_VER_INIT}};
 
-  if (s.substr(0, 5) == "cuda-") {
     const int v = atoi(s.c_str() + 5);
     if (std::find(valid_cuda_versions.begin(), valid_cuda_versions.end(), v) !=
         valid_cuda_versions.end()) {
       return createCUDATarget(ctx, v);
     }
-  }
+#else
+    error(Loc(), "LDC was not built with CUDA DCompute support.");
 #endif
+  }
 
-#define STR(x) #x
+#define STR(x...) #x
 #define XSTR(x) STR(x)
 
   error(Loc(),
-        "unrecognised or invalid DCompute targets: the format is ocl-xy0 "
+        "Unrecognised or invalid DCompute targets: the format is ocl-xy0 "
         "for OpenCl x.y and cuda-xy0 for CUDA CC x.y."
 #if LDC_LLVM_SUPPORTED_TARGET_SPIRV
-        " Valid versions for OpenCl are " XSTR((OCL_VALID_VER_INIT)) "."
+        " Valid version strings for OpenCl are ocl-{" XSTR(OCL_VALID_VER_INIT) "}."
 #endif
 #if LDC_LLVM_SUPPORTED_TARGET_NVPTX
-        " Valid versions for CUDA are " XSTR((CUDA_VALID_VER_INIT))
+        " Valid version strings for CUDA are cuda-{" XSTR(CUDA_VALID_VER_INIT) "}."
 #endif
   );
 
