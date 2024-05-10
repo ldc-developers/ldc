fa4f032baf4cd2c5d347b4bf6f3e99c93a70c2bb
Johan Engelen <jbc.engelen@gmail.com>
2024-05-10 23:38:52 +0200

    Fix crashing core.thread.fiber unittest for AArch64. (#4648)

--- runtime/druntime/src/core/thread/fiber.d
+++ runtime/druntime/src/core/thread/fiber.d
@@ -558,6 +558,8 @@ version (LDC)
 
     version (Android) version = CheckFiberMigration;
 
+    version (AArch64) version = CheckFiberMigration;
+
     // Fiber migration across threads is (probably) not possible with ASan fakestack enabled (different parts of the stack
     // will contain fakestack pointers that were created on different threads...)
     version (SupportSanitizers) version = CheckFiberMigration;
@@ -1132,6 +1134,16 @@ class Fiber
      */
     static Fiber getThis() @safe nothrow @nogc
     {
+        // LDC NOTE:
+        // Currently, it is not safe to migrate fibers across threads when they
+        // use TLS at all, as LLVM might cache the TLS address lookup across a
+        // context switch (see https://github.com/ldc-developers/ldc/issues/666).
+        // Preventing inlining of this function, as well as switch{In,Out}
+        // below, enables users to do this at least as long as they are very
+        // careful about accessing TLS data themselves (such as in the shared
+        // fiber unittest below, which tends to sporadically crash with enabled
+        // optimizations if this prevent-inlining workaround is removed).
+        version (LDC) pragma(inline, false);
         return sm_this;
     }
 
@@ -1951,6 +1963,7 @@ private:
     //
     final void switchIn() nothrow @nogc
     {
+        // see note in getThis()
         version (LDC) pragma(inline, false);
 
         ThreadBase  tobj = ThreadBase.getThis();
@@ -2044,6 +2057,7 @@ private:
     //
     final void switchOut() nothrow @nogc
     {
+        // see note in getThis()
         version (LDC) pragma(inline, false);
 
         ThreadBase  tobj = m_curThread;
@@ -2320,7 +2334,7 @@ unittest
                         fibs[idx].call();
                         cont |= fibs[idx].state != Fiber.State.TERM;
                     }
-                    locks[idx] = false;
+                    locks[idx].atomicStore(false);
                 }
                 else
                 {
