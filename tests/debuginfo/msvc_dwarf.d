f1b1b1d23a0b07124cd7a004b13e554e5a67e78b
Martin Kinkelin <kinke@users.noreply.github.com>
2024-06-13 13:18:29 +0200

    Try to work around sporadic lld assertion on Win32 for lit-test debuginfo/msvc_dwarf.d (#4687)

--- tests/debuginfo/msvc_dwarf.d
+++ tests/debuginfo/msvc_dwarf.d
@@ -4,6 +4,9 @@
 // RUN: FileCheck %s < %t_stderr.log
 // CHECK: lld-link: warning: section name .debug_info is longer than 8 characters and will use a non-standard string table
 
+// The test sometimes fails on Win32 due to an lld assertion; no problems on Win64.
+// ALLOW_RETRIES: 2
+
 int foo(int p)
 {
     auto r = 2 * p;
