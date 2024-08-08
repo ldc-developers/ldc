79fba2e5fcec4f2f04ebf603e6ddfb74aa2ee16d
Nicholas Wilson <thewilsonator@users.noreply.github.com>
2024-08-08 08:08:47 +0800

    [dcompute] correct target triples for SPIRV

--- gen/dcompute/targetOCL.cpp
+++ gen/dcompute/targetOCL.cpp
@@ -24,8 +24,8 @@
 #include <string>
 
 // from SPIRVInternal.h
-#define SPIR_TARGETTRIPLE32 "spir-unknown-unknown"
-#define SPIR_TARGETTRIPLE64 "spir64-unknown-unknown"
+#define SPIR_TARGETTRIPLE32 "spirv-unknown-unknown"
+#define SPIR_TARGETTRIPLE64 "spirv64-unknown-unknown"
 #define SPIR_DATALAYOUT32                                                      \
   "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32"                             \
   "-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v24:32:32"                         \
