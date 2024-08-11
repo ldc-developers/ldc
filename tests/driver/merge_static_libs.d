a8c0550f5911c4ac62602b853fd472725d94bd41
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2024-08-11 19:23:44 +0200

    [modify test]

--- tests/driver/merge_static_libs.d
+++ tests/driver/merge_static_libs.d
@@ -1,5 +1,5 @@
-// RUN: %ldc -lib %s -of=%t_1%lib
-// RUN: %ldc -lib %s -of=%t_2%lib
-// RUN: %ldc -lib %t_1%lib %t_2%lib -of=%t_merged%lib
+// RUN: %ldc -lib %s -of=%t%lib
+// RUN: %ldc -lib %t%lib -of=%t_merged%lib
+// RUN: %diff_binary %t%lib %t_merged%lib
 
 void foo() {}
