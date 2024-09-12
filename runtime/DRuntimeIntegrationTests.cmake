7f313035f87a5f364e868e98b5cc85b2c902b8a4
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2024-09-12 19:01:16 +0200

    [druntime: Pass D_EXTRA_FLAGS and RT_CFLAGS in {C,D}FLAGS_BASE for make-based integration tests]

--- runtime/DRuntimeIntegrationTests.cmake
+++ runtime/DRuntimeIntegrationTests.cmake
@@ -34,8 +34,13 @@ else()
     endif()
 endif()
 
-if(NOT "${TARGET_SYSTEM}" MATCHES "MSVC")
-    set(cflags_base "CFLAGS_BASE=-Wall -Wl,-rpath,${CMAKE_BINARY_DIR}/lib${LIB_SUFFIX}")
+string(REPLACE ";" " " dflags_base "${D_EXTRA_FLAGS}")
+
+string(REPLACE ";" " " cflags_base "${RT_CFLAGS}")
+if("${TARGET_SYSTEM}" MATCHES "MSVC")
+    set(cflags_base "${cflags_base} /Wall")
+else()
+    set(cflags_base "${cflags_base} -Wall -Wl,-rpath,${CMAKE_BINARY_DIR}/lib${LIB_SUFFIX}")
 endif()
 
 set(linkdl "")
@@ -56,8 +61,6 @@ else()
     list(REMOVE_ITEM testnames uuid)
 endif()
 
-string(REPLACE ";" " " LDMD_CMD "${LDMD_EXE_FULL} ${D_EXTRA_FLAGS}")
-
 foreach(name ${testnames})
     foreach(build debug release)
         set(druntime_path_build ${druntime_path})
@@ -74,9 +77,9 @@ foreach(name ${testnames})
         )
         add_test(NAME ${fullname}
             COMMAND ${GNU_MAKE_BIN} -C ${PROJECT_SOURCE_DIR}/druntime/test/${name}
-                ROOT=${outdir} DMD=${LDMD_CMD} BUILD=${build}
+                ROOT=${outdir} DMD=${LDMD_EXE_FULL} BUILD=${build} SHARED=1
                 DRUNTIME=${druntime_path_build} DRUNTIMESO=${shared_druntime_path_build}
-                SHARED=1 ${cflags_base} ${linkdl}
+                CFLAGS_BASE=${cflags_base} DFLAGS_BASE=${dflags_base} ${linkdl}
         )
         set_tests_properties(${fullname} PROPERTIES DEPENDS clean-${fullname})
     endforeach()
