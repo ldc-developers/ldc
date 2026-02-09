ldc-developers/ldc

commit b6e3298ca0f701b2674d6830c0cafc3a4f2ab4df

PowerPC64: Generalize inherit-real-ABI-from-C++-host-compiler logic to all PowerPC64 hosts, not just Linux (#5054)
Should fix #4959 for FreeBSD.

gen/target.cpp
@@ -98,11 +98,12 @@ llvm::Type *getRealType(const llvm::Triple &triple) {
     if (triple.isMusl()) { // Musl uses double
       return LLType::getDoubleTy(ctx);
     }
-#if defined(__linux__) && defined(__powerpc64__)
-    // for a PowerPC64 Linux build:
-    // default to the C++ host compiler's `long double` ABI when targeting
-    // PowerPC64 (non-musl) Linux
-    if (triple.isOSLinux()) {
+#if defined(__powerpc64__)
+    // for a PowerPC64 LDC build:
+    // for native builds (targeting some PowerPC64 with native OS), default to
+    // the C++ host compiler's `long double` ABI
+    auto nativeTriple = llvm::Triple(llvm::sys::getProcessTriple());
+    if (triple.getOS() == nativeTriple.getOS()) {
 #if __LDBL_MANT_DIG__ == 113
       return LLType::getFP128Ty(ctx);
 #elif __LDBL_MANT_DIG__ == 106

commit 9b4231e19cc5b4df1f8e53c1cfd483f35550cd94

PowerPC64: Fix build failure on ppc64le
Commit b6e3298ca0f701b2674d6830c0cafc3a4f2ab4df introduced a C++ scoping
issue where the `nativeTriple` variable was declared inside a case label
without proper braces, causing a "jump to case label crosses
initialization" error, breaking the build on ppc64le.

This commit adds the missing braces and fixes the build.

gen/target.cpp
@@ -94,7 +94,7 @@ llvm::Type *getRealType(const llvm::Triple &triple) {
     return LLType::getFP128Ty(ctx);
 
   case Triple::ppc64:
-  case Triple::ppc64le:
+  case Triple::ppc64le: {
     if (triple.isMusl()) { // Musl uses double
       return LLType::getDoubleTy(ctx);
     }
@@ -118,6 +118,7 @@ llvm::Type *getRealType(const llvm::Triple &triple) {
     }
 #endif
     return LLType::getPPC_FP128Ty(ctx);
+  }
 
   default:
     // 64-bit double precision for all other targets