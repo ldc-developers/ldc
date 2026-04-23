b374996f403df585e9932966d8fef35a83e54a20
Asadbek <aosindarov@gmail.com>
2026-04-23 21:15:28 +0200

    Implement support for simple saxpy kernel. Convert scalars into pointer and load them in the function body

--- gen/dcompute/abi-rewrites.h
+++ gen/dcompute/abi-rewrites.h
@@ -13,7 +13,16 @@
 
 #pragma once
 
+#include "gen/abi/abi.h"
 #include "gen/abi/generic.h"
+#include "gen/dcompute/druntime.h"
+#include "gen/dvalue.h"
+#include "gen/irstate.h"
+#include "gen/llvm.h"
+#include "gen/tollvm.h"
+#include "llvm/IR/AssemblyAnnotationWriter.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/Support/raw_ostream.h"
 
 #include "ir/irfuncty.h"
 
@@ -69,5 +78,21 @@ struct DComputePointerRewrite : ABIRewrite {
     arg.byref = false;
     arg.attrs.removeAttribute(llvm::Attribute::Dereferenceable);
     arg.attrs.removeAttribute(llvm::Attribute::NonNull);
+  }
+};
+
+struct DcomputeMetalScalarRewrite : ABIRewrite {
+  LLType *type(Type* t) override {
+    // XXXX: Scalar variables are stored in the constant memory space for Metal GPU
+    return llvm::PointerType::get(gIR->context(), 2/*Constant Memory space*/);
+  }
+
+  LLValue *getLVal(Type *dty, LLValue *v) override {
+    return v;
+  }
+
+  LLValue *put(DValue *v, bool isLValueExp, bool) override {
+    auto value = DtoRVal(v);
+    return value;
   }
 };
