2f933360a02aa1152c434589cc178cd0df71f3d5
Asadbek <aosindarov@gmail.com>
2026-04-25 12:01:53 +0200

    Fix CI build error

--- gen/dcompute/targetMetal.cpp
+++ gen/dcompute/targetMetal.cpp
@@ -49,11 +49,14 @@ public:
     // TODO: need to find 32-bit triple
     llvm::StringRef tripleString = "air64_v28-apple-macosx26.0.0";
 
-    #if LDC_LLVM_VER >= 2100
-        _ir->module.setTargetTriple(llvm::Triple(tripleString));
-    #else
-        _ir->module.setTargetTriple(tripleString);
-    #endif
+    _ir->module.setTargetTriple(llvm::Triple(tripleString));
+
+
+    // #if LDC_LLVM_VER >= 2100
+    //     _ir->module.setTargetTriple(llvm::Triple(tripleString));
+    // #else
+        // _ir->module.setTargetTriple(tripleString);
+    // #endif
         llvm::StringRef dataLayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64"
             "-v16:16:16-v24:32:32-v32:32:32-v48:64:64-v64:64:64-v96:128:128-v128:128:128-v192:256:256-v256:256:256-"
             "v512:512:512-v1024:1024:1024-n8:16:32";
