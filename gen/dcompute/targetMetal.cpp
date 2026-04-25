eae34b9a67a7dd2b8eb357fe07b1806534519e8e
Asadbek <aosindarov@gmail.com>
2026-04-25 11:23:47 +0200

    Fix CI build error

--- gen/dcompute/targetMetal.cpp
+++ gen/dcompute/targetMetal.cpp
@@ -48,14 +48,12 @@ public:
     // TODO: need to find 32-bit triple
     auto tripleString = "air64_v28-apple-macosx26.0.0";
 
-    // std::string targTripleStr = is64 ? SPIR_TARGETTRIPLE64
-    //                                   : SPIR_TARGETTRIPLE32;
     #if LDC_LLVM_VER >= 2100
         llvm::Triple targTriple = llvm::Triple(tripleString);
     #else
         std::string targTriple = tripleString;
     #endif
-        _ir->module.setTargetTriple(tripleString);
+        _ir->module.setTargetTriple(targTriple);
         llvm::StringRef dataLayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64"
             "-v16:16:16-v24:32:32-v32:32:32-v48:64:64-v64:64:64-v96:128:128-v128:128:128-v192:256:256-v256:256:256-"
             "v512:512:512-v1024:1024:1024-n8:16:32";
