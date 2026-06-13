c36b7a042b587fbfc85f6ec50b845f789b781a67
badnikhil <nikhilljatt@gmail.com>
2026-06-13 08:18:09 +0530

    Address PR feedback: Add semantic test for doHostThings

--- tests/compilable/dcompute_host_template_test.d
+++ tests/compilable/dcompute_host_template_test.d
@@ -1,4 +1,6 @@
 // RUN: %ldc -c -mdcompute-targets=cuda-350 -I%S %s
+// RUN: not %ldc -o- -mdcompute-targets=cuda-350 -verrors=0 -I%S -d-version=Fail %s 2>&1 | FileCheck %s
+
 @compute(CompileFor.deviceOnly) module dcompute_host_template_test;
 import ldc.dcompute;
 import inputs.host_template_module;
@@ -8,4 +10,9 @@ void kernel() {
     // This should compile successfully because DCompute semantic analysis
     // should skip over host-only template instantiations.
     alias X = HostTemplate!int;
+
+    version(Fail) {
+        // CHECK: dcompute_host_template_test.d([[@LINE+1]]): Error: can only call functions from other `@compute` modules in `@compute` code
+        X.doHostThings();
+    }
 }
