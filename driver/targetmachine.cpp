fdf2ba6e316e282834cf316330d4f4ee199733b7
Piotr Kubaj <pkubaj@anongoth.pl>
2026-07-16 12:32:46 +0000

    FreeBSD: add support for powerpc64 (#5141)
    powerpc64 has switched to ELFv2 in FreeBSD 13.0-RELEASE.

--- driver/targetmachine.cpp
+++ driver/targetmachine.cpp
@@ -143,7 +143,7 @@ const char *getABI(const llvm::Triple &triple, const llvm::SmallVectorImpl<llvm:
   case llvm::Triple::mips64el:
     return "n32";
   case llvm::Triple::ppc64:
-    return "elfv1";
+    return triple.isOSFreeBSD() ? "elfv2" : "elfv1";
   case llvm::Triple::ppc64le:
     return "elfv2";
   case llvm::Triple::riscv64:
