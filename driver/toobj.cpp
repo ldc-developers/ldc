51e72ef72e5795516d719f3cd1cb95f29ccf78fe
Asadbek <aosindarov@gmail.com>
2026-07-28 14:12:56 +0200

    Fix build error

--- driver/toobj.cpp
+++ driver/toobj.cpp
@@ -38,7 +38,6 @@
 #include "llvm/Transforms/IPO/AlwaysInliner.h"
 #include "llvm/Transforms/Utils/Cloning.h"
 #include "llvm/IR/Module.h"
-#include <_string.h>
 #include <cstddef>
 
 namespace llvm {
@@ -117,6 +116,7 @@ void codegenModule(llvm::TargetMachine &Target, llvm::Module &m,
 #ifdef LDC_LLVM_SUPPORTED_TARGET_AArch64
   if (cb == ComputeBackend::METAL) {
     {
+#ifdef __APPLE__
       // Inline non-kernel functions for Metal dcompute target
       inlineDComputeKernelFunctions(&m);
 
@@ -163,6 +163,8 @@ void codegenModule(llvm::TargetMachine &Target, llvm::Module &m,
           strsignal(-status));
       fatal();
     }
+
+#endif
 
     return;
   }
