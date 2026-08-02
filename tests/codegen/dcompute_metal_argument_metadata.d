40fc982344ca49ccb95bac66cca48893299b8b10
Asadbek <aosindarov@gmail.com>
2026-08-02 20:30:09 +0200

    Update llvm20 requirement to atleast_llvm20 in dcompute metal tests

--- tests/codegen/dcompute_metal_argument_metadata.d
+++ tests/codegen/dcompute_metal_argument_metadata.d
@@ -1,5 +1,5 @@
 // REQUIRES: target_AArch64
-// REQUIRES: llvm20
+// REQUIRES: atleast_llvm20
 
 // RUN: %ldc -c -mdcompute-targets=metal-400 --mdcompute-file-prefix=argument_metadata -output-ll -of=%t.ll %s
 // RUN: FileCheck %s --check-prefix=AIR < argument_metadata_metal400_64.air
