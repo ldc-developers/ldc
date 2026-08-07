292ec7bf752da92e546b0a1d1fca84d0d853ec5d
Asadbek <aosindarov@gmail.com>
2026-08-07 11:51:04 +0200

    Update file extension of Metal target output

--- tests/codegen/dcompute_metal_kernel_metadata.d
+++ tests/codegen/dcompute_metal_kernel_metadata.d
@@ -5,7 +5,7 @@
 // REQUIRES: atleast_llvm20
  
 // RUN: %ldc -c -mdcompute-targets=metal-400 --mdcompute-file-prefix=kernel_metadata -output-ll -of=%t.ll %s
-// RUN: FileCheck %s --check-prefix=AIR < kernel_metadata_metal400_64.air
+// RUN: FileCheck %s --check-prefix=AIR < kernel_metadata_metal400_64.ll
 @compute(CompileFor.deviceOnly) module kernels;
 import ldc.dcompute;
 
