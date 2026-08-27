cbab87d9d644013af0579e7d8f6f315512e45b70
Martin Kinkelin <noone@nowhere.com>
2026-08-27 15:46:20 +0200

    [one more]

--- tests/driver/cross_cc_fallback.d
+++ tests/driver/cross_cc_fallback.d
@@ -1,3 +1,5 @@
+// REQUIRES: target_AArch64
+
 // RUN: env PATH= CC= not %ldc -mtriple aarch64-unknown-linux-gnut64 -v -defaultlib= %s 2>&1 | FileCheck %s
 
 // CHECK-DAG: aarch64-unknown-linux-gnut64-gcc
